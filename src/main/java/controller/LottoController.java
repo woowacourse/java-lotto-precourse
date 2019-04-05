b0444efd7b8b591263426c16516278f9a50c1597
yunheehyeon <ok94life@daum.net>
2019-04-05 20:52:41 +0900

    사용자가 입력한 금액만큼 로또 번호 생성

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -2,19 +2,31 @@ package controller;
 
 import domain.Lotto;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LottoController {
 
     private List<Lotto> lottoList = new ArrayList<>();
-    private int money;
+    private static final int LOTTO_BOUND = 45;
+    private static final int LOTTO_SIZE = 6;
+    private Random random = new Random();
 
     public LottoController(int money) {
-        this.money = money;
+        for (int i = 0; i < money / 1000; i++) {
+            lottoList.add(createLotto());
+        }
     }
-    
-    public int getMoney() {
-        return money;
+
+    private Lotto createLotto() {
+        Set<Integer> numSet = new HashSet<>();
+
+        while(numSet.size() != LOTTO_SIZE) {
+            int num = random.nextInt(LOTTO_BOUND) + 1;
+            numSet.add(num);
+        }
+
+        return new Lotto(new ArrayList<>(numSet));
     }
+
+
 }
