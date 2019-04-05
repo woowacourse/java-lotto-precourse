534fd2d49cf106180f770d29b5260a3d30e06cf4
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-04-05 16:45:28 +0900

    틀 구축

---
+++ src/main/java/com/github/seokhyeonchoi/game/lotto/LottoGame.java
@@ -0,0 +1,24 @@
+package com.github.seokhyeonchoi.game.lotto;
+
+import java.util.List;
+
+import com.github.seokhyeonchoi.game.Game;
+
+public class LottoGame implements Game{
+	private List<Lotto> lottoNums;
+	private int purchaseAmount;
+	
+	public LottoGame() {
+		
+	}
+	
+	@Override
+	public void start() {
+		
+	}
+	
+	private void init() {
+		
+	}
+	
+}
