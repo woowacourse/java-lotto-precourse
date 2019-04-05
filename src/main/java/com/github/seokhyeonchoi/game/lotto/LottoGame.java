ad67a42f302569b8fad7cdb7ef0b3483cf608544
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-04-05 16:57:14 +0900

    구입 금액 입력 및 검증 메소드 구현

--- src/main/java/com/github/seokhyeonchoi/game/lotto/LottoGame.java
+++ src/main/java/com/github/seokhyeonchoi/game/lotto/LottoGame.java
@@ -1,24 +1,44 @@
 package com.github.seokhyeonchoi.game.lotto;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.github.seokhyeonchoi.game.Game;
 
-public class LottoGame implements Game{
+public class LottoGame implements Game {
+	private final int LEAST_PURCHASE_AMOUNT = 1000;
+	private final Scanner SCANNER = new Scanner(System.in);
+	
 	private List<Lotto> lottoNums;
 	private int purchaseAmount;
-	
+	private WinningLotto winningLotto;
+
 	public LottoGame() {
-		
+		lottoNums = new ArrayList<Lotto>();
+		init();
 	}
-	
+
 	@Override
 	public void start() {
-		
+
 	}
-	
+
 	private void init() {
-		
+
 	}
-	
+
+	private int inputPurchaseAmount() {
+		int amount = 0;
+		
+		while(!validPurchaseAmount(amount)) {
+			amount = SCANNER.nextInt();
+		}
+		
+		return amount;
+	}
+
+	private boolean validPurchaseAmount(int amount) {
+		return (amount >= LEAST_PURCHASE_AMOUNT);
+	}
 }
