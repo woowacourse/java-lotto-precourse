7b3c0347283d2c76e92c56c7a5d0a7b870d1a874
yunheehyeon <ok94life@daum.net>
2019-04-05 19:51:18 +0900

    기능 목록 수정

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,13 +1,19 @@
 package controller;
 
+import domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoController {
 
+    private List<Lotto> lottoList = new ArrayList<>();
     private int money;
 
     public LottoController(int money) {
         this.money = money;
     }
-
+    
     public int getMoney() {
         return money;
     }
