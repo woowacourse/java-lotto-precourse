4314fb67fd7cdae41702bd447958817912f0a301
chowhr1231 <thddbswo14@gmail.com>
2019-04-05 21:17:57 +0900

    [ADD] Lotto 만드는 메서드
    Lotto 클래스에서 static메서드로 Lotto번호 만든다
    랜덤 숫자 생성은 generateRandomNumber() 메서드로 생성한다.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,28 @@
+import domain.Lotto;
 import domain.UserInterface;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-
+        List<Lotto> lottos = new ArrayList<>();
+        Lotto lotto;
         String purchaseAmountStr = "";
-        int purchaseAmount = 0;
+
+        int lottoCnt = 0;
         boolean flag = false;
 
-        while(!flag){
+        while (!flag) {
             purchaseAmountStr = ui.inputPurchaseAmount();
             flag = ui.validatePurchaseAmount(purchaseAmountStr);
         }
-        purchaseAmount = Integer.parseInt(purchaseAmountStr);
+
+        lottoCnt = (int) Integer.parseInt(purchaseAmountStr) / 1000;
+        for (int i = 0; i < lottoCnt; i++) {
+            lotto = new Lotto(Lotto.generateLottoNumber());
+            lottos.add(lotto);
+        }
     }
 }
--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,16 +1,35 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
     private final List<Integer> numbers;
+    private final static int RANDOM_NUMBER_RANGE = 45;
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
     }
 
-    // 추가 기능 구현
+    public static List<Integer> generateLottoNumber() {
+        List<Integer> numList = new ArrayList<>();
+        int number;
+
+        while (numList.size() < 6) {
+            number = generateRandomNumber();
+            if (!numList.contains(number)) {
+                numList.add(number);
+            }
+        }
+
+        return numList;
+    }
+
+    private static int generateRandomNumber() {
+        return new Random().nextInt(RANDOM_NUMBER_RANGE) + 1;
+    }
 }
