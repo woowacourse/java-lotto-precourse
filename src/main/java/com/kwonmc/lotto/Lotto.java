4fbc30ea175ab8c816c84089aaa4988cea414a1a
mrkwon <erased11@gmail.com>
2019-04-05 15:42:50 +0900

    create README.md

--- src/main/java/com/kwonmc/lotto/Lotto.java
+++ src/main/java/com/kwonmc/lotto/Lotto.java
@@ -1,4 +1,4 @@
-package domain;
+package com.kwonmc.lotto;
 
 import java.util.List;
 
@@ -6,6 +6,9 @@ import java.util.List;
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
+    private static final int INT_ONE = 1;
+    private static final int LOTTO_TOTAL_NUMBERS = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
--- src/main/java/com/kwonmc/lotto/Rank.java
+++ src/main/java/com/kwonmc/lotto/Rank.java
@@ -1,4 +1,4 @@
-package domain;
+package com.kwonmc.lotto;
 
 /**
  * 로또 등수를 의미하는 enum
