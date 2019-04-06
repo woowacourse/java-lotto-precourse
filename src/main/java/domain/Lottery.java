e52fb29df4fd9680d0c8038a4a17384cf448abab
ep1stas1s <masloz@konkuk.ac.kr>
2019-04-06 17:25:04 +0900

    [ADD] 금액(개수)에 따른 무작위 로또 번호 추출 및

--- src/main/java/domain/Lottery.java
+++ src/main/java/domain/Lottery.java
@@ -1,5 +1,9 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Lottery {
@@ -7,38 +11,77 @@ public class Lottery {
 	// XXX Only 3 field in method!!
 	private static final Scanner SCANNER = new Scanner(System.in);
 	private static final int LOTTO_PRICE = 1_000;
-	
-	private int numberOfPurchases = 0;
-	
+
+	private Integer[] ALL_LOTTO_NUMBER = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+		11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+		21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+		31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+		41, 42, 43, 44, 45};
+	private static final int DRAWS_COUNT = 6;
+
+	private int lottoCount = 0;
+	private List<Lotto> lottoes;
+
 	public void runProgram() {
 		int inputtedPrice = inputPrice();
 		setLottoCount(inputtedPrice);
+		setLottoes(lottoCount);
+		printLottoes();
 	}
-	
+
 	private int inputPrice() {
 		String price = "";
 		do {
 			System.out.println("구입 금액을 입력해 주세요.");
 			price = SCANNER.nextLine();
-			
-		}while(isNaturalNumber(price));
+
+		} while (!isNaturalNumber(price));
+		// TODO int? long?
 		return Integer.parseInt(price);
 	}
-	
+
 	private boolean isNaturalNumber(String value) {
 		// TODO ADD more exception!
-		if(value == null || value.length() == 0) {
+		if (value == null || value.length() == 0) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	private void setLottoCount(int inputtedPrice) {
-		numberOfPurchases = inputtedPrice / LOTTO_PRICE;
-		
-		// TODO 위치 변경
-		System.out.println(numberOfPurchases + "개를 구매했습니다.");
+		lottoCount = inputtedPrice / LOTTO_PRICE;
 	}
-	
-	
+
+	private void setLottoes(int lottoCount) {
+		List<Lotto> purchasedLotto = new ArrayList<>();
+		for (int i = 0; i < lottoCount; ++i) {
+			purchasedLotto.add(createLotto());
+		}
+		this.lottoes = purchasedLotto;
+	}
+
+	private void printLottoes() {
+		System.out.println(lottoCount + "개를 구매했습니다.");
+		for(Lotto lotto : lottoes) {
+			System.out.println(lotto);
+		}
+	}
+
+	private Lotto createLotto() {
+		List<Integer> lottoNumbersToExtract = new ArrayList<>(Arrays.asList(ALL_LOTTO_NUMBER));
+		
+		List<Integer> lottoNumber = new ArrayList<>();
+		for (int i = 0; i < DRAWS_COUNT; ++i) {
+			int randomNumber = selectRandomLottoNumber(lottoNumbersToExtract);
+			lottoNumber.add(randomNumber);
+		}
+		Collections.sort(lottoNumber);
+		Lotto lotto = new Lotto(lottoNumber);
+		return lotto;
+	}
+
+	private int selectRandomLottoNumber(List<Integer> lottoNumbersToExtract) {
+		return lottoNumbersToExtract
+			.remove((int)(Math.random() * lottoNumbersToExtract.size()));
+	}
 }
