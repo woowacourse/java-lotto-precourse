842812231c786a665f5a1b3984c439bd0a54d282
ep1stas1s <masloz@konkuk.ac.kr>
2019-04-06 21:19:33 +0900

    [MOD] 자연수 확인 메소드의 예외상황 추가

--- src/main/java/domain/util/Validator.java
+++ src/main/java/domain/util/Validator.java
@@ -1,11 +1,21 @@
 package domain.util;
 
+import java.util.regex.Pattern;
+
 public class Validator {
+	private static final String NUMBER_PATTERN = "^[0-9]*$";
+
 	public static boolean isNaturalNumber(String value) {
-		// TODO ADD more exception!
 		if (value == null || value.length() == 0) {
+			return false;
+		}
+		if (!Pattern.matches(NUMBER_PATTERN, value)) {
+			return false;
+		}
+		if (Integer.parseInt(value) < 0) {
 			return false;
 		}
 		return true;
 	}
+
 }
