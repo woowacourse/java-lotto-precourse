dc1a21c6defc8db893a6ead8fe944f97eb74c644
Sungho,Yun <itlockit@gmail.com>
2019-04-06 23:08:09 +0900

    Verify user input is integer range
    Implement to re-input if the user has entered a number beyond the integer range

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -4,24 +4,34 @@ import java.util.Scanner;
 
 public class LottoGame {
     public LottoGame() {
-
+        int userInput = userInputPurchase();
     }
 
     private int userInputPurchase() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("구입 금액을 입력해주세요");
-        int purchase = scanner.nextInt();
+        int purchase = userInputStringToInt();
         while (!userInputVerify(purchase)) {
             System.out.println("잘못된 금액을 입력하셧습니다.");
-            purchase = scanner.nextInt();
+            purchase = userInputStringToInt();
         }
         return purchase;
     }
 
-    public boolean userInputVerify(int userInput) {
+    private boolean userInputVerify(int userInput) {
         if (userInput <= 0 || userInput % 1000 != 0) {
             return false;
         }
         return true;
+    }
+
+    private int userInputStringToInt() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("구입 금액을 입력해주세요");
+            String userInput = scanner.nextLine();
+            int result = Integer.parseInt(userInput);
+            return result;
+        } catch (NumberFormatException ex) {
+            return -1;
+        }
     }
 }
