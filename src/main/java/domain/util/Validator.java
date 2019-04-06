514e395754b69a275d6d061c7e05505f56ee185b
ep1stas1s <masloz@konkuk.ac.kr>
2019-04-06 21:20:19 +0900

    [ADD] 입력 받은 번호들이 로또에 사용할 수 있는지 확인

--- src/main/java/domain/util/Validator.java
+++ src/main/java/domain/util/Validator.java
@@ -1,8 +1,13 @@
 package domain.util;
 
+import java.util.Arrays;
+import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Validator {
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 45;
 	private static final String NUMBER_PATTERN = "^[0-9]*$";
 
 	public static boolean isNaturalNumber(String value) {
@@ -18,4 +23,37 @@ public class Validator {
 		return true;
 	}
 
+	public static boolean isValidInputtedNumbers(String input, int drawCount) {
+		System.out.println("잘 못 입력하였습니다.");
+		if (input == null || input.length() == 0) {
+			return false;
+		}
+		if (!isSameCount(input, drawCount)) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean isSameCount(String input, int drawCount) {
+		Set<Integer> refinedNumber = Arrays.stream(input.split(","))
+			.filter(stringNumber -> isIncludeLottoNumber(stringNumber))
+			.map(stringNumber -> Integer.parseInt(stringNumber))
+			.collect(Collectors.toSet());
+
+		if (refinedNumber.size() != drawCount) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean isIncludeLottoNumber(String value) {
+		if (!isNaturalNumber(value)) {
+			return false;
+		}
+		int number = Integer.parseInt(value);
+		if (number < MIN_NUMBER || number > MAX_NUMBER) {
+			return false;
+		}
+		return true;
+	}
 }
