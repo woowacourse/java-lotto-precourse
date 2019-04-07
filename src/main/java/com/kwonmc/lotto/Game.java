752440da349a403bca86e608c4416e38960306e1
mrkwon <erased11@gmail.com>
2019-04-07 21:06:36 +0900

    구입금액을 입력받는 Game 클래스의 getPurchaseAmount 메서드 구현

--- src/main/java/com/kwonmc/lotto/Game.java
+++ src/main/java/com/kwonmc/lotto/Game.java
@@ -7,4 +7,9 @@ public class Game {
     private ArrayList<Lotto> myLottoList; // 구매한 로또 번호들
     private Lotto lastWeekNumbers; // 지난 주 당첨 번호
     private int bonusNo; // 보너스 볼 번호
+
+    public void getPurchaseAmount(int purchaseAmount) {
+        System.out.println(Constant.MESSAGE_GET_PURCHASE_AMOUNT);
+        this.purchaseAmount = purchaseAmount;
+    }
 }
