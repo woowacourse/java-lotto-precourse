b18ca9c47e55cc44588fe0bcb364a7982f1b665f
mrkwon <erased11@gmail.com>
2019-04-07 21:19:15 +0900

    Game 클래스와 GameBuilder 클래스 분리

---
+++ src/main/java/com/kwonmc/lotto/Game.java
@@ -0,0 +1,13 @@
+package com.kwonmc.lotto;
+
+import java.util.ArrayList;
+
+public class Game {
+    private int purchaseAmount; // 구매금액
+    private ArrayList<Lotto> myLottoList; // 구매한 로또 번호들
+    private Lotto lastWeekNumbers; // 지난 주 당첨 번호
+    private int bonusNo; // 보너스 볼 번호
+
+
+
+}
--- src/main/java/com/kwonmc/lotto/GameBuilder.java
+++ src/main/java/com/kwonmc/lotto/GameBuilder.java
@@ -2,28 +2,22 @@ package com.kwonmc.lotto;
 
 import java.util.ArrayList;
 
-public class Game {
-    private int purchaseAmount; // 구매금액
-    private ArrayList<Lotto> myLottoList; // 구매한 로또 번호들
-    private Lotto lastWeekNumbers; // 지난 주 당첨 번호
-    private int bonusNo; // 보너스 볼 번호
+public class GameBuilder {
+    private int purchaseAmount;
+    private ArrayList<Lotto> myLottoList;
+    private Lotto lastWeekNumbers;
+    private int bonusNo;
 
-
-    public class GameBuilder {
-        private int purchaseAmount;
-        private ArrayList<Lotto> myLottoList;
-        private Lotto lastWeekNumbers;
-        private int bonusNo;
-
-        public GameBuilder setPurchaseAmount(int purchaseAmount) {
-            this.purchaseAmount = purchaseAmount;
-            return this;
-        }
-
-        public GameBuilder setMyLottoList() {
-            this.myLottoList = new ArrayList<>();
-            // TODO 구매 금액만큼 로또 객체 생성하여 추가
-            return this;
-        }
+    public GameBuilder setPurchaseAmount(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+        return this;
     }
+
+    public GameBuilder setMyLottoList() {
+        this.myLottoList = new ArrayList<>();
+        // TODO 구매 금액만큼 로또 객체 생성하여 추가
+        return this;
+    }
+
+
 }
