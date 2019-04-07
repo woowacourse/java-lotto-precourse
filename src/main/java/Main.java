19f9d10b2c8aebf6257e968be7e08efc0829423f
Jo Jinsik <kunggom@gmail.com>
2019-04-08 03:37:50 +0900

    getLottoNum() 작성.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,15 @@ import domain.*;
 
 public class Main {
     /** 상수 영역 */
-    final static int LOTTO_NUM_START = 1;
-    final static int LOTTO_NUM_END = 45;
-    final static int LOTTO_NUM_LENGTH = 6;
+    private final static int LOTTO_NUM_START = 1;
+    private final static int LOTTO_NUM_END = 45;
+    private final static int LOTTO_NUM_LENGTH = 6;
+    private final static Random random = new Random();
+    
+    /** 1에서 45 사이의 임의의 정수 하나를 생성하여 반환 */
+    private static int getLottoNum() {
+        return random.nextInt(LOTTO_NUM_END) + LOTTO_NUM_START;
+    }
     
     /** 문자열을 입력받아, 쉼표를 기준으로 나누어 문자열 배열로 반환 */
     private static String[] getInput(String message) {
