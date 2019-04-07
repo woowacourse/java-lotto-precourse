2baac1f3e5896c19283085945dcaedcef0f78c1b
deocksoo <dusol92@snu.ac.kr>
2019-04-07 17:19:27 +0900

    [ADD] LottoManager클래스의 issueLottoOf 메소드를 구현합니다
    issueLottoOf 메소드는 인자로 넘겨받은 구입 금액에 해당하는 로또를 발급합니다.
    autoIssue 메소드를 추가해 6개의 숫자를 랜덤하게 배정받은 로또를 생성하도록 하였습니다.

--- src/main/java/domain/LottoManager.java
+++ src/main/java/domain/LottoManager.java
@@ -1,9 +1,7 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 로또 게임 configuration과 사용자 인터페이스를 담당하는 객체
@@ -11,6 +9,12 @@ import java.util.Scanner;
 public class LottoManager {
     Scanner sc = new Scanner(System.in);
     Validator validator = new Validator();
+
+    private final int PRICE_PER_LOTTO = 1000;
+    private final int NUM_OF_WINNING_NUMBER = 6;
+    private final int WINNING_NUMBER_BOUND = 45 + 1;
+    private final int WINNING_NUMBER_ORIGIN = 1;
+
     private int purchaseAmount;
 
     public int inputPurchaseAmount() {
@@ -25,8 +29,24 @@ public class LottoManager {
         return this.purchaseAmount;
     }
 
+    public Lotto autoIssue() {
+        Random random = new Random();
+        List<Integer> autoNumbers = random.ints(NUM_OF_WINNING_NUMBER, WINNING_NUMBER_ORIGIN, WINNING_NUMBER_BOUND)
+                                    .boxed()
+                                    .collect(Collectors.toList());
+
+        return new Lotto(autoNumbers);
+    }
+
     public List<Lotto> issueLottoOf(int purchasedMoney) {
-        return new ArrayList<Lotto>();
+        List<Lotto> lottos = new ArrayList<Lotto>();
+        int numberOfLotto = purchasedMoney / PRICE_PER_LOTTO;
+
+        for (int i = 1; i <= numberOfLotto; i++) {
+            lottos.add(autoIssue());
+        }
+
+        return lottos;
     }
 
     public Lotto inputWinningNumber() {
