090c025ac6687934478a5cfb9638c99884c3f83c
Jo Jinsik <kunggom@gmail.com>
2019-04-08 06:13:48 +0900

    inputLottoNums() 메소드 작성.
    하는 김에 Main 클래스의 모든 구성요소를 final로 선언. 어디 다른 데서 이걸 갖다쓸 일도 없으니까.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,8 @@ public class Main {
             = "올바른 숫자가 입력되지 않았습니다. 다시 입력해 주세요.";
     private final static String INPUT_WIN_NUMBERS
             = "지난 주 당첨 번호를 입력해 주세요.";
+    private final static String INPUT_BONUS_BALL
+            = "보너스 볼을 입력해 주세요.";
     
     /** 1에서 45 사이의 임의의 정수 하나를 생성하여 반환 */
     private static int getLottoNum() {
@@ -21,14 +23,14 @@ public class Main {
     }
     
     /** 문자열을 입력받아, 쉼표를 기준으로 나누어 문자열 배열로 반환 */
-    private static String[] getInput(String message) {
+    private final static String[] getInput(String message) {
         Console console = System.console();
         String inputData = console.readLine(message + "\n"); // 메시지 출력
         return inputData.split(",\\s*"); // 정규 표현식 적용 - 띄어쓰기 때문
     }
     
     /** 입력된 문자열을 정수로 형변환할 수 있을지 테스트 */
-    private static boolean isOnlyNumber(String string) {
+    private final static boolean isOnlyNumber(String string) {
         if (string.length() == 0) {
             return false; // 빈 문자열은 취급하지 않는다.
         }
@@ -38,12 +40,12 @@ public class Main {
     }
     
     /** 임의의 정수가 로또 번호 범위 안에 있는지를 확인 */
-    private static boolean isLottoRange(int number) {
+    private final static boolean isLottoRange(int number) {
         return (number >= LOTTO_NUM_START) && (number <= LOTTO_NUM_END);
     }
     
     /** 임의의 정수 하나를 입력받아 반환한다. */
-    private static int inputSingleInt(String message) {
+    private final static int inputSingleInt(String message) {
         String temp = getInput(message)[0];
         boolean check = isOnlyNumber(temp);
         while (!check) { // 입력에 문제가 있으면 계속 재입력 요구
@@ -53,21 +55,34 @@ public class Main {
         return Integer.parseInt(temp);
     }
     
-    /** 로또 번호 6개를 입력받으려고 1번 시도한다. */
-    private static List<Integer> tryInputLottoNums(String message) {
+    /** 로또 번호를 입력받으려고 1번 시도한다. */
+    private final static List<Integer> tryInputLottoNums(String message) {
         Stream<String> temp = Arrays.stream(getInput(message));
         return temp
-                .filter(Main::isOnlyNumber)
-                .map(Integer::parseInt)
-                .filter(Main::isLottoRange)
+                .filter(Main::isOnlyNumber) // 숫자만 거름
+                .map(Integer::parseInt) // 정수로 변환
+                .filter(Main::isLottoRange) // 범위 확인
+                .distinct() // 중복 제거
+                .sorted() // 오름차순 정렬
                 .collect(Collectors.toList()); // stream을 List로 변환
     }
     
+    /** 로또 번호를 입력받아 Lotto 객체를 반환한다. */
+    private final static Lotto inputLottoNums() {
+        List<Integer> temp = tryInputLottoNums(INPUT_WIN_NUMBERS);
+        while (temp.size() != LOTTO_NUM_LENGTH) { // 문제가 없을 때까지 반복
+            temp = tryInputLottoNums(INPUT_INT_ERROR);
+        }
+        return new Lotto(temp);
+    }
+    
     /** main 진입점 */
-    public static void main(String[] args) {
+    public final static void main(String[] args) {
         // Test code - 추후 삭제 예정
         // List<Integer> lottoNums = Arrays.asList(1, 2, 3, 4, 5, 6);
         // Lotto lotto = new Lotto(lottoNums);
+        // System.out.println(lotto);
+        // Lotto lotto = inputLottoNums();
         // System.out.println(lotto);
     }
 }
