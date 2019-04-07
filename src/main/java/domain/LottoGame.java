fb7aeea664752325371809a355b3d216a2b0d9eb
YuSeonChoi <marine8743@naver.com>
2019-04-08 00:54:35 +0900

    당첨 통계 구현 완료

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -7,12 +7,17 @@ import java.util.Scanner;
 import static domain.InfoString.*;
 
 public class LottoGame {
-
     private static int INPUT_MONEY;
-
-    static List<Integer> lottoList = new ArrayList<>();
-    static Lotto winLotto;
-    static int bonus;
+    private static List<Integer> lottoList = new ArrayList<>();
+    private static Lotto winLotto;
+    private static int bonus;
+    private static List<Integer> rankArr = new ArrayList<>();
+    private static int five = 0;
+    private static int four = 0;
+    private static int three = 0;
+    private static int two = 0;
+    private static int one = 0;
+    private static double totalMoney = 0;
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
@@ -25,14 +30,14 @@ public class LottoGame {
         System.out.println(INFO_BONUS_NUM);
         setBonusNum();
         matchRank();
-        printOutPut();
+        printOutPut(INPUT_MONEY);
     }
 
     public static int checkMoney() {
-        int count = INPUT_MONEY / 1000;
+        int count = INPUT_MONEY / MIN_MONEY;
 
         if(count < MIN_COUNT) {
-            System.out.println("구입금액은 1000원 이상이어야 합니다!!!.");
+            System.out.println(INFO_MIN_BUY);
         }
 
         return count;
@@ -63,17 +68,23 @@ public class LottoGame {
 
         for(int i = 0; i < checkMoney(); i++) {
             rank = CreateLottoObject.match(CreateUserLotto.lottos[i]);
-            System.out.println(rank);
+            five = (rank == Rank.FIFTH) ? five+1 : five;
+            four = (rank == Rank.FOURTH) ? four+1 : four;
+            three = (rank == Rank.THIRD) ? three+1 : three;
+            two = (rank == Rank.SECOND) ? two+1 : two;
+            one = (rank == Rank.FIRST) ? one+1 : one;
+            totalMoney += rank.getWinningMoney();
         }
     }
 
-    public static void printOutPut() {
+    public static void printOutPut(int INPUT_MONEY) {
         System.out.println(INFO_RESULT_LOTTO);
-        System.out.println(INFO_FIVE_RANK);
-        System.out.println(INFO_FOUR_RANK);
-        System.out.println(INFO_THREE_RANK);
-        System.out.println(INFO_TWO_RANK);
-        System.out.println(INFO_ONE_RANK);
-        System.out.println(INFO_LOSS_RATE);
+        System.out.println(INFO_UNDER_LINE);
+        System.out.println(INFO_FIVE_RANK + five);
+        System.out.println(INFO_FOUR_RANK + four);
+        System.out.println(INFO_THREE_RANK + three);
+        System.out.println(INFO_TWO_RANK + two);
+        System.out.println(INFO_ONE_RANK + one);
+        System.out.println(INFO_LOSS_RATE + (totalMoney / INPUT_MONEY) +"입니다.");
     }
 }
