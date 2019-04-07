be5856771d4d8058db52a5e13322d18e3f68cd3a
DoorSupporter <jiwon3346@gmail.com>
2019-04-08 02:12:45 +0900

    Refactoring make BonusNumber(it work to return validational Bonus number)

--- src/main/java/domain/Shop.java
+++ src/main/java/domain/Shop.java
@@ -27,7 +27,7 @@ public class Shop extends Config {
         System.out.println("당첨 번호를 입력해주세요");
         Lotto lotto = createLotto(getLottoNumber());
         System.out.println("보너스 번호를 입력해주세요.");
-        int bonusNumber = createBonusNumber(lotto.getLottoNumber());
+        int bonusNumber = new BonusNumber(lotto.getLottoNumber()).getBonusNumber();
         WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
         return winningLotto;
     }
@@ -48,39 +48,5 @@ public class Shop extends Config {
         for (int i = 0; i < amount; i++) {
             System.out.println(lottobundle[i].printLottoNumber());
         }
-    }
-
-    private int createBonusNumber(List<Integer> winningNumber) {
-        int bonusNumber = checkBonusNumberValidation(INITIAL_VALUE, winningNumber);
-        return bonusNumber;
-    }
-
-    private int checkBonusNumberValidation(int bonusNumber, List<Integer> winningNumber) {
-        Scanner scan = new Scanner(System.in);
-        try {
-            int signedBonusNumber = scan.nextInt();
-            bonusNumber = checkBonusNumberRange(signedBonusNumber, winningNumber);
-            bonusNumber = checkDuplicateBonusNumber(bonusNumber, winningNumber);
-        } catch (InputMismatchException e) {
-            resetScanner();
-            bonusNumber = checkBonusNumberValidation(INITIAL_VALUE, winningNumber);
-        }
-        return bonusNumber;
-    }
-
-    private int checkBonusNumberRange(int bonusNumber, List<Integer> winningNumber) {
-        if (bonusNumber > LOTTO_MAX_VALUE || bonusNumber <= LOTTO_MIN_VALUE) {
-            System.out.println("번호는 1이상 45이하의 수만 입력이 가능합니다.");
-            bonusNumber = checkBonusNumberValidation(INITIAL_VALUE, winningNumber);
-        }
-        return bonusNumber;
-    }
-
-    private int checkDuplicateBonusNumber(int bonusNumber, List<Integer> winningNumber) {
-        if (winningNumber.contains(bonusNumber)) {
-            System.out.println("보너스 번호는 당첨 번호와 중복 될 수 없습니다.");
-            bonusNumber = checkBonusNumberValidation(INITIAL_VALUE, winningNumber);
-        }
-        return bonusNumber;
     }
 }
