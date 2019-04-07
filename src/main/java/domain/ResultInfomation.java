bdd930fcf3fbc3eeb32e5debe1c8017f6b27fa3f
chowhr1231 <thddbswo14@gmail.com>
2019-04-07 17:16:36 +0900

    [ADD] enumMap 초기화 작업
    파라미터로 받은 Rank list의 값으로 현재 각 Rank가
    몇개 인지 넣는 작업

--- src/main/java/domain/ResultInfomation.java
+++ src/main/java/domain/ResultInfomation.java
@@ -1,16 +1,32 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 
 public class ResultInfomation {
     private List<Rank> ranks = new ArrayList<>();
-    private int money;
+    private EnumMap<Rank, Integer> enumMap;
 
-    private static final int MIN_MONEY = 1000;
 
-    ResultInfomation(List<Rank> ranks) {
+    public ResultInfomation(List<Rank> ranks) {
         this.ranks = ranks;
-        this.money = ranks.size() * MIN_MONEY;
+        this.enumMap = createEnumMap();
+    }
+
+    private EnumMap<Rank, Integer> createEnumMap(){
+        EnumMap<Rank, Integer> map = new EnumMap<Rank, Integer>(Rank.class);
+        int cnt;
+
+        for(Rank type : Rank.values()){
+            map.put(type, 0);
+        }
+
+        for(Rank rank : ranks){
+            cnt = map.get(rank);
+            map.put(rank, ++cnt);
+        }
+
+        return map;
     }
 }
