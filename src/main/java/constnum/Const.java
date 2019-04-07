16b0daac6a6f76010f97608e4a775f4a465aac02
heebong <heebg2@naver.com>
2019-04-07 13:07:48 +0900

    상수 관리 - README.md에 상수 리스트 추가 - 상수 관리 Const.java파일 추가

---
+++ src/main/java/constnum/Const.java
@@ -0,0 +1,14 @@
+package constnum;
+
+public class Const {
+    public static final int PRIECE_OF_SINGLE_LOTTO = 1000;
+
+    public static final int MIN_PRICE_OF_LOTTO_BUY = PRIECE_OF_SINGLE_LOTTO;
+    public static final int MAX_PRICE_OF_LOTTO_BUY = 100000;
+
+    public static final int MIN_NUM_OF_LOTTO_SELECT = 10;
+    public static final int MAX_NUM_OF_LOTTO_SELECT = 45;
+    
+    public static final int WIN_NUM_CNT = 6;
+    public static final int BONUS_NUM_CNT = 1;
+}
