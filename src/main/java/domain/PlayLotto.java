0c196378c9e787088294b8020242033da90e6bb1
YuJieun <idpresent@naver.com>
2019-04-08 00:55:38 +0900

    로또 객체 생성 & 로또 번호 생성

--- src/main/java/domain/PlayLotto.java
+++ src/main/java/domain/PlayLotto.java
@@ -2,14 +2,20 @@ package domain;
 
 import java.util.*;
 
+import domain.Lotto;
+
 public class PlayLotto {
     public static int purchase_amount;
+    public static List<Lotto> lottos;
     public static Scanner sc;
 
     private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_COUNT = 6;
 
     public static void main(String[] args) {
         getPurchaseAmount();
+        makeLottoObject();
+        System.out.println("dd");
     }
 
     public static void getPurchaseAmount() {
@@ -30,5 +36,22 @@ public class PlayLotto {
         else if (amount % LOTTO_PRICE == 0)
             return true;
         return false;
+    }
+
+    public static void makeLottoObject() {
+        lottos = new ArrayList();
+        int i;
+        for (i = 0; i < purchase_amount; i++) {
+            List<Integer> numbers = new ArrayList<Integer>();
+            makeNumbers(numbers);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+    }
+
+    public static void makeNumbers(List<Integer> numbers) {
+        for (int i = 0; i < LOTTO_COUNT; i++) {
+            numbers.add((int) (Math.random() * 45) + 1);
+        }
     }
 }
