56910c7f8f0a1ab5c5852a8308fe8a52763b466f
Jo Jinsik <kunggom@gmail.com>
2019-04-08 02:30:04 +0900

    Rank.getRankDescription() 작성.
    이걸 쓰면 나중에 당첨 통계를 비교적 간결하게 작성할 수 있을 것 같다.
    
    그나저나 이거 자동완성 어떻게 고치지? 다른 파일에 있는 메소드 같은 걸 전혀 못 불러오는 모양이다.

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -8,17 +8,27 @@ public enum Rank {
     FIFTH(3, 5_000), // 5등
     FOURTH(4, 50_000), // 4등
     THIRD(5, 1_500_000), // 3등
-    SECOND(5, 30_000_000), // 2등
+    SECOND(5, 30_000_000, ", 보너스 볼 일치"), // 2등
     FIRST(6, 2_000_000_000); // 1등;
 
     private static final int WINNING_MIN_COUNT = 3;
 
     private int countOfMatch;
     private int winningMoney;
+    private String additionalMessage = "";
 
     private Rank(int countOfMatch, int winningMoney) {
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
+    }
+
+    private Rank(int countOfMatch, int winningMoney, String additionalMessage) {
+        this(countOfMatch, winningMoney);
+        this.additionalMessage = additionalMessage;
+    }
+    
+    private boolean matchCount(int countOfMatch) {
+        return this.countOfMatch == countOfMatch;
     }
 
     public int getCountOfMatch() {
@@ -47,7 +57,12 @@ public enum Rank {
         throw new IllegalArgumentException(countOfMatch + "는 유효하지 않은 값입니다.");
     }
 
-    private boolean matchCount(int countOfMatch) {
-        return this.countOfMatch == countOfMatch;
+    public String getRankDescription() {
+        if (MISS.equals(this)) {
+            return "낙첨";
+        }
+        
+        return this.countOfMatch + "개 일치" + this.additionalMessage
+                + " (" + this.winningMoney + "원)";
     }
 }
