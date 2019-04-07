35767c97760ff2bc9f8192eb88b2a3dad3c3d7f5
HeavyRyan92 <heavyryan92@gmail.com>
2019-04-07 10:41:25 +0900

    로또
     - makeLottoNumber() 메소드 추가
       로또 번호 생성 메소드 추가
    
     - 위 사항에 따른 클래스 변수 및 유틸리티 임포트

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,15 +1,30 @@
 package domain;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int LOTTO_LENGTH = 6;
+    private static final int END_LOTTO_NUMBER = 45;
+    private static final int START_LOTTO_NUMBER = 1;
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
+    }
+
+    public List<Integer> makeLottoNumber(){
+
+        Random makeLottoNumber = new Random();
+        for(int i = 0; i<LOTTO_LENGTH; i++){
+
+            this.numbers.add(makeLottoNumber.nextInt(END_LOTTO_NUMBER)+START_LOTTO_NUMBER);        // 로또 번호 생성
+        }
+        return numbers;
     }
 
     // 추가 기능 구현
