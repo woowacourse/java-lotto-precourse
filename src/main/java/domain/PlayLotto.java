f286fd717b5a2041b5417092bf4dbb1343971d1f
YuJieun <idpresent@naver.com>
2019-04-08 00:28:30 +0900

    구입금액을 검사한다

--- src/main/java/domain/PlayLotto.java
+++ src/main/java/domain/PlayLotto.java
@@ -1,16 +1,34 @@
 package domain;
+
 import java.util.*;
 
 public class PlayLotto {
     public static int purchase_amount;
-    public static void main(String[] args){
+    public static Scanner sc;
+
+    private static final int LOTTO_PRICE = 1000;
+
+    public static void main(String[] args) {
         getPurchaseAmount();
     }
 
-    public static int getPurchaseAmount(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("구입금액을 입력해주세요");
-        purchase_amount = sc.nextInt();
-        return purchase_amount;
+    public static void getPurchaseAmount() {
+        sc = new Scanner(System.in);
+        int purchase_money = 0;
+        boolean flag = false;
+        while (!flag) {
+            System.out.println("구입금액을 입력해주세요");
+            purchase_money = sc.nextInt();
+            flag = isPurchaseMoneyValid(purchase_money);
+        }
+        purchase_amount = purchase_money / LOTTO_PRICE;
+    }
+
+    public static boolean isPurchaseMoneyValid(int amount) {
+        if (amount <= 0)
+            return false;
+        else if (amount % LOTTO_PRICE == 0)
+            return true;
+        return false;
     }
 }
