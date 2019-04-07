c2f56edf60fbd78ad48c183391312ff70668626c
DoorSupporter <jiwon3346@gmail.com>
2019-04-07 18:10:12 +0900

    Refactoring Machine get class Variable

--- src/main/java/domain/Machine.java
+++ src/main/java/domain/Machine.java
@@ -8,30 +8,30 @@ public class Machine extends Config {
     private int count;
     private Lotto[] lottoBundle;
 
-    public Machine(int price){
-        this.count = (price/LOTTO_PRICE);
+    public Machine(int price) {
+        this.count = (price / LOTTO_PRICE);
         this.lottoBundle = new Lotto[count];
     }
 
     public Lotto[] sellLotto(int price) {
         System.out.println("랜덤으로 로또가 생성됩니다.");
-        for(int i=0; i< count; i++) {
+        for (int i = 0; i < count; i++) {
             fillLottoNumber();
             lottoBundle[i] = createLotto(getRandomLottoNumber());
         }
         return this.lottoBundle;
     }
 
-    private List<Integer> getRandomLottoNumber(){
+    private List<Integer> getRandomLottoNumber() {
         List<Integer> lottoUserNumber = new ArrayList<>();
-        for(int i=0; i<LOTTO_NUMBER_SIZE; i++) {
-            lottoUserNumber.add(lottoMachineNumber.remove(createRandomNumber((LOTTO_MAX_VALUE-1)-i)));
+        for (int i = 0; i < LOTTO_NUMBER_SIZE; i++) {
+            lottoUserNumber.add(lottoMachineNumber.remove(createRandomNumber((LOTTO_MAX_VALUE - 1) - i)));
         }
         return lottoUserNumber;
     }
 
     private int createRandomNumber(int size) {
-        return(int)(Math.random()*size)+1;
+        return (int) (Math.random() * size) + 1;
     }
 
     private Lotto createLotto(List<Integer> lottoNumber) {
@@ -40,7 +40,7 @@ public class Machine extends Config {
 
     private void fillLottoNumber() {
         lottoMachineNumber.clear();
-        for(int i=(LOTTO_MIN_VALUE+1); i<=LOTTO_MAX_VALUE; i++) {
+        for (int i = (LOTTO_MIN_VALUE + 1); i <= LOTTO_MAX_VALUE; i++) {
             lottoMachineNumber.add(i);
         }
     }
