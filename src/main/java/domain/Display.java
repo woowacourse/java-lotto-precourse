f65a7bb89bbb38d5d56e67152ef7a76775a14f54
Sangwon <junothyme@gmail.com>
2019-04-07 14:56:18 +0900

    구입한 로또의 숫자를 보여주는 기능 + 지난주 당첨 숫자를 입력하는 기능 + 보너스 번호를 입력하는 기능 - Display.java

--- src/main/java/domain/Display.java
+++ src/main/java/domain/Display.java
@@ -1,19 +1,44 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Display {
 
-    Scanner sc;
+    private static final int limitOfLotto = 6;
+    private static Scanner sc = new Scanner(System.in);
+    private static List<Integer> winningLotto = new ArrayList<Integer>();
 
-    public Display(){
-        sc = new Scanner(System.in);
+    private Display() {
     }
 
-    int inputMoney(){
+    public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         int money = sc.nextInt();
-        int numbersOfLottoes = money / 1000;
-        return numbersOfLottoes;
+        return money;
+    }
+
+    public static void showBoughtLotto(List<Lotto> userLottoTickets) {
+        System.out.println(userLottoTickets.size() + "개를 구매했습니다.");
+        for (int i = 0; i < userLottoTickets.size(); i++) {
+            System.out.println(userLottoTickets.get(i));
+        }
+    }
+
+    public static List<Integer> inputLastWeekWinningNumbers() {
+        System.out.println("지난주 당첨번호를 입력해 주세요.");
+        String tmp = sc.next();
+        String[] s = tmp.split(",");
+        for(String s1: s) {
+            winningLotto.add(Integer.parseInt(s1));
+        }
+        return winningLotto;
+    }
+
+    public static int inputBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        int bonusNo = sc.nextInt();
+        return bonusNo;
     }
 }
