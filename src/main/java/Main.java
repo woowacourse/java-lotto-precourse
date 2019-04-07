f618ad4c7ded4a1e118eeddf4b99441fc8d0b81f
Jo Jinsik <kunggom@gmail.com>
2019-04-08 02:58:59 +0900

    getInput() 메소드 작성.
    지금 와서 요구사항을 잘 들여다보다가 깨달은 것인데, 로또 번호를 수동으로 입력하는 게 아니라 자동 생성해야 하는 모양이다.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
+import java.util.*;
+import java.io.Console;
+import domain.*;
+
 public class Main {
+    /** 상수 영역 */
+    final static int LOTTO_NUM_START = 1;
+    final static int LOTTO_NUM_END = 45;
+    final static int LOTTO_NUM_LENGTH = 6;
+    
+    /** 문자열을 입력받아, 쉼표를 기준으로 나누어 문자열 배열로 반환 */
+    private static String[] getInput(String message) {
+        Console console = System.console();
+        String inputData = console.readLine(message + "\n"); // 메시지 출력
+        return inputData.split(",\\s*"); // 정규 표현식 적용 - 띄어쓰기 때문
+    }
+    
+    /** main 진입점 */
     public static void main(String[] args) {
         // TODO
-        System.out.println("Hello, World!");
     }
 }
