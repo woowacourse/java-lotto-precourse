b10ab431989231250ec3b131725fa5a14c829dda
mrkwon <erased11@gmail.com>
2019-04-07 20:32:57 +0900

    상수들을 모아둘 Constant 클래스 생성

---
+++ src/main/java/com/kwonmc/lotto/Constant.java
@@ -0,0 +1,6 @@
+package com.kwonmc.lotto;
+
+public class Constant {
+    public static final int INT_ONE = 1;
+    public static final int LOTTO_TOTAL_NUMBERS = 45;
+}
--- src/main/java/com/kwonmc/lotto/Lotto.java
+++ src/main/java/com/kwonmc/lotto/Lotto.java
@@ -6,9 +6,6 @@ import java.util.List;
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
-    private static final int INT_ONE = 1;
-    private static final int LOTTO_TOTAL_NUMBERS = 45;
-
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
