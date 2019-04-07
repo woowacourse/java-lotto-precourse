c33cc2fa2d80e5d1cc8af32bdae8d418d16b925e
moonsung <visitant5@nate.com>
2019-04-08 02:00:35 +0900

    add 입출력 Message

--- src/main/java/domain/Message.java
+++ src/main/java/domain/Message.java
@@ -1,4 +1,21 @@
 package domain;
 
+import java.util.HashMap;
+
 public class Message {
+    public static HashMap<String, String> lottoInputMessage = new HashMap<String, String>() {
+        {
+            put("INPUT_PURCHASEMONEY", "구입금액을 입력해 주세요.");
+            put("INPUT_LASTWEEK_LOTTONUMBER", "지난주 당첨 번호를 입력해 주세요.");
+            put("INPUT_BONUSLOTTO", "보너스 볼을 입력해 주세요.");
+        }
+    };
+
+    public static HashMap<String,String> lottoOutputMessage = new HashMap<String, String>() {
+        {
+            put("OUTPUT_BUYLOTTO", "개를 구매했습니다.");
+            put("OUTPUT_STATISTICS","당첨 통계");
+            put("OUTPUT_STATISTICSLINE","=======");
+        }
+    };
 }
