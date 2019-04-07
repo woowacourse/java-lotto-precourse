5718c649f1e53d3d1c9099c3a688d31c68916d33
Yujin Kim <99stella24@naver.com>
2019-04-07 20:05:27 +0900

    add method intiLotto()

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,14 +1,29 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoGame {
     public void run(){
-      int num = getThePriceOfLotto();
+      int num = getThePriceOfLotto()/1000;
+      ArrayList<Lotto> lottoArrayList = new ArrayList<Lotto>();
+      for(int i = 0 ; i < num ; i++){
+        lottoArrayList.add(initLotto());
+      }
     }
     public int getThePriceOfLotto(){
       System.out.println("구입금액을 입력해 주세요.");
       Scanner scan = new Scanner(System.in);
       return scan.nextInt();
+    }
+    public Lotto initLotto(){
+      List<Integer> lottoNumbers = IntStream.range(1,46).boxed().collect(Collectors.toList());
+      Collections.shuffle(lottoNumbers);
+      return new Lotto(lottoNumbers.subList(0,6));
     }
 }
