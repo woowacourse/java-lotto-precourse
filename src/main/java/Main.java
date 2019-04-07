85f95b2a5abafe238759f014125cf743a26f9c4c
Jo Jinsik <kunggom@gmail.com>
2019-04-08 08:02:15 +0900

    정수 -> 자연수로 고침.
    코드 테스트하다 문득 생각난 것인데, 이 프로그램에서는 입력도 그렇고 처리 내용도 그렇고 음수가 나올 일이 전혀 없다.
    
    심지어는 음수 부호를 사용자가 입력하는 것도 걸러지는 마당에 정수라고 하는 건 좀 그러니 정확한 표현인 [자연수]를 쓰기로 했다.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,7 +37,7 @@ public class Main {
         return inputData.split(",\\s*"); // 정규 표현식 적용 - 띄어쓰기 때문
     }
     
-    /** 입력된 문자열을 정수로 형변환할 수 있을지 테스트 */
+    /** 입력된 문자열을 자연수로 형변환할 수 있을지 테스트 */
     private final static boolean isOnlyNumber(String string) {
         if (string.length() == 0) {
             return false; // 빈 문자열은 취급하지 않는다.
@@ -47,12 +47,12 @@ public class Main {
                 .noneMatch(i -> (i < 0 || i > 9)); // 0부터 9 사이의 숫자인가
     }
     
-    /** 임의의 정수가 로또 번호 범위 안에 있는지를 확인 */
+    /** 임의의 자연수가 로또 번호 범위 안에 있는지를 확인 */
     private final static boolean isLottoRange(int number) {
         return (number >= LOTTO_NUM_START) && (number <= LOTTO_NUM_END);
     }
     
-    /** 임의의 정수 하나를 입력받아 반환한다. */
+    /** 임의의 자연수 하나를 입력받아 반환한다. */
     private final static int inputSingleInt(String message) {
         String temp = getInput(message)[0];
         boolean check = isOnlyNumber(temp);
@@ -68,7 +68,7 @@ public class Main {
         Stream<String> temp = Arrays.stream(getInput(message));
         return temp
                 .filter(Main::isOnlyNumber) // 숫자만 거름
-                .map(Integer::parseInt) // 정수로 변환
+                .map(Integer::parseInt) // 자연수로 변환
                 .filter(Main::isLottoRange) // 범위 확인
                 .distinct() // 중복 제거
                 .sorted() // 오름차순 정렬
