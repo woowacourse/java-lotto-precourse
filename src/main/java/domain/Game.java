08de8d0ca81a3931522dd9d5cac7c3deaf2aa957
dasistHYOJIN <hgenie14@gmail.com>
2019-04-08 16:49:13 +0900

    Game 클래스 generateLotto() 추가: 구입 금액만큼의 로또 생성

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,21 +1,39 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Game {
     private Scanner sc = new Scanner(System.in);
+    private Random random = new Random();
 
     private List<Lotto> lottos = new ArrayList<>();
 
     protected void execute() {
-        int gameNum = setGameNum(sc.nextInt());
-        sc.nextLine();
+        int gameNum = setGameNum();
+
+        for (int i = 0; i < gameNum; i++) {
+            lottos.add(generateLotto());
+        }
 
     }
 
-    private int setGameNum(int total) {
+    private Lotto generateLotto() {
+        Set<Integer> set = new HashSet<>();
+
+        while (set.size() < 6) {
+            set.add(random.nextInt(45) + 1);
+        }
+
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+
+        return new Lotto(list);
+    }
+
+    private int setGameNum() {
+        int total = sc.nextInt();
+        sc.nextLine();
+
         return total / 1000;
     }
 }
