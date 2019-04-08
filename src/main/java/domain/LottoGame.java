8f94d3d2d61fb5d05420ab67e15eaf8fcd9f7310
wbluke <pwb0127@naver.com>
2019-04-08 16:19:26 +0900

    [FIX] Rank 클래스의 valueOf() 수정

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,12 +1,11 @@
 package domain;
 
 public class LottoGame {
-
-
     public static void main(String[] args) {
         int purchasePrice = new Purchasing().getPrice();
         LottoTickets lottoTickets = new LottoTickets(purchasePrice);
         lottoTickets.showNumberOfTicketsAndChanges();
         lottoTickets.showAll();
+
     }
 }
