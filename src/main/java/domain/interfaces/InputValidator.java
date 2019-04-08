f760b137e13cdefdb544c48886d6881812acb8a2
LeeJoonkyu <ljk6463@gmail.com>
2019-04-08 18:09:49 +0900

    implement 1~45 임의의 난수 생성

--- src/main/java/domain/interfaces/InputValidator.java
+++ src/main/java/domain/interfaces/InputValidator.java
@@ -2,7 +2,7 @@ package domain.interfaces;
 
 public interface InputValidator {
 
-    int MAX_AFFORDABLE_LOTTO_PURCHASE_AMOUNT = 10000;
+    int MAX_AFFORDABLE_LOTTO_PURCHASE_AMOUNT = 100000;
     int MINIMUM_PURCHASE_AMOUNT = 0;
     int MAX_LOTTO_LENGTH = 6;
     int PRICE_PER_LOTTO = 1000;
---
+++ src/main/java/domain/interfaces/NumberGenerator.java
@@ -0,0 +1,9 @@
+package domain.interfaces;
+
+public interface NumberGenerator {
+
+    int MIN = 1;
+    int MAX = 45;
+
+    int getNumber();
+}
