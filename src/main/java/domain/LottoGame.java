2cac5b541a506df05a29211df9974160975b7388
Soojin Roh <sooreal91@gmail.com>
2019-04-08 21:37:17 +0900

    구매 금액을 입력 받는 기능 구현
    inputMoney()의 Scanner.nextLine()매소드를 이용해 로또 구매 금액을 입력 받음

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,5 +1,15 @@
 package domain;
 
-public class LottoGame {
+import java.util.Scanner;
 
+public class LottoGame {
+	static private Scanner sc = new Scanner(System.in);
+	
+	private String inputMoney() {
+		System.out.println("구매금액을 입력해 주세요.");
+		
+		String money = sc.nextLine();
+		
+		return money;
+	}
 }
