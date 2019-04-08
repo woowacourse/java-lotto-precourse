cec9b4053612c727f7726e3b39b93d0dd3c07d03
nauco <hodong42@gmail.com>
2019-04-08 17:03:25 +0900

    make branch

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -5,6 +5,12 @@ import java.util.List;
 /**
  * 로또 한장을 의미하는 객체
  */
+
+/*
+ 기본 생성자 추가 x
+ numbers 변수의 접근 제어자인 private을 변경할 수 없다.
+ Lotto에 필드(인스턴스 변수)를 추가할 수 없다.
+ */
 public class Lotto {
     private final List<Integer> numbers;
 
--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -3,6 +3,12 @@ package domain;
 /**
  * 당첨 번호를 담당하는 객체
  */
+
+/*
+ WinningLotto 기본 생성자를 추가할 수 없다.
+ lotto, bonusNo 변수의 접근 제어자인 private을 변경할 수 없다.
+ WinningLotto에 필드(인스턴스 변수)를 추가할 수 없다.
+ */
 public class WinningLotto {
     private final Lotto lotto;
     private final int bonusNo;
