d5f1b55e0e570a153c457fb7c470d178c33f7caa
483759 <483759@naver.com>
2019-04-09 00:53:34 +0900

    잘못된 입력을 판단하는 IsValidAmount() 메소드 구현 및 메소드의 접근제어자 수정

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,17 +1,42 @@
 package domain;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LottoGame {
 
-  public static int InputPurchaseAmount(){
+  private boolean IsValidAmount(int purchaseAmount){
+    if(purchaseAmount < 0){
+      return false;
+    }
+    if(purchaseAmount % 1000 != 0){
+      return false;
+    }
+    return true;
+  }
+
+  private int InputPurchaseAmount(){
     Scanner sc = new Scanner(System.in);
     System.out.println("구입금액을 입력해 주세요.");
-    int purchaseAmount = sc.nextInt();
+    int purchaseAmount = -1;
+    try {
+      purchaseAmount= sc.nextInt();
+    }catch (InputMismatchException e){
+      System.out.println("유효하지 않은 값입니다");
+    }
     return purchaseAmount;
   }
 
+  public void StartLotto(){
+    int purchaseAmount = this.InputPurchaseAmount();
+    if(!this.IsValidAmount(purchaseAmount)){
+      System.out.println("잘못된 금액입니다.");
+      return;
+    }
+  }
+
   public static void main(String args[]){
-    
+    LottoGame game = new LottoGame();
+    game.StartLotto();
   }
 }
