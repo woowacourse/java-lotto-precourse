d1c477db4857814729de772fe00b2d2c925b0a18
DoWon Lee <gch01410@ajou.ac.kr>
2019-04-08 17:02:46 +0900

    로또번호와 당첨번호 사이의 일치하는 개수 구하는 기능 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -6,6 +6,9 @@ import java.util.List;
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
+    private static final int ONE = 1;
+    private static final int ZERO = 0;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -14,5 +17,19 @@ public class Lotto {
 
     protected void printLottoNumbers() {
         System.out.println(numbers.toString());
+    }
+
+    protected int getCountOfMatch(Lotto winningLotto) {
+        int countOfMatch = 0;
+
+        for (int number : numbers) {
+            countOfMatch += winningLotto.hasNumber(number) ? ONE : ZERO;
+        }
+
+        return countOfMatch;
+    }
+
+    private boolean hasNumber(int numberToCompare) {
+        return numbers.contains(numberToCompare);
     }
 }
