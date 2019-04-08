3cc7f2ee25cafa6f9fa0f3d6dc6a2e483ce4b116
moonsung <visitant5@nate.com>
2019-04-09 01:07:36 +0900

    update ErrorMessage

--- src/main/java/domain/Message.java
+++ src/main/java/domain/Message.java
@@ -11,11 +11,11 @@ public class Message {
         }
     };
 
-    public static HashMap<String,String> lottoOutputMessage = new HashMap<String, String>() {
+    public static HashMap<String, String> lottoOutputMessage = new HashMap<String, String>() {
         {
             put("OUTPUT_BUYLOTTO", "개를 구매했습니다.");
-            put("OUTPUT_STATISTICS","당첨 통계");
-            put("OUTPUT_STATISTICSLINE","=======");
+            put("OUTPUT_STATISTICS", "당첨 통계");
+            put("OUTPUT_STATISTICSLINE", "=======");
         }
     };
 
@@ -23,9 +23,10 @@ public class Message {
         {
             put("ERROR_ONLYNUMBER", "숫자로 입력하세요.");
             put("ERROR_MONEYSHORT", "금액이 부족하여 구매할 수 없습니다.");
-            put("ERROR_SIXNUMBER","숫자 여섯개를 정확히 입력하세요.");
+            put("ERROR_SIXNUMBER", "숫자 여섯개를 정확히 입력하세요.");
             put("ERROR_OVERLAP", "중복된 숫자가 있습니다.");
-            put("ERROR_NUMBERLIMIT","숫자의 범위는 0~45입니다.");
+            put("ERROR_NUMBERLIMIT", "숫자의 범위는 0~45입니다.");
+            put("ERROR_EMPTY", "공백부분이 포함되어있습니다.");
         }
     };
 }
