39b5724066a0a7894b42ec85ad236f1666006227
kangmin <kangmin789@naver.com>
2019-04-08 20:42:15 +0900

    [ADD] GenerateLotto 메소드 기능 구현
    GenerateLotto 메소드는 생성된 난수를 리스트에 넣고 그 리스트를 인자로 Lotto객체를 생성하는 기능을 하는 메소드 입니다.

--- src/main/java/domain/LottoGenerator.java
+++ src/main/java/domain/LottoGenerator.java
@@ -7,6 +7,7 @@ public class LottoGenerator {
     private static final int PRIZE_OF_LOTTO = 1000;
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int LOTTO_NUMBER_COUNT = 6;
 
     private List<Lotto> lottoList;
     private List<Integer> lottoNumbers;
@@ -24,12 +25,18 @@ public class LottoGenerator {
     }
 
     private void GenerateLotto() {
+        for (int lottoNumberCount = 1; lottoNumberCount <= LOTTO_NUMBER_COUNT; lottoNumberCount++){
+            lottoNumbers.add(GenerateRandomNumber());
+        }
+
+        lottoList.add(new Lotto(lottoNumbers));
+        lottoNumbers.clear();
         
     }
 
     private int GenerateRandomNumber() {
         boolean isNotValidLottoNumber = true;
-        int candidateLottoNumber =0;
+        int candidateLottoNumber = 0;
 
         while (isNotValidLottoNumber) {
             candidateLottoNumber = (int) ((Math.random() * MAX_LOTTO_NUMBER) + MIN_LOTTO_NUMBER);
