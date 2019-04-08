86b348a6142d7eb5d3b7536c75cbd471de54e233
Soojin Roh <sooreal91@gmail.com>
2019-04-08 21:51:23 +0900

    입력받은 구매금액이 1,000원 단위의 정수인지, 100000이 넘지 않는지 검사
    로또 1장의 금액이 1000원이기 때문에 구매 금액으로 입력 받은 값은 1000으로 나누어져야 하는 양수이고, 1인당 1회 로또 구입 가능 금액의 한도가 100000만원이기 때문에 구매 금액이 100000원 이하인지 검사하는 기능 구현

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -6,6 +6,8 @@ import java.util.regex.Pattern;
 public class LottoGame {
 	static private Scanner sc = new Scanner(System.in);
 	static private final String NUMBER_CHECK_REGEX = "^[0-9]*$";
+	static private final int LOTTO_PRICE = 1000;
+	static private final int LOTTO_BUY_LIMIT = 100000;
 	
 	private String inputMoney() {
 		System.out.println("구매금액을 입력해 주세요.");
@@ -17,4 +19,13 @@ public class LottoGame {
 		return (Pattern.matches(NUMBER_CHECK_REGEX, money)) ? true : false;
 	}
 	
+	private boolean devideByThousand(String money) {
+		if (Integer.parseInt(money) % LOTTO_PRICE != 0 || Integer.parseInt(money) > LOTTO_BUY_LIMIT) {
+			System.out.println("100,000이하의 금액을 1,000원 단위로 입력해주세요.");
+			return false;
+		}
+		return true; 
+	}
+	
+
 }
