61a33d0b0426a3368517018a4c327c0616f319c3
wbluke <pwb0127@naver.com>
2019-04-08 14:49:23 +0900

    [REF] showNumberOfTicketsAndChanges()를 외부(메인)에서 호출할 수 있도록 변경

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -6,6 +6,7 @@ public class LottoGame {
     public static void main(String[] args) {
         int purchasePrice = new Purchasing().getPrice();
         LottoTickets lottoTickets = new LottoTickets(purchasePrice);
+        lottoTickets.showNumberOfTicketsAndChanges();
 
     }
 }
--- src/main/java/domain/LottoTickets.java
+++ src/main/java/domain/LottoTickets.java
@@ -1,29 +1,38 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoTickets {
     public static final int PRICE_OF_LOTTO = 1000;
     public static final int LOWER_BOUND_OF_CHANGE = 0;
     private List<Lotto> tickets = new ArrayList<>();
+    private int purchasePrice;
 
     public LottoTickets(int purchasePrice) {
-        int numberOfTickets = calculateNumberOfTickets(purchasePrice);
-        showTicketsAndChanges(numberOfTickets, purchasePrice);
-
+        this.purchasePrice = purchasePrice;
+        int numberOfTickets = calculateNumberOfTickets();
+        generateTickets(numberOfTickets);
     }
 
-    private int calculateNumberOfTickets(int purchasePrice) {
+    private int calculateNumberOfTickets() {
         return (purchasePrice / PRICE_OF_LOTTO);
     }
 
-    private void showTicketsAndChanges(int numberOfTickets, int purchasePrice) {
-        System.out.print(numberOfTickets + "개를 구매했습니다. ");
-        int change = purchasePrice - (numberOfTickets * PRICE_OF_LOTTO);
+    public void showNumberOfTicketsAndChanges() {
+        System.out.print(tickets.size() + "개를 구매했습니다. ");
+        int change = purchasePrice - (tickets.size() * PRICE_OF_LOTTO);
         if (change > LOWER_BOUND_OF_CHANGE) {
             System.out.print("잔돈은 " + change + "원입니다.");
         }
         System.out.println();
+    }
+
+    private void generateTickets(int numberOfTickets) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        for (int i = 0; i < numberOfTickets; i++) {
+            tickets.add(new Lotto(numbers));
+        }
     }
 }
