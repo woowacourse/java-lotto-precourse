5559a8ae124b29e8af797913be50665dbd910667
kangmin <kangmin789@naver.com>
2019-04-08 20:37:37 +0900

    [ADD] GenerateRandomNumber, isNotOverlapLottoNumber 기능 구현
    GenerateRandomNumber 메소드는 난수를 생성하고 isNotOverlapLottoNumber메소드를 호출하고 난수를 리턴해주는 기능입니다.
    isNotOverlapLottoNumber 메소드는 생성된 난수가 겹치는 LottoNumber인지 판단하는 기능을 합니다.

--- src/main/java/domain/LottoGenerator.java
+++ src/main/java/domain/LottoGenerator.java
@@ -5,10 +5,15 @@ import java.util.List;
 
 public class LottoGenerator {
     private static final int PRIZE_OF_LOTTO = 1000;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int MIN_LOTTO_NUMBER = 1;
+
     private List<Lotto> lottoList;
+    private List<Integer> lottoNumbers;
 
     LottoGenerator() {
         lottoList = new ArrayList<>();
+        lottoNumbers = new ArrayList<>();
     }
 
     private void GenerateLottos(int purchaseAmount) {
@@ -18,8 +23,24 @@ public class LottoGenerator {
         }
     }
 
-    private void GenerateLotto(){
+    private void GenerateLotto() {
         
+    }
+
+    private int GenerateRandomNumber() {
+        boolean isNotValidLottoNumber = true;
+        int candidateLottoNumber =0;
+
+        while (isNotValidLottoNumber) {
+            candidateLottoNumber = (int) ((Math.random() * MAX_LOTTO_NUMBER) + MIN_LOTTO_NUMBER);
+            isNotValidLottoNumber = isNotOverlapLottoNumber(candidateLottoNumber);
+        }
+
+        return candidateLottoNumber;
+    }
+
+    private boolean isNotOverlapLottoNumber(int candidateLottoNumber) {
+        return lottoNumbers.contains(candidateLottoNumber) ? false : true;
     }
 
 
