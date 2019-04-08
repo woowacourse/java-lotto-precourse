8a1f817d6106c17d348875585392100b87ea8ebf
Soojin Roh <sooreal91@gmail.com>
2019-04-08 21:44:43 +0900

    입력 받은 값이 숫자인지 확인하는 기능 구현
    정규표현식을 이용하여 입력받은 값이 숫자인지 확인해 숫자면 true, 숫자가 아니면 false를 리턴하는 기능을 구현

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,15 +1,20 @@
 package domain;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class LottoGame {
 	static private Scanner sc = new Scanner(System.in);
+	static private final String NUMBER_CHECK_REGEX = "^[0-9]*$";
 	
 	private String inputMoney() {
 		System.out.println("구매금액을 입력해 주세요.");
-		
 		String money = sc.nextLine();
-		
 		return money;
 	}
+	
+	private boolean checkNumberOrNot(String money) {
+		return (Pattern.matches(NUMBER_CHECK_REGEX, money)) ? true : false;
+	}
+	
 }
