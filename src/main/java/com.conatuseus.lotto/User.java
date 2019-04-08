0a79ae78a898d4f679c8d47eb0c4abfa4ab88b2f
conatuseus <conatuseus@gmail.com>
2019-04-08 11:38:21 +0900

    구입금액 입력 유효성 검사 기능추가
    isMoneyValid 메소드를 통해 입력받은 금액 유효성 검사.
    숫자, 0이상, 1000단위의 금액이 올바른 입력
    
    Squashed commit of the following:
    
    commit 9833cc508c5e18ba24697356852d7d5b35cab5d6
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Mon Apr 8 11:37:47 2019 +0900
    
        Add var numbberOfLotto, getter,setter
    
    commit 7a538b0fabf44b27068e0d29db8e8936efe86855
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Mon Apr 8 11:35:10 2019 +0900
    
        Add isValid function for checking valid

--- src/main/java/com.conatuseus.lotto/User.java
+++ src/main/java/com.conatuseus.lotto/User.java
@@ -1,7 +1,6 @@
 package com.conatuseus.lotto;
 
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,9 +9,11 @@ import java.util.List;
 
 
 public class User {
-    private static final BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    private List<Lotto> lottoList=new LinkedList<>();
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final String MONEY_REGEX = "[0-9]+";
+    private List<Lotto> lottoList = new LinkedList<>();
     private int money;
+    private int numberOfLotto;
 
     public int getMoney() {
         return money;
@@ -20,12 +21,33 @@ public class User {
 
     private void setMoney(int money) {
         this.money = money;
+        this.setNumberOfLotto(money/1000);
+    }
+
+    public int getNumberOfLotto() {
+        return numberOfLotto;
+    }
+
+    private void setNumberOfLotto(int numberOfLotto) {
+        this.numberOfLotto = numberOfLotto;
     }
 
     public void inputMoney() throws IOException {
-        System.out.println("구입금액을 입력해 주세요.");
-        String input=br.readLine();
+        String input;
+        do {
+            System.out.println("구입금액을 입력해 주세요.(0이상 1000원 단위)");
+            input=br.readLine();
+        }while (!this.isMoneyValid(input));
+
         this.setMoney(Integer.parseInt(input));
+    }
+
+    private boolean isMoneyValid(String input) {
+        if(!input.matches(MONEY_REGEX)){
+            return false;
+        }
+        int tInputMoney=Integer.parseInt(input);
+        return (tInputMoney % 1000) == 0;
     }
 
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,9 +1,22 @@
 import com.conatuseus.lotto.User;
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class test {
     @Test
-    public void 사용자_로또_금액_입력(){
+    public void 사용자_로또_금액_입력_유효성_체크(){      // isMoneyValid 메소드 private로 변경 위해서 주석으로 변경
+//        User user=new User();
+//        assertThat(user.isMoneyValid("12345"),is(false));
+//        assertThat(user.isMoneyValid(" 1000"),is(false));
+//        assertThat(user.isMoneyValid("1000 "),is(false));
+//        assertThat(user.isMoneyValid("-1"),is(false));
+//        assertThat(user.isMoneyValid("1000"),is(true));
+//        assertThat(user.isMoneyValid("0"),is(true));
+//        assertThat(user.isMoneyValid("010000"),is(true));
+//        assertThat(user.isMoneyValid("abcd"),is(false));
+//        assertThat(user.isMoneyValid("a1000"),is(false));
+//        assertThat(user.isMoneyValid("008000"),is(true));
 
     }
 }
