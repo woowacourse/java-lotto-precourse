08db386c2a1965451e0a965a2c3a356d4b8005f1
yun <blpomspo@gmail.com>
2019-04-08 20:39:06 +0900

    구매금액만큼 Lotto 구매

--- src/main/java/domain/PlayLotto.java
+++ src/main/java/domain/PlayLotto.java
@@ -13,12 +13,34 @@ public class PlayLotto {
         return sc.nextInt();
     }
 
+    public List<Lotto> purchaseLotto() {
+        int LOTTO_PRICE = 1000;
+        int money = insertMoney();
+        int numberOfLotto; // 구입할 Lotto 개수
+        List<Integer> lottoNumber; // Lotto 번호 List
+        Lotto lotto;
+        List<Lotto> myLotto = new ArrayList<>(); // 구입한 내 Lotto List
+        while (money < 0) {
+            System.out.println("Lotto 구입금액은 0이상의 자연수입니다.");
+            money = insertMoney();
+        }
+        numberOfLotto = money / LOTTO_PRICE;
+        System.out.printf("%d개를 구매했습니다.\n", numberOfLotto);
+        for (int i = 0; i < numberOfLotto; i++) {
+            lottoNumber = GeneratingLottoNumber.generatingLottoNumber();
+            System.out.println(lottoNumber);
+            lotto = new Lotto(lottoNumber);
+            myLotto.add(lotto);
+        }
+        return myLotto;
+    }
+
 }
 
 class GeneratingLottoNumber {
     private static final int LOTTO_COUNT = 6;
     private static final int LOTTO_NUMBER_BEGIN = 1;
-    private static final int LOTTO_NUMBER_END = 1;
+    private static final int LOTTO_NUMBER_END = 45;
 
     static List<Integer> generatingLottoNumber() {
         List<Integer> lottoNumber = new ArrayList<>();
