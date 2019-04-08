9f8c64dc259f46128be11636653c9c91c7cdbd5b
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-04-08 23:12:30 +0900

    [추가] 당첨 통계를 출력

--- src/main/java/logic/OutputView.java
+++ src/main/java/logic/OutputView.java
@@ -4,6 +4,7 @@
 package logic;
 
 import domain.Lotto;
+import domain.LottoResult;
 
 import java.util.List;
 
@@ -13,5 +14,21 @@ public class OutputView {
 		for (Lotto lotto : lottoList) {
 			System.out.println(lotto);
 		}
+	}
+
+	public static void printResult(LottoResult lottoResult) {
+		printLottoStatResult(lottoResult);
+		printProfitRate(lottoResult);
+	}
+
+	public static void printLottoStatResult(LottoResult lottoResult) {
+		System.out.println("당첨 통계");
+		System.out.println("---------");
+		lottoResult.printStatResult();
+	}
+
+	public static void printProfitRate(LottoResult lottoResult) {
+		System.out.print("총 수익률은 ");
+		System.out.printf("%.3f입니다.", lottoResult.getProfitRate());
 	}
 }
