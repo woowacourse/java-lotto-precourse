1684234e01488280aea9b9f3338d94e7683c3edb
yun <blpomspo@gmail.com>
2019-04-08 20:31:32 +0900

    GeneratingLottoNumber class : Lotto number 생성하는 class

--- src/main/java/domain/PlayLotto.java
+++ src/main/java/domain/PlayLotto.java
@@ -1,5 +1,8 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class PlayLotto {
@@ -8,5 +11,21 @@ public class PlayLotto {
         System.out.println("구입금액을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         return sc.nextInt();
+    }
+
+}
+
+class GeneratingLottoNumber {
+    private static final int LOTTO_COUNT = 6;
+    private static final int LOTTO_NUMBER_BEGIN = 1;
+    private static final int LOTTO_NUMBER_END = 1;
+
+    static List<Integer> generatingLottoNumber() {
+        List<Integer> lottoNumber = new ArrayList<>();
+        for (int number = LOTTO_NUMBER_BEGIN; number < LOTTO_NUMBER_END+1; number++) {
+            lottoNumber.add(number);
+        }
+        Collections.shuffle(lottoNumber);
+        return lottoNumber.subList(0, LOTTO_COUNT);
     }
 }
