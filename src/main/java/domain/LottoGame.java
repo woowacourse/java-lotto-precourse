4c56845d7b2f5ac64f65325333facf32dacd7b1c
usang <usang>
2019-04-09 15:12:41 +0900

    살 수 있는 로또 개수만큼 로또를 생성하는 기능 구현
    createLotto() 메소드에서 살수 있는 로또 개수를 인자로 받아, getRandomList()를 통해 로또를 생성합니다.
    getRandomList() 메소드는 1~45까지 숫자중 랜덤으로 6개를 선택해 리스트를 반환합니다.

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,13 +1,19 @@
 package domain;
 
-import java.awt.peer.LabelPeer;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class LottoGame {
 
     private static final String MESSAGE_INPUT_MONEY = "구매금액을 입력해 주세요.";
     private static final String MESSAGE_GET_LOTTO = "개를 구매했습니다.";
     private static final int LOTTO_PRICE = 1000;
+    private static final int MAX_LOTTO_NUMBER= 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    private List<Lotto> purchasedLottoList;
 
     private int inputMoney() {
         Scanner scan = new Scanner(System.in);
@@ -24,6 +30,22 @@ public class LottoGame {
         int count = money / LOTTO_PRICE;
         System.out.println(count+MESSAGE_GET_LOTTO);
         return count;
+    }
+
+    private void createLotto(int countLotto) {
+        for(int i = 0; i < countLotto; i++) {
+            Lotto lotto = new Lotto(getRandomList());
+            purchasedLottoList.add(lotto);
+        }
+    }
+
+    private List<Integer> getRandomList(){
+        List<Integer> randomList = new ArrayList<>();
+        Random random = new Random();
+        for(int i = 0; i < LOTTO_NUMBER_COUNT; i++) {
+            randomList.add(random.nextInt(MAX_LOTTO_NUMBER) + 1);
+        }
+        return randomList;
     }
 
 
