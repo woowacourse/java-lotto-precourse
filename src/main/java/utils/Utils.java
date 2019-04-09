e440daa6f823297f4afe39d1d559f439d48733a8
yun <blpomspo@gmail.com>
2019-04-10 04:30:44 +0900

    Utils 주석 추가

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,13 +1,27 @@
+/*
+ * @Utils.java     0.1 2019-04-10
+ * Copyright(c) 2019 LeeYunSeop All rights reserved.
+ * */
+
 package utils;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 다른 class와 성격이 다른 기능을 여기서 구현
+ *
+ * @author yun
+ * @version 0.1
+ */
 public class Utils {
 
+    /**
+     * string[]를 List<Integer>로 변환
+     * */
     public static List<Integer> convertString2Int(String[] rawWinningNumber) {
         List<Integer> intList = new ArrayList<>();
-        for (String str: rawWinningNumber) {
+        for (String str : rawWinningNumber) {
             intList.add(Integer.parseInt(str));
         }
         return intList;
