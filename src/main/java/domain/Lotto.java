20ef3cd50093572379a9944b638eeaa91ef87f48
NotYet <wiseoldster@gmail.com>
2019-04-09 17:28:24 +0900

    클래스 변수 추가, main(shell) 함수 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,16 +1,30 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
-    private final List<Integer> numbers;
+	private final int PRICE = 1000;
+	private final int MAX_BALL_NUM = 45;
+	private final int NUM_BALL = 6;
+	private static List<Lotto> myLottos = new ArrayList<>();
+	private static WinningLotto winningLotto;
+	private static HashMap<Rank, Integer> result = new HashMap<Rank, Integer>(Rank.values().length);
 
-    public Lotto(List<Integer> numbers) {
-        this.numbers = numbers;
-    }
+	private final List<Integer> numbers;
 
-    // 추가 기능 구현
+	public Lotto(List<Integer> numbers) {
+		this.numbers = numbers;
+	}
+
+	// 추가 기능 구현
+
+	public static void main(String[] args) {
+		// 객체로 불러 실행해야한다면 shell()로 대체한다.
+
+	}
 }
