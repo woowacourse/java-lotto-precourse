94b2606b609afff9b185c14c6cf72c8442a4e5db
KIMJUNGMIN <testprogramms9104@gmail.com>
2019-04-10 02:12:00 +0900

    유저에게 구입금액을 입력받을 수 있는 method 정의.

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,11 +1,18 @@
 package domain;
 
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
+    private static final int PRICE_OF_ONE_LOTTO = 1000;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int STARTING_INDEX_OF_LOTTO_NUMBERS = 0;
+    private static final int ENDING_INDEX_OF_LOTTO_NUMBERS = 5;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,4 +20,23 @@ public class Lotto {
     }
 
     // 추가 기능 구현
+    public static String askAndReceiveInput(String sentence) {
+        System.out.println(sentence);
+        Scanner reader = new Scanner(System.in);
+        String userInput = reader.nextLine();
+        return userInput;
+    }
+
+    public static String askUserMoneyAmount() {
+        boolean isUserInputRight = false;
+        String userInputMoneyAmount = "ERROR:askUSerMoneyAmount()";
+        while (!isUserInputRight) {
+            userInputMoneyAmount = askAndReceiveInput("구입금액을 입력해 주세요.");
+            userInputMoneyAmount = userInputMoneyAmount.replaceAll("\\s+","");
+            isUserInputRight = checkUserInput(userInputMoneyAmount);
+        }
+        return userInputMoneyAmount;
+    }
+
+
 }
