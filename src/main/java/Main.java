5a872ea65b80548a3a399fae726b3974c0d95e1a
SunHo Van <vsh123@naver.com>
2019-04-09 23:57:55 +0900

    feat : Main.java 구현 1. 게임 실행을 위한 main 클래스 생성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,21 @@
 /*
- * @(#)Main.java        0.1 2019/04/08
+ * @(#)Main.java        0.1 2019/04/09
  *
  *
  */
+
+import domain.LottoGame;
 
 /**
  * Lotto 실행을 위한 Main클래스 입니다.
  *
  * @author 반선호
- * @version 0.1 2019년 4월 08일
+ * @version 0.1 2019년 4월 09일
  */
 public class Main {
+    public static void main(String[] args) {
+        LottoGame game = new LottoGame();
+
+        game.start();
+    }
 }
