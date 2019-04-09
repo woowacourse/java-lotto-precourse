bc15e0d7d1458a2ac723816b0af7e53c7dc6273e
yun <blpomspo@gmail.com>
2019-04-10 03:12:12 +0900

    주석 추가

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,7 +1,13 @@
+/*
+ * @Rank.java     0.1 2019-04-10
+ * */
+
 package domain;
 
 /**
  * 로또 등수를 의미하는 enum
+ *
+ * @version 0.1
  */
 public enum Rank {
     MISS(0, 0),
@@ -11,10 +17,10 @@ public enum Rank {
     SECOND(5, 30_000_000), // 2등
     FIRST(6, 2_000_000_000); // 1등
 
-    private static final int WINNING_MIN_COUNT = 3;
+    private static final int WINNING_MIN_COUNT = 3; // 최소 3개를 맞춰야 당첨 금액이 존재
 
-    private int countOfMatch;
-    private int winningMoney;
+    private int countOfMatch; // 맞춘 갯수
+    private int winningMoney; // 당첨 금액
 
     private Rank(int countOfMatch, int winningMoney) {
         this.countOfMatch = countOfMatch;
@@ -29,8 +35,17 @@ public enum Rank {
         return winningMoney;
     }
 
-    public int getWinningMinCount() {return WINNING_MIN_COUNT; }
+    public int getWinningMinCount() {
+        return WINNING_MIN_COUNT;
+    }
 
+    /**
+     * 맞춘 갯수와 보너스 볼 일치 여부로 Lotto 당첨 등급을 결정
+     *
+     * @param countOfMatch Lotto 번호와 당첨 번호 일치한 갯수
+     * @param matchBonus   Lotto 번호와 보너스 볼 일치 여부
+     * @return 당첨 등급
+     */
     public static Rank valueOf(int countOfMatch, boolean matchBonus) {
         if (countOfMatch < WINNING_MIN_COUNT) {
             return MISS;
