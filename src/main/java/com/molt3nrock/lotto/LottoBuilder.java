0cc0fa5b37924385eecb2a435f557fe08b61cbc6
molt3nrock <molt3nrock@github.com>
2019-04-09 15:54:35 +0900

    LottoBuilder 클래스: 구입한 돈으로 Lotto 리스트 생성

--- src/main/java/com/molt3nrock/lotto/LottoBuilder.java
+++ src/main/java/com/molt3nrock/lotto/LottoBuilder.java
@@ -6,12 +6,34 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-class LottoBuilder {
+class LottoBuilder implements MoneyLottoBuilder, FinalLottoBuilder {
 
     private static final int MINIMUM_NUMBER_OF_LOTTO = 1;
     private static final int MAXIMUM_NUMBER_OF_LOTTO = 45;
     private static final int PRICE_PER_LOTTO = 1000;
     private static final int COUNT_OF_NUMBERS_PER_LOTTO = 6;
+    private int money = 0;
+
+    static MoneyLottoBuilder create() {
+        return new LottoBuilder();
+    }
+
+    @Override
+    public FinalLottoBuilder withMoney(int money) {
+        if (money >= PRICE_PER_LOTTO) {
+            this.money = money;
+            return this;
+        }
+        throw new IllegalArgumentException("로또를 구입하기에 돈이 충분하지 않습니다.");
+    }
+
+    @Override
+    public List<Lotto> build() {
+        return IntStream
+            .range(0, money / PRICE_PER_LOTTO)
+            .mapToObj(i -> new Lotto(generateNumbers()))
+            .collect(Collectors.toList());
+    }
 
     private static List<Integer> generateNumbers() {
         ArrayList<Integer> integers = IntStream
@@ -22,4 +44,14 @@ class LottoBuilder {
         Collections.shuffle(integers);
         return integers;
     }
+}
+
+interface MoneyLottoBuilder {
+
+    FinalLottoBuilder withMoney(int money);
+}
+
+interface FinalLottoBuilder {
+
+    List<Lotto> build();
 }
---
+++ src/test/java/com/molt3nrock/lotto/LottoBuilderTest.java
@@ -0,0 +1,37 @@
+package com.molt3nrock.lotto;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import org.junit.Test;
+
+public class LottoBuilderTest {
+
+    @Test
+    public void build() {
+        List<Lotto> lottoList;
+        lottoList = LottoBuilder.create().withMoney(1000).build();
+        assertEquals(1, lottoList.size());
+
+        lottoList = LottoBuilder.create().withMoney(99000).build();
+        assertEquals(99, lottoList.size());
+    }
+
+    @Test
+    public void buildIllegalNotEnough() {
+        try{
+            LottoBuilder.create().withMoney(999).build();
+        } catch (IllegalArgumentException e) {
+            assertEquals("로또를 구입하기에 돈이 충분하지 않습니다.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void buildIllegalNegative() {
+        try{
+            LottoBuilder.create().withMoney(-1).build();
+        } catch (IllegalArgumentException e) {
+            assertEquals("로또를 구입하기에 돈이 충분하지 않습니다.", e.getMessage());
+        }
+    }
+}
