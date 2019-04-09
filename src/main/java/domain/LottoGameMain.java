fb0f64fbaea4aaef363f0a2ca0d00a76a569183a
moonsung <visitant5@nate.com>
2019-04-09 11:12:12 +0900

    update  variable name

--- src/main/java/domain/LottoGameMain.java
+++ src/main/java/domain/LottoGameMain.java
@@ -2,7 +2,7 @@ package domain;
 
 public class LottoGameMain {
     public static void main(String[] args) {
-        LottoGameManager lottostore = new LottoGameManager();
-        lottostore.lottoPurchase();
+        LottoGameManager lottogame = new LottoGameManager();
+        lottogame.lottoPurchase();
     }
 }
