ee8100ca9ed7f323de28be3a627a3e9f6f433781
youngbae10000 <youngbae10000@gmail.com>
2019-04-09 12:39:48 +0900

    Add Lotto 로또생성[Random 수 1~45까지수만 발생]

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -6,11 +6,31 @@ import java.util.List;
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
+	
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
     }
+    
+    public void createLottoNumber() {
+    	
+    	int [] visit = new int[46];
+    	int lottoNumber;
+    	int loopCount = 0;
+    	
+    	while (loopCount < 6) {
+    		
+    		lottoNumber = (int) (Math.random()*100) % 46;
+    		
+			if(lottoNumber == 0 || visit[lottoNumber] == 1)
+				continue;
+			
+			loopCount++;
+			visit[lottoNumber] = 1;
+			this.numbers.add(lottoNumber);
+		}
+    }
 
-    // 추가 기능 구현
+    
 }
