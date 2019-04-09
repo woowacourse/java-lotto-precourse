b0596cd209f67933de47961707e6fbed89ce4199
molt3nrock <molt3nrock@github.com>
2019-04-10 01:13:48 +0900

    WinningLottoTest 클래스: matchNoRank 테스트 리팩토링
    프로그래밍 요구사항에 맞도록 수정

--- src/test/java/com/molt3nrock/lotto/WinningLottoTest.java
+++ src/test/java/com/molt3nrock/lotto/WinningLottoTest.java
@@ -3,6 +3,7 @@ package com.molt3nrock.lotto;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -47,13 +48,11 @@ public class WinningLottoTest {
 
     @Test
     public void matchNoRank() {
-        Rank rankMissMatchDouble = winningLotto.match(new Lotto(Arrays.asList(1, 2, 8, 9, 10, 11)));
-        assertEquals(Rank.MISS, rankMissMatchDouble);
-
-        Rank rankMissMatchSingle = winningLotto.match(new Lotto(Arrays.asList(1, 8, 9, 10, 11, 12)));
-        assertEquals(Rank.MISS, rankMissMatchSingle);
-
-        Rank rankMissNone = winningLotto.match(new Lotto(Arrays.asList(8, 9, 10, 11, 12, 13)));
-        assertEquals(Rank.MISS, rankMissNone);
+        Lotto noMatchLotto = new Lotto(Arrays.asList(8, 9, 10, 11, 12, 13));
+        Lotto singleMatchLotto = new Lotto(Arrays.asList(1, 8, 9, 10, 11, 12));
+        Lotto doubleMatchLotto = new Lotto(Arrays.asList(1, 2, 8, 9, 10, 11));
+        Boolean actual = Stream.of(noMatchLotto, singleMatchLotto, doubleMatchLotto)
+            .allMatch(lotto -> winningLotto.match(lotto).equals(Rank.MISS));
+        assertEquals(true, actual);
     }
 }
