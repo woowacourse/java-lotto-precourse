bdf6c9e48b4bcc07e7148f871b00ee1420916e68
molt3nrock <molt3nrock@github.com>
2019-04-09 16:35:07 +0900

    Game 클래스: 로또 규칙에 맞는 당첨번호 입력받기

--- src/main/java/com/molt3nrock/lotto/Game.java
+++ src/main/java/com/molt3nrock/lotto/Game.java
@@ -1,5 +1,9 @@
 package com.molt3nrock.lotto;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Game {
 
     private int parseInput(String line) throws IllegalArgumentException {
@@ -10,5 +14,28 @@ public class Game {
             throw new IllegalArgumentException("올바른 돈이 아닙니다.");
         }
         return money;
+    }
+
+
+    private List<Integer> parseInputAsWinningNumbers(String line) throws IllegalArgumentException {
+        final int COUNT_OF_NORMAL_WINNING_NUMBERS = 6;
+        final int MINIMUM_NUMBER_OF_LOTTO = 1;
+        final int MAXIMUM_NUMBER_OF_LOTTO = 45;
+        List<Integer> numbers =  Arrays.stream(line.split(","))
+            .map(s -> parseStringAsInteger(s.trim()))
+            .filter( i -> i >= MINIMUM_NUMBER_OF_LOTTO && i <= MAXIMUM_NUMBER_OF_LOTTO)
+            .collect(Collectors.toList());
+            if (numbers.stream().distinct().count() == COUNT_OF_NORMAL_WINNING_NUMBERS) {
+               return numbers;
+            }
+            throw new IllegalArgumentException("올바른 당첨번호 입력이 아닙니다.");
+    }
+
+    private Integer parseStringAsInteger(String input) throws IllegalArgumentException {
+       try  {
+           return Integer.parseInt(input);
+       } catch (NumberFormatException e) {
+           throw new IllegalArgumentException("올바른 번호가 아닙니다.");
+       }
     }
 }
