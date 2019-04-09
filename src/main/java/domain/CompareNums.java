8df76e41a5b03a183b6778e81beb7f374915a67a
qkrjh0904 <qkrjh0904@gamil.com>
2019-04-09 22:41:07 +0900

    당첨 번호, 보너스 번호 중복 및 예외 처리

--- src/main/java/domain/CompareNums.java
+++ src/main/java/domain/CompareNums.java
@@ -3,11 +3,14 @@ package domain;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class CompareNums {
 
+	private final int LOTTO_NUM_START = 1;
+	private final int LOTTO_NUM_END = 45;
 	private final int LOTTO_NUM_SELECT = 6;
 	private final int LOTTO_WINNING_CASE = 6;
 	private final int FIRST = 0;
@@ -18,7 +21,7 @@ public class CompareNums {
 	private final int MISS = 5;
 
 	private Lotto[] userLotto;
-	private List<Integer> winNums_int; // 당첨번호
+	private List<Integer> winNums_int = new ArrayList<Integer>(LOTTO_NUM_SELECT);
 
 	static Scanner in = new Scanner(System.in);
 
@@ -26,31 +29,108 @@ public class CompareNums {
 		this.userLotto = userLotto;
 	}
 
-	public boolean isBonusNumberRight(int bonusBall) {
-		if (bonusBall < 1 || bonusBall > 45) {
-			System.out.println("유효하지 않은 보너스 숫자 입니다. 다시 입력해 주세요.");
-			return false;
+	private boolean outOfRangeBonusBall(int bonusBall) {
+		if (bonusBall < LOTTO_NUM_START || bonusBall > LOTTO_NUM_END) {
+			System.out.println("범위를 벗어난 보너스 숫자 입니다. 다시 입력해 주세요.");
+			return true;
 		}
-		return true;
+		return false;
 	}
 
-	public int bonusNumber() {
+	private boolean bonusNumOverlap(int bonusBall) {
+		if (winNums_int.contains(bonusBall)) {
+			System.out.println("보너스 번호가 당첨번호와 중복됩니다.");
+			return true;
+		}
+		return false;
+	}
+
+	private int bonusNumber() {
 		int bonusBall;
 		do {
 			System.out.println("보너스 볼을 입력해 주세요.");
 			bonusBall = in.nextInt();
-		} while (!isBonusNumberRight(bonusBall));
+		} while (outOfRangeBonusBall(bonusBall) || bonusNumOverlap(bonusBall));
 		return bonusBall;
 	}
 
-	public List<Integer> winningNumber() {
-		System.out.println("지난 주 당첨 번호를 입력해 주세요.( 6개, 쉼표(,)로 구분 )");
-		List<String> winNums_string = new ArrayList<String>(LOTTO_NUM_SELECT);
-		String winNum = in.nextLine();
-		winNums_string = Arrays.asList(winNum.split(","));
-		winNums_int = new ArrayList<Integer>(LOTTO_NUM_SELECT);
-		for (String s : winNums_string)
+	private boolean isSizeSix() {
+		if (winNums_int.size() != LOTTO_NUM_SELECT) {
+			System.out.println("당첨 번호는 6개입니다. 다시 입력해 주세요.");
+			winNums_int.clear();
+			return true;
+		}
+		return false;
+	}
+
+	private boolean rangeCheck(int num) {
+		if (num < LOTTO_NUM_START || num > LOTTO_NUM_END) {
+			System.out.println("범위를 벗어난 당첨번호 입니다. 다시 입력해 주세요.");
+			winNums_int.clear();
+			return true;
+		}
+		return false;
+	}
+
+	private boolean overlap(int num1, int num2) {
+		if (num1 == num2) {
+			System.out.println("중복된 번호가 있습니다. 다시 입력해 주세요.");
+			winNums_int.clear();
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isOverlap() {
+		boolean state = false;
+		for (int i = 0; i < winNums_int.size() - 1; ++i) {
+			state = state || overlap(winNums_int.get(i), winNums_int.get(i + 1));
+		}
+		return state;
+	}
+
+	private boolean outOfRange() {
+		boolean state = false;
+		for (int i = 0; i < winNums_int.size(); ++i) {
+			state = state || rangeCheck(winNums_int.get(i));
+		}
+		return state;
+	}
+
+	private void stringToInteger(List<String> winNums_string) {
+		for (String s : winNums_string) {
 			winNums_int.add(Integer.parseInt(s));
+		}
+	}
+
+	private boolean errorCatch(List<String> winNums_string) {
+		try {
+			stringToInteger(winNums_string);
+		} catch (NumberFormatException e) {
+			System.out.println("유효하지 않은 당첨 번호 입니다. 다시 입력해 주세요.");
+			winNums_int.clear();
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isWinningNumberRight(String winNum) {
+		List<String> winNums_string = new ArrayList<String>(LOTTO_NUM_SELECT);
+		winNums_string = Arrays.asList(winNum.split(","));
+		if (errorCatch(winNums_string))
+			return false;
+		if (isSizeSix())
+			return false;
+		return true;
+	}
+
+	private List<Integer> winningNumber() {
+		String winNum;
+		do {
+			System.out.println("지난 주 당첨 번호를 입력해 주세요.( 6개, 쉼표(,)로 구분 )");
+			winNum = in.nextLine();
+		} while (!isWinningNumberRight(winNum) || outOfRange() || isOverlap());
+		System.out.println(winNums_int);
 		return winNums_int;
 	}
 
