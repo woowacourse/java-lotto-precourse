1b27e0a70d539837297a02c99a62648a2d3c0e51
usang <usang>
2019-04-09 14:19:55 +0900

    구매금액으로 살 수 있는 로또의 개수 구하는 기능 구현

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,10 +1,13 @@
 package domain;
 
+import java.awt.peer.LabelPeer;
 import java.util.Scanner;
 
 public class LottoGame {
 
-    private static final String MESSAGE_INPUT_MONEY = "구매금액 입력";
+    private static final String MESSAGE_INPUT_MONEY = "구매금액을 입력해 주세요.";
+    private static final String MESSAGE_GET_LOTTO = "개를 구매했습니다.";
+    private static final int LOTTO_PRICE = 1000;
 
     private int inputMoney() {
         Scanner scan = new Scanner(System.in);
@@ -15,6 +18,12 @@ public class LottoGame {
         }
         while (!Validation.isValidInputMoney(input));
         return Integer.parseInt(input);
+    }
+
+    private int getCountLotto(int money){
+        int count = money / LOTTO_PRICE;
+        System.out.println(count+MESSAGE_GET_LOTTO);
+        return count;
     }
 
 
