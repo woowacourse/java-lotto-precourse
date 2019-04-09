f3d67eef56f6c5f2068253b059aa2ed604f492c1
Yookibaek <psykibaek@gmail.com>
2019-04-09 14:00:22 +0900

    [ADD] [서브로직 3-2] 당첨번호 입력값 6개 모두 1~45로 변환가능한지 확인하는 로직 구현

--- src/main/java/domain/Validator.java
+++ src/main/java/domain/Validator.java
@@ -7,6 +7,7 @@ public class Validator {
     static final String WARNING_WHEN_LOTTO_INPUT_COUNT_OVER = "WARNING: 입력이 너무 깁니다. " + MAX_POSSIBLE_LOTTO_INPUT_LENGTH + "자리 아래로 다시 입력해주세요.";
     static final String WARNING_WHEN_LOTTO_INPUT_SMALL = "WARNING: 금액이 너무 적습니다. 1장의 금액은 " + GameSetting.PRICE_PER_1LOTTO + "원 입니다. 다시 입력해주세요.";
     static final String WARNING_WHEN_LOTTO_NUMBER_COUNT_NOT_MATCHING = "WARNING: 로또넘버는 " + GameSetting.LOTTO_NORMAL_NUMBER_COUNT + "개 이어야 합니다. 다시 입력해주세요.";
+    static final String WARNING_WHEN_LOTTO_NUMBER_NOT_IN_RANGE = "WARNING:" + GameSetting.MIN_LOTTO_NUMBER + "~" + GameSetting.MAX_LOTTO_NUMBER + " 의 숫자들만 가능합니다. 다시 입력해주세요.";
 
     public static boolean checkInputLottoMoney(String lottoCountInput) {
         if (!checkInputLottoMoneyLengthIsTooLong(lottoCountInput) ||
@@ -54,6 +55,37 @@ public class Validator {
             System.out.println(WARNING_WHEN_LOTTO_NUMBER_COUNT_NOT_MATCHING);
             return false;
         }
+        return true;
+    }
+
+    public static boolean checkLottoNumbersInputHasNoInvalidValue(String[] numbers) {
+        boolean invalidValueNotFound = true;
+        for (int i = 0; (i < numbers.length) && (invalidValueNotFound); i++) {
+            invalidValueNotFound = checkIsLottoNumberValid(numbers[i]);
+        }
+
+        return invalidValueNotFound;
+    }
+
+    //
+    public static boolean checkIsLottoNumberValid(String inputLottoNumber) {
+        if (!checkIsInteger(inputLottoNumber)) {
+            return false;
+        }
+        int inputNumberInt = Integer.parseInt(inputLottoNumber);
+        if (!checkIsLottoNumberInRange(inputNumberInt)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkIsLottoNumberInRange(int lottoInput) {
+        if ((lottoInput < GameSetting.MIN_LOTTO_NUMBER) ||
+                (lottoInput > GameSetting.MAX_LOTTO_NUMBER)) {
+            System.out.println(WARNING_WHEN_LOTTO_NUMBER_NOT_IN_RANGE);
+            return false;
+        }
+
         return true;
     }
 }
