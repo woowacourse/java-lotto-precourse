2357aac8d5ab79e96e9631abb7d76a13afa91450
jinwook <jin942002@naver.com>
2019-04-09 21:52:56 +0900

    구입금액을 매개변수로 하여 로또들을 생성하고 출력하는 함수 구현.
    함수이름: generateAndShowLottos() in Game class.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,11 +1,13 @@
 import domain.Lotto;
 import domain.UserInput;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
     static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     static final String NOT_PURCHASE_AMOUNT_ERROR = "1000 이상의 숫자를 입력해 주세요.";
+    static final String YOU_BOUGHT_IT = "개를 구매했습니다.";
 
     public static int getPurchaseAmount() {
         System.out.println(ASK_PURCHASE_AMOUNT);
@@ -24,6 +26,21 @@ public class Game {
         }
         System.out.println(NOT_PURCHASE_AMOUNT_ERROR);
         return false;
+    }
+
+    public static List<Lotto> generateAndShowLottos(int purchaseAmount) {
+        List<Lotto> lottos = Lotto.generateLottos(purchaseAmount);
+        showLottos(lottos);
+        return lottos;
+    }
+
+    private static void showLottos(List<Lotto> lottos) {
+        System.out.println();
+        System.out.println(lottos.size() + YOU_BOUGHT_IT);
+        for (Lotto lotto : lottos
+             ) {
+            System.out.println(lotto);
+        }
     }
 
 }
