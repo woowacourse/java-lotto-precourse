f4702510a7692a90b5942e6a3cd37ac658f21e03
molt3nrock <molt3nrock@github.com>
2019-04-10 00:52:27 +0900

    WinningLotto 클래스: 클래스와 클래스의 메쏘드 접근제한자 수정

--- src/main/java/com/molt3nrock/lotto/WinningLotto.java
+++ src/main/java/com/molt3nrock/lotto/WinningLotto.java
@@ -6,17 +6,17 @@ import java.util.stream.Stream;
 /**
  * 당첨 번호를 담당하는 객체
  */
-public class WinningLotto {
+class WinningLotto {
 
     private final Lotto lotto;
     private final int bonusNo;
 
-    public WinningLotto(Lotto lotto, int bonusNo) {
+    WinningLotto(Lotto lotto, int bonusNo) {
         this.lotto = lotto;
         this.bonusNo = bonusNo;
     }
 
-    public Rank match(Lotto userLotto) {
+    Rank match(Lotto userLotto) {
         List<Integer> winningNumbers = this.lotto.getNumbers();
         List<Integer> userNumbers = userLotto.getNumbers();
         int totalCount = winningNumbers.size() + userNumbers.size();
