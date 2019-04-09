3c7837caeebc28a245dd32f94267ccbeaa8d20e7
mrkwon <erased11@gmail.com>
2019-04-09 15:58:07 +0900

    Java Code Convention, Access Modifier 수정

--- src/main/java/com/kwonmc/lotto/Game.java
+++ src/main/java/com/kwonmc/lotto/Game.java
@@ -19,32 +19,32 @@ import java.util.ArrayList;
  * @version 0.0.0
  * @author kwonmc
  */
-public class Game {
+class Game {
     private int purchaseAmount; // 구매금액
     private ArrayList<Lotto> myLottoList; // 구매한 로또 번호들
     private WinningLotto winningLotto; // 지난주 당첨 번호와 보너스 번호
 
-    public void setPurchaseAmount(int purchaseAmount) {
+    void setPurchaseAmount(int purchaseAmount) {
         this.purchaseAmount = purchaseAmount;
     }
 
-    public int getPurchaseAmount() {
+    int getPurchaseAmount() {
         return purchaseAmount;
     }
 
-    public void setMyLottoList(ArrayList<Lotto> myLottoList) {
+    void setMyLottoList(ArrayList<Lotto> myLottoList) {
         this.myLottoList = myLottoList;
     }
 
-    public ArrayList<Lotto> getMyLottoList() {
+    ArrayList<Lotto> getMyLottoList() {
         return myLottoList;
     }
 
-    public void setWinningLotto(WinningLotto winningLotto) {
+    void setWinningLotto(WinningLotto winningLotto) {
         this.winningLotto = winningLotto;
     }
 
-    public WinningLotto getWinningLotto() {
+    WinningLotto getWinningLotto() {
         return winningLotto;
     }
 }
--- src/main/java/com/kwonmc/lotto/Message.java
+++ src/main/java/com/kwonmc/lotto/Message.java
@@ -17,19 +17,19 @@ package com.kwonmc.lotto;
  * @version 0.0.0
  * @author kwonmc
  */
-public class Message {
+class Message {
     // GameRunner 문자열들
-    public static String PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    public static String LOTTO_INFO = "개를 구매했습니다.";
-    public static String LAST_WEEK_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
-    public static String BONUS_NO = "보너스 볼을 입력해 주세요.";
-    public static String RESULT_DESCRIPTION= "%s개 일치%s(%s원)- %s개";
-    public static String BONUS_TRUE = ", 보너스 볼 일치";
-    public static String BONUS_FALSE = "";
-    public static String LOTTO_MATCH_RESULT_HEADER = "당첨통계";
-    public static String SEPARATION_LINE = "---------";
-    public static String RESULT_YIELD = "총 수익률은 %.3f입니다.";
+    static String PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    static String LOTTO_INFO = "개를 구매했습니다.";
+    static String LAST_WEEK_NUMBER = "지난 주 당첨 번호를 입력해 주세요.";
+    static String BONUS_NO = "보너스 볼을 입력해 주세요.";
+    static String RESULT_DESCRIPTION= "%s개 일치%s(%s원)- %s개";
+    static String BONUS_TRUE = ", 보너스 볼 일치";
+    static String BONUS_FALSE = "";
+    static String LOTTO_MATCH_RESULT_HEADER = "당첨통계";
+    static String SEPARATION_LINE = "---------";
+    static String RESULT_YIELD = "총 수익률은 %.3f입니다.";
 
     // Rank 문자열들
-    public static String EXCEPTION_MESSAGE = "는 유효하지 않은 값입니다.";
+    static String EXCEPTION_MESSAGE = "는 유효하지 않은 값입니다.";
 }
