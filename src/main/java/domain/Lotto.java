8d2c5e79e77c1c73e687baeeb3b9934c146897b1
NotYet <wiseoldster@gmail.com>
2019-04-09 18:52:25 +0900

    기능 수정 : 입력 기능 10 라인 이하로 수정

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,9 +11,10 @@ import java.util.List;
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
-	private final int PRICE = 1000;
-	private final int MAX_BALL_NUM = 45;
-	private final int NUM_BALL = 6;
+	private final static int PRICE = 1000;
+	private final static int MAX_BALL_NUM = 45;
+	private final static int NUM_BALL = 6;
+	private static int Num_Lotto;
 	private static List<Lotto> myLottos = new ArrayList<>();
 	private static WinningLotto winningLotto;
 	private static HashMap<Rank, Integer> result = new HashMap<Rank, Integer>(Rank.values().length);
@@ -24,22 +26,24 @@ public class Lotto {
 	}
 
 	// 추가 기능 구현
-	private static int inputCost() {
+	private static void inputCost() throws NumberFormatException, IOException {
 		System.out.println("구입금액을 입력해주세요. ");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int ret = 0;
+		int ret = Integer.parseInt(br.readLine());
+		Num_Lotto = ret / PRICE;
+	}
+	
+	private static void recurInputCost() {
 		try {
-			ret = Integer.parseInt(br.readLine());
+			inputCost();
 		} catch (Exception e) {
 			System.out.println("유효하지 않은 입력입니다. 다시 입력해주세요.");
-			inputCost();
+			recurInputCost();
 		}
-		return ret;
 	}
 
-	public static void main(String[] args) {
-		// 객체로 불러 실행해야한다면 shell()로 대체한다.
-		int cost = inputCost();
+	public static void main(String[] args) { // 객체로 불러 실행해야한다면 shell()로 대체한다.
+		recurInputCost();
 
 	}
 }
