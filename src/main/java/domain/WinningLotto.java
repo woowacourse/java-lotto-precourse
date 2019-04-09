0b97daee795115142d7df3148e479d5f2881714f
bactoria <bactoria@gmail.com>
2019-04-09 20:56:08 +0900

    FEAT :: 당첨 로또 예외처리 추가 - 보너스 번호가 1 ~ 45 사이 값이 아닌 경우 - 보너스 번호가 당첨 번호에 존재하는 경우

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -1,5 +1,8 @@
 package domain;
 
+import static domain.LottoConst.LOTTO_MAX_NUMBER;
+import static domain.LottoConst.LOTTO_MIN_NUMBER;
+
 /**
  * 당첨 번호를 담당하는 객체
  */
@@ -8,8 +11,23 @@ public class WinningLotto {
     private final int bonusNo;
 
     public WinningLotto(Lotto lotto, int bonusNo) {
+        validateDuplication(lotto, bonusNo);
+        validateRange(bonusNo);
+
         this.lotto = lotto;
         this.bonusNo = bonusNo;
+    }
+
+    private void validateDuplication(Lotto lotto, int bonusNo) {
+        if (lotto.hasNumber(bonusNo)) {
+            throw new IllegalArgumentException("보너스 번호가 당첨 번호에 포함될 수 없습니다.");
+        }
+    }
+
+    private void validateRange(int bonusNo) {
+        if (bonusNo < LOTTO_MIN_NUMBER || bonusNo > LOTTO_MAX_NUMBER) {
+            throw new IllegalArgumentException("유효하지 않은 보너스 번호 입니다.");
+        }
     }
 
     public Rank match(Lotto userLotto) {
