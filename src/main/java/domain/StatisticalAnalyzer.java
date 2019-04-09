2dc8991d232d8a7aaaf5e0d9daefa36cb5936f8a
deocksoo <dusol92@snu.ac.kr>
2019-04-09 17:31:24 +0900

    [ADD] StatisticalAnalyzer의 analyzeEarningsOf 메소드를 구현합니다
    랭크별 당첨 복권의 개수를 집계하기 위해 rankCounts를 Hashmap을 사용해
    구현합니다. Hashmap을 사용하면 원하는 rank들을 선별하여 집계할 수 있습니다.

--- src/main/java/domain/StatisticalAnalyzer.java
+++ src/main/java/domain/StatisticalAnalyzer.java
@@ -4,6 +4,27 @@ package domain;
 import java.util.HashMap;
 
 public class StatisticalAnalyzer {
+    public HashMap<Rank, Integer> makeRankCountsTableOf(HashMap<Lotto, Rank> lotteryResults) {
+        return null;
+    }
+
+    public double calculateEarningRateBy(HashMap<Rank, Integer> rankCounts) {
+        return 0;
+    }
+
+    public void show(HashMap<Rank, Integer> rankCounts) {
+
+    }
+
+    public void show(double earningRate) {
+
+    }
+
     public void analyzeEarningsOf(HashMap<Lotto, Rank> lotteryResults) {
+        HashMap<Rank, Integer> rankCounts = makeRankCountsTableOf(lotteryResults);
+        double earningRate = calculateEarningRateBy(rankCounts);
+
+        show(rankCounts);
+        show(earningRate);
     }
 }
