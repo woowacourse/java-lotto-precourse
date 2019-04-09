78aea592ac05b39eee7f61a3077ef3e5b2461249
483759 <483759@naver.com>
2019-04-09 16:47:27 +0900

    입력 금액을 1000으로 나눈 몫을 구매 개수로 정하는 기능 구현

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,38 +1,55 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class LottoGame {
+  public static int oneLottoPrice = 1000;
+  public static int oneLottoAmount = 6;
+  public static int maxLottoNum = 45;
 
-  private boolean IsValidAmount(int purchaseAmount){
-    if(purchaseAmount < 0){
+  public int purchasePrice;
+  public int purchaseAmount;
+  public int lottoAmount;
+
+  private boolean IsValidAmount(int purchasePrice){
+    if(purchasePrice < 0){
       return false;
     }
-    if(purchaseAmount % 1000 != 0){
+    if(purchasePrice % oneLottoPrice != 0){
       return false;
     }
     return true;
   }
 
-  private int InputPurchaseAmount(){
+  private int InputPurchasePrice(){
     Scanner sc = new Scanner(System.in);
     System.out.println("구입금액을 입력해 주세요.");
-    int purchaseAmount = -1;
+    int purchasePrice = -1;
     try {
-      purchaseAmount= sc.nextInt();
+      purchasePrice= sc.nextInt();
     }catch (InputMismatchException e){
       System.out.println("유효하지 않은 값입니다");
     }
-    return purchaseAmount;
+    return purchasePrice;
   }
 
   public void StartLotto(){
-    int purchaseAmount = this.InputPurchaseAmount();
-    if(!this.IsValidAmount(purchaseAmount)){
+    purchasePrice = InputPurchasePrice();
+    if(!IsValidAmount(purchasePrice)) {
       System.out.println("잘못된 금액입니다.");
       return;
     }
+    purchaseAmount = purchasePrice/oneLottoPrice;
+    System.out.println("\n"+purchaseAmount+"개를 구매했습니다.");
+    /*
+    for (int i = 0; i < oneLottoAmount; i++) {
+      System.out.print(list.get(i) + " ");
+    }
+    */
   }
 
   public static void main(String args[]){
