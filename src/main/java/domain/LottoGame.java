6850010eccf21726708643ef7377c938711ee0b6
Yookibaek <psykibaek@gmail.com>
2019-04-09 14:22:38 +0900

    [ADD] [메인로직 4] 유저로부터 보너스넘버를 입력받는 로직 구현
    - 당첨결과 저장 데이터구조로 Map<Rank, Integer>을 사용한 이유에 관하여
    "Rank에 어떤 변수가 추가되더라도 Rank별로 카운팅하는 자료구조를
    바꿀일이 없도록 하자" 라는생각으로 Map의 key를 Rank자체로 지정한 것
    이었습니다. 이렇게 하면 Rank Enum클래스의 변경이 당첨결과 자료구조에
    영향을 미치지 않습니다.
    
    - createSortAppliedRankCounter() 함수에서 왜 Map에 sort를 지정해 정렬하려하나?
    Rank별로 갯수를 print할때 특정한 순서로 print하기 위함 입니다. 애초에 해당 TreeMap
    에 정렬순서를 지정해 놓으면, Rank의 종류가 추가되거나 하더라도 출력순서에 영향을
    미치지 않기 때문입니다.

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -2,6 +2,8 @@ package domain;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class LottoGame {
 
@@ -21,6 +23,46 @@ public class LottoGame {
     private void printLottos() {
         for (int i = 0; i < lottoList.size(); i++) {
             lottoList.get(i).printLottoNumbers();
+        }
+    }
+
+    private Map<Rank, Integer> calculateLottoMatch() {
+        Map<Rank, Integer> rankCounter = createSortAppliedRankCounter();
+        addAllRankTypes(rankCounter);
+
+        for (int i = 0; i < lottoList.size(); i++) {
+            Rank rank = winningLotto.match(lottoList.get(i));
+            rankCounter.put(rank, rankCounter.get(rank) + 1);
+        }
+        return rankCounter;
+    }
+
+    /**
+     * TreeMap을 생성해 반환합니다. TreeMap은 key(Rank)가 countOfMatch의 내림차순으로 정렬하기로
+     * 약속되어있습니다. 이렇게 하는 이유는, 앞으로 여러 Rank가 추가되더라도 Rank.java 에만 그 종류를
+     * 추가하면 Rank별로 print할때 자동으로 정렬이 되어 print되도록 하기 위함입니다.
+     *
+     * @return Map<Rank, Integer> 랭크별로 갯수를 세는데 이용되는 Map 반환
+     */
+    private Map<Rank, Integer> createSortAppliedRankCounter() {
+        Map<Rank, Integer> rankSortedMap = new TreeMap<Rank, Integer>((Rank a, Rank b) -> {
+            if (a.getWinningMoney() != b.getWinningMoney()) {
+                return (a.getWinningMoney() < b.getWinningMoney()) ? -1 : 1;
+            }
+
+            return 0;
+        });
+        return rankSortedMap;
+    }
+
+    /**
+     * rankCounter를 입력받아, 모든 종류의 Rank를 미리 입력해놓는 함수입니다.
+     *
+     * @return Map<Rank, Integer>
+     */
+    private void addAllRankTypes(Map<Rank, Integer> rankCounter) {
+        for (Rank rank : Rank.values()) {
+            rankCounter.put(rank, 0);
         }
     }
 }
