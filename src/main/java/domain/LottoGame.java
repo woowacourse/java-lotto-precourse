957d7b37e09ffdf3c238c1ed056f943481923584
SunHo Van <vsh123@naver.com>
2019-04-09 17:52:22 +0900

    feat : 사용자의 구임급액에 맞는 로또 발급 기능 구현 1. lottoPurchase : 구입금액 횟수만큼 반복하여 List에 add하는 메소드
    2. makeLotto : 6자리로 이루어진 Lotto를 만드는 메소드
    
    3. maskeLottoNumber : 중복되지 않는 LottoNumber를 생성하는 메소드

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,28 +1,70 @@
 /*
- * @(#)LottoGame.java        0.1 2019/04/08
+ * @(#)LottoGame.java        0.2 2019/04/09
  *
  *
  */
 
 package domain;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * LottoGame을 담당하는 객체입니다.
  *
  * @author 반선호
- * @version 0.1 2019년 4월 08일
+ * @version 0.2 2019년 4월 09일
  */
 public class LottoGame {
     private static final int TICKET_PRICE = 1000;
+    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_MAX_NUM = 45;
+    private static final int LOTTO_MIN_NUM = 1;
+
+    private List<Lotto> lottoList = new ArrayList<>();
 
     public void start() {
-        Scanner scanner = new Scanner(System.in);
-        int purchaseAmount = requestPurchaseAmount(scanner);
+        int lottoAmount = getLottoAmount();
+
+        lottoPurchase(lottoAmount);
     }
 
-    private int requestPurchaseAmount(Scanner scanner) {
+    private void lottoPurchase(int lottoAmount) {
+        System.out.println(lottoAmount + "개를 구매했습니다.");
+        for (int i = 0; i < lottoAmount; i++) {
+            lottoList.add(makeLotto());
+        }
+    }
+
+    private Lotto makeLotto() {
+        List<Integer> lotto = new ArrayList<>();
+
+        do {
+            lotto.add(makeLottoNumber(lotto));
+        } while (lotto.size() < LOTTO_SIZE);
+        Collections.sort(lotto);
+        System.out.println(lotto);
+        return new Lotto(lotto);
+    }
+
+    private int makeLottoNumber(List<Integer> lotto) {
+        Random random;
+        int randomNumber;
+
+        do {
+            random = new Random();
+            randomNumber = random.nextInt(LOTTO_MAX_NUM) + LOTTO_MIN_NUM;
+        } while (lotto.contains(randomNumber));
+        return randomNumber;
+    }
+
+    private int getLottoAmount() {
+        int purchaseAmount = requestPurchaseAmount();
+
+        return (purchaseAmount / TICKET_PRICE);
+    }
+
+    private int requestPurchaseAmount() {
+        Scanner scanner = new Scanner(System.in);
         String purchaseAmount;
 
         do {
