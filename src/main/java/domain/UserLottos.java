604ac41172a1507c21ea4e900542583491b40c9b
andole87 <andole87@gmail.com>
2019-04-10 00:16:27 +0900

    Update UserLotto.java
    UserLotto 클래스 생성자 작성.
    로또 구입금액을 문자열로 받아 1000을 나눈 숫자만큼 로또 인스턴스를
    갖는다.
    만약 인수를 Integer로변환할 수 없다면 NumberFormatException을 띄운다.
    테스트코드와 함께 작성했다.

--- src/main/java/domain/UserLottos.java
+++ src/main/java/domain/UserLottos.java
@@ -1,5 +1,33 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class UserLottos {
-//    List<Lotto> user
+    private List<Lotto> userLottos = new ArrayList<>();
+
+    public UserLottos(String moneyString){
+        try{
+            int buyToLottoMoney = (Integer.parseInt(moneyString) / 1000);
+
+            List<Integer> basicLottoNumbers = new ArrayList<>();
+            for (int i = 1; i <= 45; i ++){
+                basicLottoNumbers.add(i);
+            }
+            for (int i = 1; i <= buyToLottoMoney; i++){
+                Collections.shuffle(basicLottoNumbers);
+                List<Integer> selectedLottoNumbers = new ArrayList<>(basicLottoNumbers.subList(0,5));
+                userLottos.add(new Lotto(selectedLottoNumbers));
+            }
+        }catch (NumberFormatException ex){
+            throw new NumberFormatException("구입금액은 숫자만 가능합니다.");
+        }
+    }
+
+    public int getUserLottosCount(){
+        return userLottos.size();
+    }
+
+
 }
--- src/test/java/domain/UserLottosTest.java
+++ src/test/java/domain/UserLottosTest.java
@@ -1,7 +1,22 @@
 package domain;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class UserLottosTest {
 
+    @Test(expected = NumberFormatException.class)
+    public void constructorExceptionTest() {
+        UserLottos userLottosInvalidArgs = new UserLottos("만원");
+    }
+
+    @Test
+    public void constructorTest() {
+        UserLottos userLottos = new UserLottos("3000");
+        assertEquals(3,userLottos.getUserLottosCount());
+
+        UserLottos userLottos1 = new UserLottos("1000000");
+        assertEquals(1000, userLottos1.getUserLottosCount());
+    }
 }
