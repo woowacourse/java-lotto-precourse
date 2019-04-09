22cc73bfd58f560b8c92c0021e2166c005eb940f
yun <blpomspo@gmail.com>
2019-04-10 04:31:11 +0900

    Main 주석 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+/*
+ * @Main.java     0.1 2019-04-10
+ * Copyright(c) 2019 LeeYunSeop All rights reserved.
+ * */
+
 import domain.PlayLotto;
 
+/**
+ * Lotto 실행
+ *
+ * @author yun
+ * @version 0.1
+ */
 public class Main {
+
     public static void main(String[] args) {
         PlayLotto lotto = new PlayLotto();
         lotto.play();
