70af4601120f630cb507167158771a64bfdc1f43
JOngNan <dhorlawhddbs@naver.com>
2019-04-09 20:20:40 +0900

    사용자에게 입력 받을 때 유효성을 검사하기 위한 Validator 클래스 추가 및 Inputter 클래스 수정

--- src/main/java/domain/Inputter.java
+++ src/main/java/domain/Inputter.java
@@ -1,23 +1,78 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
+import static domain.Validator.AMOUNT_UNIT;
+
+final
+/**
+ * 사용자 입력을 받는 입력기
+ *
+ * @version 1.0(2019.04.09)
+ * @author jongyoon Kim
+ */
 public class Inputter {
+    private static final String MSG_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String MSG_INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.(6자리)"
+                                                                + "\n입력 예시 : 5,10,14,32,40,45";
+    private static final String MSG_INPUT_BONUS_BALL = "보너스 볼을 입력해 주세요.";
     private static Scanner scan = new Scanner(System.in);
+    private static Validator validator = new Validator();
 
     public int inputPurchaseAmount(){
-        System.out.println("구입금액을 입력해 주세요.");
-        int money = scan.nextInt();
-        return money / 1000;
+        String amount;
+        do{
+            System.out.println(MSG_INPUT_PURCHASE_AMOUNT);
+            amount = scan.nextLine();
+        }while (!validator.isNumeric(amount) && !validator.isPossibleAmountUnit(amount));
+        return Integer.parseInt(amount) / AMOUNT_UNIT;
     }
 
     public String inputWinningNumber(){
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        return scan.nextLine();
+        String winningNum;
+        do{
+            System.out.println(MSG_INPUT_WINNING_NUMBERS);
+            winningNum = scan.nextLine();
+        }while (!validator.isProperNumberDigit(winningNum));
+        return winningNum;
     }
 
-    public int inputBonusNumber(){
-        System.out.println("보너스 볼을 입력해 주세요.");
-        return scan.nextInt();
+    public List<Integer> winningNumberSplit(String winningNum){
+        List<Integer> splittedWinningNum;
+        boolean isDuplicateAndOverNum;
+        do{
+            splittedWinningNum = changeStrListToIntList(Arrays.asList(winningNum.split(",")));
+            isDuplicateAndOverNum = validator.isDuplicateAndNumOverValue(splittedWinningNum);
+            winningNum = reInputWinningNumbers(isDuplicateAndOverNum, winningNum);
+        }while (isDuplicateAndOverNum);
+
+        return splittedWinningNum;
+    }
+
+    private List<Integer> changeStrListToIntList(List<String> strList){
+        List<Integer> intList = new ArrayList<>();
+        for(String str : strList){
+            intList.add(Integer.parseInt(str));
+        }
+        return intList;
+    }
+
+    private String reInputWinningNumbers(boolean isRestart, String origin){
+        if(isRestart){
+            return inputWinningNumber();
+        }
+        return origin;
+    }
+
+    public int inputBonusNumber(List<Integer> splittedWinningNum){
+        int bonusNum;
+        do {
+            System.out.println(MSG_INPUT_BONUS_BALL);
+            bonusNum = scan.nextInt();
+        }while(validator.isDuplicateAndNumOverValue(bonusNum, splittedWinningNum));
+        return bonusNum;
     }
 }
--- src/main/java/domain/Validator.java
+++ src/main/java/domain/Validator.java
@@ -1,4 +1,74 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * 사용자 입력의 유효성 검사
+ *
+ * @version 1.0(2019.04.09)
+ * @author jongyoon Kim
+ */
 public class Validator {
+    public static final int AMOUNT_UNIT = 1000;
+    public static final int LOTTO_MAXIMUM_NUMBER_VALUE = 45;
+    private static final String NUMBER_PATTERN = "^[0-9]*$";
+    private static final String DIGIT_PATTERN = "^\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2}$";
+    private static final String ERROR_MSG_AMOUNT_UNIT_MESSAGE = "1000원 단위로 적어주세요!";
+    private static final String ERROR_MSG_NUMBER_MESSAGE = "숫자만 입력해주세요!";
+    private static final String ERROR_MSG_NUMBER_DIGIT_MESSAGE = "입력 형식이 잘못되었습니다.";
+    private static final String ERROR_MSG_DUPLICATE_NUMBER = "입력된 숫자들 중 중복이 있습니다.";
+    private static final String ERROR_MSG_NUMBER_OVER_VALUE = "입력된 숫자들 중 " + LOTTO_MAXIMUM_NUMBER_VALUE
+                                                            + "를 넘는 수가 있습니다.";
+    private static final String ERROR_MSG_BONUS_DUPLICATE_NUMBERLIST = "보너스 숫자는 당첨 번호와 같을 수 없습니다.";
+    private static final String ERROR_MSG_BONUS_OVER_VALUE = "보너스 숫자는 " + LOTTO_MAXIMUM_NUMBER_VALUE
+                                                            + "를 넘을 수 없습니다.";
+
+    public boolean isPossibleAmountUnit(String amount){
+        return checkingMatching((Integer.parseInt(amount) % AMOUNT_UNIT) == 0, ERROR_MSG_AMOUNT_UNIT_MESSAGE);
+    }
+
+    public boolean isNumeric(String str){
+        return checkingMatching(Pattern.matches(NUMBER_PATTERN,str),ERROR_MSG_NUMBER_MESSAGE);
+    }
+
+    public boolean isProperNumberDigit(String str){
+        return checkingMatching(Pattern.matches(DIGIT_PATTERN,str),ERROR_MSG_NUMBER_DIGIT_MESSAGE);
+    }
+
+    private boolean checkingMatching(boolean checking, String message){
+        if(!checking){
+            System.out.println(message);
+        }
+        return checking;
+    }
+
+    public boolean isDuplicateAndNumOverValue(List<Integer> numbers){
+        List<Integer> checkedNums = new ArrayList<>();
+        for(Integer num : numbers){
+            if(checkedNums.contains(num)){
+                System.out.println(ERROR_MSG_DUPLICATE_NUMBER);
+                return true;
+            }
+            checkedNums.add(num);
+            if(num > LOTTO_MAXIMUM_NUMBER_VALUE){
+                System.out.println(ERROR_MSG_NUMBER_OVER_VALUE);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isDuplicateAndNumOverValue(int num, List<Integer> numbers){
+        if(numbers.contains(num)){
+            System.out.println(ERROR_MSG_BONUS_DUPLICATE_NUMBERLIST);
+            return true;
+        }
+        if(num > LOTTO_MAXIMUM_NUMBER_VALUE){
+            System.out.println(ERROR_MSG_BONUS_OVER_VALUE);
+            return true;
+        }
+        return false;
+    }
 }
