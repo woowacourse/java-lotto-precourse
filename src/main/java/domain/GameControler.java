5121d18394769dbf7f9bc97a6d795f6a7844dc42
DGpress <bgbg9655@naver.com>
2019-04-09 16:30:53 +0900

    Update LottoGame.java, GameControler.java
    1-1 구입금액 입력
    구입 금액 입력에 따른 GameControler 객체 생성

--- src/main/java/domain/GameControler.java
+++ src/main/java/domain/GameControler.java
@@ -1,5 +1,19 @@
 package domain;
 
+/**
+ * 로또게임에 필요한 함수를 생성해 놓은 클래스
+ */
+
 public class GameControler {
-  
+    private int n;
+    private int pre_money;
+
+    GameControler(int n) {
+        this.n = n / 1000;
+        this.pre_money = n;
+    }
+
+    public void setLottos() {
+        
+    }
 }
--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,7 +1,18 @@
 package domain;
 
+import java.util.Scanner;
+
+/**
+ * 로또 게임을 실행할 메인함수
+ */
+
 public class LottoGame {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    System.out.println("구입금액을 입력해 주세요.");
+    GameControler control = new GameControler(sc.nextInt());
+    sc.nextLine();
+    control.setLottos();
   }
 }
