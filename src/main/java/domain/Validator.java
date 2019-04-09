a2cf3ae993e634aa2a7e6f91b1cf9433587ffeb0
Yookibaek <psykibaek@gmail.com>
2019-04-09 13:27:33 +0900

    [ADD] [서브로직 1-2] 로또구입금액이 특정길이를 초과하는지 확인하는 로직
    - 왜 특정길이 이상인지 감지하려하는지?
    구입금액의 길이가 너무 길게 들어온다면, 정수로 변환하는 과정에서 오버플로우가
    발생할수있다 생각하여, 길이제한을 10으로 두었습니다.

--- src/main/java/domain/Validator.java
+++ src/main/java/domain/Validator.java
@@ -3,6 +3,8 @@ package domain;
 public class Validator {
 
     static final String WARNING_WHEN_INPUT_IS_NOT_INTEGER = "WARNING: 정수만 입력 가능합니다. 다시 입력해주세요.";
+    static final int MAX_POSSIBLE_LOTTO_INPUT_LENGTH = 10;
+    static final String WARNING_WHEN_LOTTO_INPUT_COUNT_OVER = "WARNING: 입력이 너무 깁니다. " + MAX_POSSIBLE_LOTTO_INPUT_LENGTH + "자리 아래로 다시 입력해주세요.";
 
     public static boolean checkIsInteger(String inputNumber) {
         try {
@@ -12,5 +14,13 @@ public class Validator {
             System.out.println(WARNING_WHEN_INPUT_IS_NOT_INTEGER);
             return false;
         }
+    }
+
+    public static boolean checkInputLottoMoneyLengthIsTooLong(String lottoCountInput) {
+        if (lottoCountInput.length() > MAX_POSSIBLE_LOTTO_INPUT_LENGTH) {
+            System.out.println(WARNING_WHEN_LOTTO_INPUT_COUNT_OVER);
+            return false;
+        }
+        return true;
     }
 }
