c0b6f001cc8e0c0bce4d87249686a8f06a8da160
deocksoo <dusol92@snu.ac.kr>
2019-04-09 17:39:12 +0900

    [ADD] StatisticalAnalyzer의 makeRankCountsTableOf 메소드를 구현합니다
    1. 집계 결과를 Rank순으로 보여주기 위하여 LinkedHashMap을 사용하였습니다.
    2. 집계할 rank들을 임의로 정하고, 보다 간단하게 counting할 수 있도록
    initialize 메소드를 통해 LinkedHashmap를 초기화 합니다. initialize 메소드는
    집계할 rank type을 인자로 건네받음으로써, 원하는 rank를 선별하여 집계할 수
    있게 됩니다.

--- src/main/java/domain/StatisticalAnalyzer.java
+++ src/main/java/domain/StatisticalAnalyzer.java
@@ -2,10 +2,25 @@ package domain;
 
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class StatisticalAnalyzer {
+    private static final int COUNT_SCALE = 1;
+
+    public void initialize(HashMap<Rank, Integer> rankCounts, Rank[] typeOfranks) {
+        for (Rank rank: typeOfranks) {
+            rankCounts.put(rank, 0);
+        }
+    }
+
     public HashMap<Rank, Integer> makeRankCountsTableOf(HashMap<Lotto, Rank> lotteryResults) {
-        return null;
+        HashMap<Rank, Integer> rankCounts = new LinkedHashMap<Rank, Integer>();
+
+        initialize(rankCounts, Rank.values());
+        for (Rank rank: lotteryResults.values()) {
+            rankCounts.put(rank, rankCounts.get(rank) + COUNT_SCALE);
+        }
+        return rankCounts;
     }
 
     public double calculateEarningRateBy(HashMap<Rank, Integer> rankCounts) {
