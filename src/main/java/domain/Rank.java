c6778a6fc96f3e7bd2a4e17b5a8a2766bbc62959
wschoi8640 <cws911112@gmail.com>
2019-04-09 18:55:05 +0900

    UPDATE Rank.java (modify Template)

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -4,7 +4,7 @@ package domain;
  * 로또 등수를 의미하는 enum
  */
 public enum Rank {
-    FIRST(6, 2_000_000_000), // 1등
+	FIRST(6, 2_000_000_000), // 1등
     SECOND(5, 30_000_000), // 2등
     THIRD(5, 1_500_000), // 3등
     FOURTH(4, 50_000), // 4등
@@ -33,17 +33,17 @@ public enum Rank {
         if (countOfMatch < WINNING_MIN_COUNT) {
             return MISS;
         }
-
+ 
         if (SECOND.matchCount(countOfMatch) && matchBonus) {
             return SECOND;
         }
-
+ 
         for (Rank rank : values()) {
-            if (rank.matchCount(countOfMatch)) {
+            if (rank.matchCount(countOfMatch) && rank != SECOND) {
                 return rank;
             }
         }
-
+ 
         throw new IllegalArgumentException(countOfMatch + "는 유효하지 않은 값입니다.");
     }
 
@@ -51,4 +51,3 @@ public enum Rank {
         return this.countOfMatch == countOfMatch;
     }
 }
-
