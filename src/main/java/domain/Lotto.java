252d26e4fb489c5242e39ca49ba932b7cacc2ce6
bactoria <bactoria@gmail.com>
2019-04-09 17:33:54 +0900

    FEAT :: 로또 예외처리 추가 1. 로또 번호가 6자리가 아닌 경우 2. 로또 번호에 중복된 수가 있는 경우 3. 로또 번호가 1~45 사이 값이 아닌 경우

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,6 +1,9 @@
 package domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static domain.LottoConst.*;
 
 /**
  * 로또 한장을 의미하는 객체
@@ -9,7 +12,39 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        validateDuplication(numbers);
+        validateSize(numbers);
+        validateRange(numbers);
+
         this.numbers = numbers;
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        List<Integer> duplicationRemovedNumbers =
+                numbers.stream()
+                        .distinct()
+                        .collect(Collectors.toList());
+
+        if (!duplicationRemovedNumbers.equals(numbers)) {
+            throw new IllegalArgumentException("로또 번호에 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("로또 번호 개수가 유효하지 않습니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        long validRangeNumberCount = numbers.stream()
+                .filter(number -> number >= LOTTO_MIN_NUMBER)
+                .filter(number -> number <= LOTTO_MAX_NUMBER)
+                .count();
+
+        if (validRangeNumberCount != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("유효하지 않은 로또 번호가 있습니다.");
+        }
     }
 
     int getCountOfMatchedNumber(Lotto lotto) {
