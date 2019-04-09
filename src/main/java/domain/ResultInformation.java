b1b6cf557c1742d4908ffc672d7fa0587e7e05e1
chowhr1231 <thddbswo14@gmail.com>
2019-04-10 00:43:04 +0900

    [MOD] enumMap을 초기화
    createEnumMap 메서드가 10줄이 넘어
    분리하기 위해서 initalEnumMap 메서드로 분리

--- src/main/java/domain/ResultInformation.java
+++ src/main/java/domain/ResultInformation.java
@@ -1,11 +1,10 @@
 package domain;
 
-import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 
 public class ResultInformation {
-    private List<Rank> ranks = new ArrayList<>();
+    private List<Rank> ranks;
     private EnumMap<Rank, Integer> enumMap;
 
     private static final int MIN_MONEY = 1000;
@@ -15,15 +14,21 @@ public class ResultInformation {
         this.enumMap = createEnumMap();
     }
 
-    private EnumMap<Rank, Integer> createEnumMap(){
-        EnumMap<Rank, Integer> map = new EnumMap<Rank, Integer>(Rank.class);
-        int cnt;
+    private EnumMap<Rank, Integer> initialEnumMap() {
+        EnumMap<Rank, Integer> initalMap = new EnumMap<>(Rank.class);
 
-        for(Rank type : Rank.values()){
-            map.put(type, 0);
+        for (Rank type : Rank.values()) {
+            initalMap.put(type, 0);
         }
 
-        for(Rank rank : ranks){
+        return initalMap;
+    }
+
+    private EnumMap<Rank, Integer> createEnumMap() {
+        EnumMap<Rank, Integer> map = initialEnumMap();
+        int cnt;
+
+        for (Rank rank : ranks) {
             cnt = map.get(rank);
             map.put(rank, ++cnt);
         }
@@ -31,14 +36,14 @@ public class ResultInformation {
         return map;
     }
 
-    int getRankCount(Rank type){
+    int getRankCount(Rank type) {
         return enumMap.get(type);
     }
 
-    float getProfitRate(){
+    float getProfitRate() {
         int reward = 0;
 
-        for(Rank rank : Rank.values()){
+        for (Rank rank : Rank.values()) {
             reward += rank.getWinningMoney() * enumMap.get(rank);
         }
 
