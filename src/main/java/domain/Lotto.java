007ad58e5c9be1589800cc11239dad4d56552361
NotYet <wiseoldster@gmail.com>
2019-04-09 17:47:04 +0900

    기능 구현 : 구입 금액 입력 받는 기능

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,9 +24,22 @@ public class Lotto {
 	}
 
 	// 추가 기능 구현
+	private static int inputCost() {
+		System.out.println("구입금액을 입력해주세요. ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int ret = 0;
+		try {
+			ret = Integer.parseInt(br.readLine());
+		} catch (Exception e) {
+			System.out.println("유효하지 않은 입력입니다. 다시 입력해주세요.");
+			inputCost();
+		}
+		return ret;
+	}
 
 	public static void main(String[] args) {
 		// 객체로 불러 실행해야한다면 shell()로 대체한다.
+		int cost = inputCost();
 
 	}
 }
