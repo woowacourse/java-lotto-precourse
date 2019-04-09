f3c7acf39fcc7cc83ed225910650c9d301688a73
Yookibaek <psykibaek@gmail.com>
2019-04-09 13:44:50 +0900

    [ADD] [서브로직 3-1] 당첨로또번호 입력이 (,)기준으로 나눴을시 6개가 맞는지확인하는 로직

--- src/main/java/domain/Validator.java
+++ src/main/java/domain/Validator.java
@@ -6,6 +6,7 @@ public class Validator {
     static final int MAX_POSSIBLE_LOTTO_INPUT_LENGTH = 10;
     static final String WARNING_WHEN_LOTTO_INPUT_COUNT_OVER = "WARNING: 입력이 너무 깁니다. " + MAX_POSSIBLE_LOTTO_INPUT_LENGTH + "자리 아래로 다시 입력해주세요.";
     static final String WARNING_WHEN_LOTTO_INPUT_SMALL = "WARNING: 금액이 너무 적습니다. 1장의 금액은 " + GameSetting.PRICE_PER_1LOTTO + "원 입니다. 다시 입력해주세요.";
+    static final String WARNING_WHEN_LOTTO_NUMBER_COUNT_NOT_MATCHING = "WARNING: 로또넘버는 " + GameSetting.LOTTO_NORMAL_NUMBER_COUNT + "개 이어야 합니다. 다시 입력해주세요.";
 
     public static boolean checkInputLottoMoney(String lottoCountInput) {
         if (!checkInputLottoMoneyLengthIsTooLong(lottoCountInput) ||
@@ -43,5 +44,16 @@ public class Validator {
             return true;
         }
         return false;
+    }
+
+    //
+    public static boolean checkLottoNumbersInputLengthIsValid(String winningLottoInput) {
+        String[] numbers = winningLottoInput.split(",");
+
+        if (numbers.length != GameSetting.LOTTO_NORMAL_NUMBER_COUNT) {
+            System.out.println(WARNING_WHEN_LOTTO_NUMBER_COUNT_NOT_MATCHING);
+            return false;
+        }
+        return true;
     }
 }
