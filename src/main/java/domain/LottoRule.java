e1974df7392809c16a998482a7fd26841fa877ef
wschoi8640 <cws911112@gmail.com>
2019-04-10 13:33:34 +0900

    UPDATE ENUM LottoRule.java (ADD LottoRules)

--- src/main/java/domain/LottoRule.java
+++ src/main/java/domain/LottoRule.java
@@ -1,5 +1,34 @@
 package domain;
 
 public enum LottoRule {
-
+	BEGINNUM(1),
+	ENDNUM(45),
+	LottoLENGTH(6),
+	PRICE(1000),
+	REWARDS(new int[]{5000, 50000, 1500000, 30000000, 2000000000}),
+	RANKNAMES(new String[] {"3개 일치(5000원) - ", "4개 일치(50000원) - ", "5개 일치(1500000원) - ",
+			"5개일치,보너스볼일치(30000000원) - ", "6개일치(2000000000원) - " });
+	
+	int numericVal;
+	String[] strArrVal;
+	int[] numArrVal;
+	private LottoRule(int num) {
+		this.numericVal = num;
+	}
+	private LottoRule(String[] strArr) {
+		this.strArrVal = strArr;
+	}
+	private LottoRule(int[] numArr) {
+		this.numArrVal = numArr;
+	}
+	
+	public int getNum() {
+		return numericVal;
+	}
+	public String[] getNames() {
+		return strArrVal;
+	}
+	public int[] getNums() {
+		return numArrVal;
+	}
 }
