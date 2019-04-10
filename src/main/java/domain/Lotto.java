5dabd4b0f94ad935746ea6a71a0799b74a1290ea
backlo <hjun4512@naver.com>
2019-04-10 21:22:27 +0900

    ADD : 구매한 로또 개수 만큼 번호 발급 및 출력 하는 getLottoNumber()함수 구현 	- 숫자 중복시 재발급 : duplicateNumberCheck() 	- 1~45 까지의 번호 생성 : createLottoNumber() 	- 콘솔에 출력해주는 함수 : printLottoNumber()

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -8,11 +9,41 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
+    private static final int LOTTO_MIN_NUM = 1;
+    private static final int LOTTO_MAX_NUM = 45;
+    private static final int LOTTO_SIZE = 6;
+
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
     }
 
     // 추가 기능 구현
+    public List<Integer> getLottoNumber() {
+
+        while (duplicateNumberCheck(numbers, createLottoNumber()).size() != LOTTO_SIZE) {
+        }
+
+        Collections.sort(numbers);
+        System.out.println(printLottoNumber(numbers));
+
+        return numbers;
+    }
+
+    private List<Integer> duplicateNumberCheck(List<Integer> numbers, int checkNumber) {
+        if (!numbers.contains(checkNumber)) {
+            numbers.add(checkNumber);
+            return numbers;
+        }
+        return numbers;
+    }
+
+    private int createLottoNumber() {
+        return ((int) (Math.random() * LOTTO_MAX_NUM) + LOTTO_MIN_NUM);
+    }
+
+    private String printLottoNumber(List<Integer> numbers) {
+        return String.join(",", String.valueOf(numbers));
+    }
 
 
 }
