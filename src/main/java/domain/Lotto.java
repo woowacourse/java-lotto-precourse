947dc2fe5878682e18039de1205a5bdb7a14c0ed
imkimheejoo <jujudamdadi@naver.com>
2019-04-11 08:11:06 +0900

    구입한 로또의 번호들을 생성하는 함수 구현

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,6 +1,8 @@
 package domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 로또 한장을 의미하는 객체
@@ -12,6 +14,21 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+
+    private void addNumber(int number) {
+        numbers.add(number);
+    }
+
+    public void create() {
+        Set<Integer> set = new HashSet<>();
+
+        while (set.size() < Const.CREATE_LOTTO_NUMBER_TIME) {
+            set.add(createRandomNumber());
+        }
+
+        set.stream().forEach(r -> addNumber(r));
+    }
+    
     public boolean isOverlapToBonusNumber(String BonusNumber) {
         return numbers.contains(Integer.parseInt(BonusNumber));
     }
