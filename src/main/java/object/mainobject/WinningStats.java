966a0aa045b97b2df25b24a1cdc93bba587bb5ec
men7627 <men1219@hanmail.net>
2019-04-11 03:41:23 +0900

    WinningStats클래스 makeWinningStats() 구현

--- src/main/java/object/mainobject/WinningStats.java
+++ src/main/java/object/mainobject/WinningStats.java
@@ -6,8 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WinningStats {
+        private final PurchaseInfo purchaseInfo;
         private final Map<Rank,Integer> stats;
-        public WinningStats(){
+
+        public WinningStats(PurchaseInfo purchaseInfo){
+                this.purchaseInfo = purchaseInfo;
                 stats = new HashMap<Rank,Integer>()
                 stats.put(Rank.FIRST,0);
                 stats.put(Rank.SECOND,0);
@@ -15,5 +18,9 @@ public class WinningStats {
                 stats.put(Rank.FOURTH,0);
                 stats.put(Rank.FIFTH,0);
                 stats.put(Rank.MISS,0);
+        }
+
+        public void makeWinningStats(){
+                purchaseInfo.makeWinningStats(this.stats);
         }
 }
