4a980ce060d3275cee9eb36fc52915adfc67fee9
ArtisanK <fancyartisan@gmail.com>
2019-04-10 11:51:39 +0900

    Update PlayLotto.java Add start() method Add printStatistics() method Add setWinCheak(int i) method Add getStatistics() method

--- src/main/java/domain/PlayLotto.java
+++ src/main/java/domain/PlayLotto.java
@@ -8,6 +8,52 @@ public class PlayLotto {
     private List<Integer> tempNumbers;
     private WinningLotto masterLotto;
     private int money;
+    private int winningMoney;
+
+    private int[] winCheak = new int[5];
+
+    public void start() {
+        userLottos = new ArrayList<Lotto>();
+        getUserLottos();
+        getWinningNo();
+
+        System.out.println("당첨 통계");
+        System.out.println("--------");
+        getStatistics();
+        printStatistics();
+    }
+
+    private void printStatistics() {
+        double returnRate = ((double) winningMoney / (double) money);
+        System.out.println("3개 일치 (5000원)- " + winCheak[4] + "개");
+        System.out.println("4개 일치 (50000원)- " + winCheak[3] + "개");
+        System.out.println("5개 일치 (1500000원)- " + winCheak[2] + "개");
+        System.out.println("5개 일치,보너스 볼 일치 (30000000원)- " + winCheak[1] + "개");
+        System.out.println("6개 일치 (2000000000원)- " + winCheak[0] + "개");
+        System.out.println("총 수익률은 " + Math.round(returnRate * 1000) / 1000.0 + "입니다.");
+    }
+
+    private void setWinCheak(int i) {
+        Rank temp = masterLotto.match(userLottos.get(i));
+        winningMoney += temp.getWinningMoney();
+        if (temp == Rank.FIRST) {
+            winCheak[0]++;
+        } else if (temp == Rank.SECOND) {
+            winCheak[1]++;
+        } else if (temp == Rank.THIRD) {
+            winCheak[2]++;
+        } else if (temp == Rank.FOURTH) {
+            winCheak[3]++;
+        } else if (temp == Rank.FIFTH) {
+            winCheak[4]++;
+        }
+    }
+
+    private void getStatistics() {
+        for (int i = 0; i < money / 1000; i++) {
+            setWinCheak(i);
+        }
+    }
 
     private void getWinningNo() {
         Scanner sc = new Scanner(System.in);
