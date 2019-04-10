5659402770feb1ae0b9a2406f715f625f2f445e5
men7627 <men1219@hanmail.net>
2019-04-10 22:31:19 +0900

    PrintUtil클래스 printPurchaseConfirmMessage() 구현

--- src/main/java/util/PrintUtil.java
+++ src/main/java/util/PrintUtil.java
@@ -4,4 +4,7 @@ public class PrintUtil {
         public void printPurchaseInputMessage(){
                 System.out.println("구입금액을 입력해 주세요.");
         }
+        public void printPurchaseConfirmMessage(int numberOfLotto){
+                System.out.println(numberOfLotto+"개를 구매했습니다.");
+        }
 }
