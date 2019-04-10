2277346d9125ddbbdc8dbb6edf9de165fe0be249
wschoi8640 <cws911112@gmail.com>
2019-04-10 13:24:17 +0900

    UPDATE LottoShop.java : Add setNumOfLotto()

--- src/main/java/domain/LottoShop.java
+++ src/main/java/domain/LottoShop.java
@@ -1,5 +1,24 @@
 package domain;
 
-public class LottoShop {
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
+public class LottoShop {
+	private int numOfLotto;
+	private Scanner sc;
+
+	public LottoShop() {
+		sc = new Scanner(System.in);
+	}
+
+	public void setNumOfLotto() {
+		try {
+			System.out.println(RequestMsg.SetNum.getMsg());
+			numOfLotto = checkValid(sc.nextInt()) / LottoRule.PRICE.getNum();
+		} catch (InputMismatchException e) {
+			printAndfixErr(ErrMsg.InputMismatchErr.getMsg());
+		}
+	}
 }
