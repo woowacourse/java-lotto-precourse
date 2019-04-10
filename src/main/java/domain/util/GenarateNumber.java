f4d37744f8355e2ce68d7914b2bc1dfd49dfac77
kmdngyu <kmdngyu@gmail.com>
2019-04-10 16:16:26 +0900

    add : 1부터 45까지의 숫자가 겹치지 않게 6개 숫자 생성

--- src/main/java/domain/util/GenarateNumber.java
+++ src/main/java/domain/util/GenarateNumber.java
@@ -1,4 +1,28 @@
 package domain.util;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 public class GenarateNumber {
+
+    public static List<Integer> genarateUserNumber(){
+        List <Integer> sixNumbers = new ArrayList<>();
+        while(sixNumbers.size()<Constant.NUMBER_OF_LOTTO_NUMBERS){
+            addingNumberToList(sixNumbers);
+        }
+        Collections.sort(sixNumbers);
+        return sixNumbers;
+    }
+
+    public static void addingNumberToList(List <Integer> numberList){
+        int randomNumber = getRandomNum();
+        if(!numberList.contains(randomNumber)){
+            numberList.add(randomNumber);
+        }
+
+    }
+
+    public static int getRandomNum(){
+        int randomNum = (int)(Math.random() * Constant.MAX_LOTTO_NUM + Constant.MIN_LOTTO_NUM);
+        return randomNum;
+    }
 }
