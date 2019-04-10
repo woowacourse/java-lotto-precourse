c2c6923666d19792502ef33e5c7d30b39eb372bd
김성훈 <seonghun127@gmail.com>
2019-04-10 18:39:46 +0900

    update README.md & add UserInput

---
+++ src/main/java/domain/UserInput.java
@@ -0,0 +1,28 @@
+/*
+ * UserInput
+ *
+ * version 1.0
+ *
+ * 2019/04/10
+ *
+ *  */
+
+package domain;
+
+import java.util.Scanner;
+
+/**
+ * 로또 번호를 발급받기 위해 사용자로부터 구입 금액을 입력받는 클래스
+ *
+ * @author 김성훈
+ * @version 1.0
+ */
+public class UserInput {
+    private static final int PRICE_PER_LOTTO = 1000;
+
+    private Scanner sc = new Scanner(System.in);
+
+    public int inputTotalPrice(){
+        
+    }
+}
