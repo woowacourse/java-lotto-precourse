d6bf300a3027364bf3a51b2fdfd785a6468b276b
wschoi8640 <cws911112@gmail.com>
2019-04-10 13:34:32 +0900

    UPDATE ENUM ErrMsg.java (ADD ErrMsgs)

--- src/main/java/domain/ErrMsg.java
+++ src/main/java/domain/ErrMsg.java
@@ -1,5 +1,18 @@
 package domain;
 
 public enum ErrMsg {
-
+	InputMismatchErr("자연수를 입력해야 합니다!"),
+	InputTooSmallErr("0보다 큰 수를 입력해야 합니다."),
+	NotEnoughMoneyErr("로또를 살 수 없는 금액입니다."),
+	OutOfRangeErr("1 - 45 사이 수를 입력해야 합니다."),
+	NumLengthErr("6개의 수를 입력해야 합니다.");
+	
+	String errMsg;
+	private ErrMsg(String errMsg){
+		this.errMsg = errMsg; 
+	}
+	
+	public String getMsg() {
+		return errMsg;
+	}
 }
