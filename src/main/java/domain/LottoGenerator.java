c852cef6b5a070fc0f885cfea3e6d55e3a8c2cd7
samsung <samsung@LAPTOP-859NPSQ7>
2019-04-10 14:38:59 +0900

    1 ~ 45 사이의 임의의 정수 로또 번호 6개 생성

--- src/main/java/domain/LottoGenerator.java
+++ src/main/java/domain/LottoGenerator.java
@@ -1,10 +1,17 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class LottoGenerator {
 	static final int EACH_LOTTO_PRICE = 1000;
+	static final int MIN_LOTTO_NUMBER = 1;
+	static final int MAX_LOTTO_NUMBER = 45;
+	static final int LOTTO_NUMBER_COUNT = 6;
 	Scanner scanner = new Scanner(System.in);
+	Random random = new Random();
 
 	private int inputLottoPriceFromUser() {
 		int lottoPrice;
@@ -32,5 +39,14 @@ public class LottoGenerator {
 			return false;
 		}
 		return true;
+	}
+	
+	private List<Integer> makeLottoNumbers() {
+		List<Integer> lottoNumbers = new ArrayList<Integer>();
+		while (lottoNumbers.size() < LOTTO_NUMBER_COUNT) {
+			int randomNumber = random.nextInt(MAX_LOTTO_NUMBER) + MIN_LOTTO_NUMBER;
+				lottoNumbers.add(randomNumber);
+		}
+		return lottoNumbers;
 	}
 }
