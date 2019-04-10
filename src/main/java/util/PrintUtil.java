877d57b9d3c6ebbe7c8e8d6454009bfc9ffe0ed5
men7627 <men1219@hanmail.net>
2019-04-10 22:33:05 +0900

    PrintUtil클래스 printPurchasedLotto() 구현

--- src/main/java/util/PrintUtil.java
+++ src/main/java/util/PrintUtil.java
@@ -1,10 +1,18 @@
 package util;
 
+import domain.Lotto;
+
 public class PrintUtil {
         public void printPurchaseInputMessage(){
                 System.out.println("구입금액을 입력해 주세요.");
         }
+
         public void printPurchaseConfirmMessage(int numberOfLotto){
                 System.out.println(numberOfLotto+"개를 구매했습니다.");
+        }
+
+        public void printPurchasedLotto(Lotto lotto){
+                String message = lotto.getNumbers().toString();
+                System.out.println(message);
         }
 }
