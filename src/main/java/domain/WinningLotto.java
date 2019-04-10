145fe1c219f99fe6411feb49c94decd54f4e1318
U-Yeon <leanner@naver.com>
2019-04-10 20:57:51 +0900

    Class.WinningLotto에서의 당첨번호와 사용자의 랜덤 번호 비교 메소드

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.List;
 /**
  * 당첨 번호를 담당하는 객체
  */
@@ -13,7 +14,24 @@ public class WinningLotto {
     }
 
     public Rank match(Lotto userLotto) {
-        // TODO 로직 구현
-        return null;
+    	List<Integer> userLottoNum = userLotto.getNumbers();
+    	int countOfMatch = 0;
+    	boolean matchOfBonus = false;
+    	for(int i=0; i<LottoGame.LOTTO_NUM_LENGTH; ++i) {
+    		countOfMatch += getCountOfMatch(userLottoNum.get(i));
+    		matchOfBonus |= (bonusNo ==userLottoNum.get(i));
+    	}
+    	return Rank.valueOf(countOfMatch, matchOfBonus);
+    }
+    
+    public int getCountOfMatch(int userLottoNum) {
+    	boolean isMatched = false;
+    	for(int i=0; i<LottoGame.LOTTO_NUM_LENGTH; ++i) {
+    		isMatched |= (userLottoNum == this.lotto.getNumbers().get(i));
+    	}
+    	if(isMatched) {
+    		return 1;
+    	}
+    	return 0;
     }
 }
