1f68714bc1e8bb1202afff9b6685dd2d84fb4941
backlo <hjun4512@naver.com>
2019-04-10 21:24:12 +0900

    ADD : 사용자가 구입한 로또번호를 LottoList에 추가하는 assignPurchaseLottoList()함수 구현

--- src/main/java/domain/GameController.java
+++ src/main/java/domain/GameController.java
@@ -1,22 +1,34 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GameController {
 
-    private static final int lottoPrice = 1000;
+    private static final int LOTTO_PRICE = 1000;
 
-    public void startGame(){
+    public void startGame() {
+        List<List<Integer>> purchaseLottoList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        int numberOfPrice = purchasePrice(scanner);
+        assignPurchaseLottoList(purchasePrice(scanner), purchaseLottoList);
 
-        System.out.println("구입개수 : " + numberOfPrice + "개");
     }
 
-    private int purchasePrice(Scanner scanner){
+    private int purchasePrice(Scanner scanner) {
         System.out.println("구입 금액을 입력해 주세요.");
-
-        return (scanner.nextInt())/lottoPrice;
+        return (scanner.nextInt() / LOTTO_PRICE);
     }
+
+    private void assignPurchaseLottoList(int numberOfPrice, List<List<Integer>> purchaseLottoList) {
+        System.out.println("\n" + numberOfPrice + "개를 구매했습니다.");
+
+        for (int i = 0; i < numberOfPrice; i++) {
+            Lotto assignLotto = new Lotto(new ArrayList<>());
+            purchaseLottoList.add(assignLotto.getLottoNumber());
+        }
+
+    }
+
 
 }
