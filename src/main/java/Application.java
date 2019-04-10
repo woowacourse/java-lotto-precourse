ba18f2e04f3a31bec2ed4edf00f53af41fa25f1e
bactoria <bactoria@gmail.com>
2019-04-11 00:42:05 +0900

    CLN :: 객체 생성을 메소드 외부로 수정 함수가 10줄이 넘지 않도록 하기 위해서 외부로 뺄 수 있는 것들은 뺐음.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,20 +3,17 @@ import domain.*;
 import java.util.List;
 
 public class Application {
+    private static UserInterface userInterface = new UserInterface();
+    private static LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+    private static LottoMachine lottoMachine = new LottoMachine(lottoNumberGenerator);
 
     public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface();
         int purchasePrice = userInterface.getPurchasePrice();
-
-        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
-        LottoMachine lottoMachine = new LottoMachine(lottoNumberGenerator);
         List<Lotto> lottos = lottoMachine.purchaseLottos(purchasePrice);
-
         userInterface.printLottos(lottos);
 
         WinningLotto winningLotto = userInterface.getWinningLotto();
         LottoRankAnalyzer lottoRankAnalyzer = new LottoRankAnalyzer(winningLotto);
-
         userInterface.printRankCount(lottoRankAnalyzer.getLottoRankCount(lottos));
         userInterface.printEarningRate(lottoRankAnalyzer.getEarningRate(lottos));
     }
