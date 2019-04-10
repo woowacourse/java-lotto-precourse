d1d8f1474a20abadc4574085136443628c8d9fb9
wschoi8640 <cws911112@gmail.com>
2019-04-10 13:38:00 +0900

    UPDATE LottoShop.java : Add buyLotto()

--- src/main/java/domain/LottoShop.java
+++ src/main/java/domain/LottoShop.java
@@ -3,13 +3,18 @@ package domain;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class LottoShop {
+	private List<Integer> pickedNumbers;
+	private List<Lotto> userLottos;
 	private int numOfLotto;
 	private Scanner sc;
 
 	public LottoShop() {
+		userLottos = new ArrayList<Lotto>();
 		sc = new Scanner(System.in);
 	}
 
@@ -36,5 +41,14 @@ public class LottoShop {
 		if (errMsg.equals(ErrMsg.InputMismatchErr.getMsg()))
 			sc.next();
 		setNumOfLotto();
+	}
+	
+	public void buyLotto() {
+		for (int i = 0; i < numOfLotto; i++) {
+			pickedNumbers = new Random().ints(LottoRule.BEGINNUM.getNum(), LottoRule.ENDNUM.getNum()).distinct()
+					.limit(LottoRule.LottoLENGTH.getNum()).boxed().collect(Collectors.toList());
+			userLottos.add(new Lotto(pickedNumbers));
+		}
+		printLottos();
 	}
 }
