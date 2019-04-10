bc87716327e8338e8c87c23924fa8a50165abcf2
wschoi8640 <cws911112@gmail.com>
2019-04-10 15:11:23 +0900

    UPDATE LottoGame.java : ADD init()

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,5 +1,21 @@
 package domain;
 
 public class LottoGame {
+	
+	private LottoShop lottoShop;
+	private LottoBank lottoBank;
+	
+	public LottoGame() {
+		lottoShop = new LottoShop();
+		lottoBank = new LottoBank(lottoShop);
+	}
 
+	public void init() {
+		lottoShop.setNumOfLotto();
+		lottoShop.buyLotto();
+		
+		lottoBank.setWinningLotto();
+		lottoBank.matchUserLotto();
+		lottoBank.calUserProfit();
+	}
 }
