7dc869dba3ca768f98a5df67c5b886044cffb425
backlo <hjun4512@naver.com>
2019-04-10 16:48:54 +0900

    ADD : Main 함수 생성및 GameController클래스 생성

---
+++ src/main/java/domain/GameController.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class GameController {
+
+    public void startGame(){
+
+    }
+
+}
---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Main {
+
+    public static void main(String[] args){
+        GameController gameController = new GameController();
+        gameController.startGame();
+    }
+}
