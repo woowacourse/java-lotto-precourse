a95cc24e5d931f2f88d601764eb24f259679b3ae
men7627 <men1219@hanmail.net>
2019-04-10 23:41:23 +0900

    InputUtil클래스 inputLastWeekWinningNumber(), ConvertToListWinningNumberInput() 구현

--- src/main/java/util/InputUtil.java
+++ src/main/java/util/InputUtil.java
@@ -1,11 +1,29 @@
 package util;
 
 import domain.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputUtil {
-        public int inputPurchaseAmount(){
+        public int inputPurchaseAmount() {
                 Scanner scan = new Scanner(System.in);
                 return scan.nextInt();
+        }
+
+        public List<Integer> inputLastWeekWinningNumber() {
+                Scanner scan = new Scanner(System.in);
+                String winningNumberInput = scan.nextLine();
+                return ConvertToListWinningNumberInput(winningNumberInput);
+        }
+
+        private List<Integer> ConvertToListWinningNumberInput(String winningNumberInput){
+                List<Integer> winningNumber = new ArrayList<Integer>();
+                String[] winningNumberString = winningNumberInput.split(",");
+                for (String OneOfWinningNumber : winningNumberString) {
+                        winningNumber.add(Integer.parseInt(OneOfWinningNumber));
+                }
+                return winningNumber;
         }
 }
