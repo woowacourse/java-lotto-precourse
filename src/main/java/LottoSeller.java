dc9b0bb108971036b9ffc7295c1aab28e93d843b
LeeJaeilHan <wodlf1212@hanyang.ac.kr>
2019-04-10 15:10:09 +0900

    [feature : sell Lotto] implement LottoSeller class

---
+++ src/main/java/LottoSeller.java
@@ -0,0 +1,19 @@
+import domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoSeller {
+    private static final int lottoPrice = 1000;
+
+    public static int giveLottos(int money, List<Lotto> list){
+        int numOfLotto = Integer.valueOf(money)/lottoPrice;
+        System.out.println(numOfLotto+"개를 구매했습니다.");
+        for(int i=0; i<numOfLotto; i++) {
+            Lotto lotto = LottoCreator.createLotto();
+            list.add(lotto);
+            System.out.println(lotto);
+        }
+        return numOfLotto * lottoPrice;
+    }
+}
