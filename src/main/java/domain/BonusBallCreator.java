f87aba0ec2c9b63a5e1239f49e3f7281e891cdbb
men7627 <men1219@hanmail.net>
2019-04-11 00:27:07 +0900

    BonusBallCreator클래스 및 생성자 구현

--- src/main/java/domain/BonusBallCreator.java
+++ src/main/java/domain/BonusBallCreator.java
@@ -6,5 +6,8 @@ import util.PrintUtil;
 public class BonusBallCreator implements Creator {
         @Override
         public BonusBall create() {
+                PrintUtil.printBonusBallInputMessage();
+                BonusBall bonusBall = new BonusBall(InputUtil.inputBonusBall());
+                return bonusBall;
         }
 }
