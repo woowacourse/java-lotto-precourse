802df1c98407e96dc3a2fc85d0dec0a56ef5d177
samsung <samsung@LAPTOP-859NPSQ7>
2019-04-10 16:05:48 +0900

    사용자 로또에 해당하는 Rank를 찾음

--- src/main/java/domain/LottoGameManager.java
+++ src/main/java/domain/LottoGameManager.java
@@ -1,15 +1,31 @@
 package domain;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /*
  * 당첨 통계와 수익률을 찾는 객체
  */
 public class LottoGameManager {
+	public static LinkedHashMap<Rank, Integer> initLottoResult() {
+		// 사용자 로또가 해당 Rank에 몇 개 해당되는 지 저장
+		LinkedHashMap<Rank, Integer> lottoResult = new LinkedHashMap<Rank, Integer>();
+		lottoResult.put(Rank.MISS, 0);
+		lottoResult.put(Rank.FIFTH, 0);
+		lottoResult.put(Rank.FOURTH, 0);
+		lottoResult.put(Rank.THIRD, 0);
+		lottoResult.put(Rank.SECOND, 0);
+		lottoResult.put(Rank.FIRST, 0);
+		return lottoResult;
+	}
 
 	public static void main(String[] args) {
 		List<Lotto> userLottos = new LottoGenerator().generateUserLottos();
 		WinningLotto winningLotto = new WinningLottoGenerator().generateWinningLotto();
+		LinkedHashMap<Rank, Integer> lottoResult = initLottoResult();
+		for (int i = 0; i < userLottos.size(); i++) {
+			Rank rank = winningLotto.match(userLottos.get(i));
+			lottoResult.put(rank, lottoResult.get(rank) + 1);
+		}
 	}
-
 }
--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -1,19 +1,25 @@
 package domain;
 
+import java.util.List;
+
 /**
  * 당첨 번호를 담당하는 객체
  */
 public class WinningLotto {
-    private final Lotto lotto;
-    private final int bonusNo;
+	private final Lotto lotto;
+	private final int bonusNo;
 
-    public WinningLotto(Lotto lotto, int bonusNo) {
-        this.lotto = lotto;
-        this.bonusNo = bonusNo;
-    }
+	public WinningLotto(Lotto lotto, int bonusNo) {
+		this.lotto = lotto;
+		this.bonusNo = bonusNo;
+	}
 
-    public Rank match(Lotto userLotto) {
-        // TODO 로직 구현
-        return null;
-    }
+	public Rank match(Lotto userLotto) {
+		int matchCount = 0;
+		List<Integer> userLottoNum = userLotto.getNumbers();
+		for (Integer num : lotto.getNumbers()) {
+			matchCount += userLottoNum.contains(num) ? 1 : 0;
+		}
+		return Rank.valueOf(matchCount, userLottoNum.contains(bonusNo));
+	}
 }
