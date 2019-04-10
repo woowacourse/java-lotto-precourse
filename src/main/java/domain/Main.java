3bdd0a742e4227cecf8c24d30647bcee09647489
이영현 <leey9494@icloud.com>
2019-04-11 02:41:15 +0900

    컨벤션에 맞게 수정

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,5 +1,15 @@
 package domain;
 
+/*
+ * 이름 : 로또 게임
+ *
+ * 버전 정보 : version 1
+ *
+ * 날짜 : 2019/4/11
+ *
+ * 제작자 : LeeYoungHyeon
+ */
+
 public class Main {
     public static void main(String[] args) {
         LottoGame lottoGame = new LottoGame();
