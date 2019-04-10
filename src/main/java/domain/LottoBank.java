22c1b010e8fdd14c82e303874dda8762ba1820b8
wschoi8640 <cws911112@gmail.com>
2019-04-10 14:53:40 +0900

    UPDATE LottoBank.java : ADD setWinningLotto()

--- src/main/java/domain/LottoBank.java
+++ src/main/java/domain/LottoBank.java
@@ -1,5 +1,33 @@
 package domain;
 
-public class LottoBank {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
+public class LottoBank {
+	
+	private WinningLotto winLotto;
+	private LottoShop lottoShop;
+	private Scanner sc;
+	private List<Integer> winnerLottoNumbers;
+	
+	public LottoBank(LottoShop lottoShop) {
+		sc = new Scanner(System.in);
+		this.lottoShop = lottoShop;
+		winnerLottoNumbers = new ArrayList<Integer>();
+	}
+
+	public void setWinningLotto() {
+		try {
+			System.out.println(RequestMsg.WinningLotto.getMsg());
+			parseWinnerLottoNums(checkValid(sc.nextLine()));
+			System.out.println(RequestMsg.BonusNum.getMsg());
+			bonusNo = checkValid(sc.nextInt());
+		} catch (InputMismatchException e) {
+			printAndfixErr(ErrMsg.InputMismatchErr.getMsg());
+		}
+		winLotto = new WinningLotto(new Lotto(winnerLottoNumbers), bonusNo);
+	}
 }
