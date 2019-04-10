9e0edd93f5b54ffcaea2c1e36977ff4ec4468cc0
kmdngyu <kmdngyu@gmail.com>
2019-04-10 12:18:24 +0900

    fix : 로또 구입 금액 입력 / 상수 설정 및 출력

--- src/main/java/domain/util/Input.java
+++ src/main/java/domain/util/Input.java
@@ -2,15 +2,17 @@ package domain.util;
 
 import java.util.InputMismatchException;
 public class Input {
+    public static final int MIN_INPUT_MONEY = 1_000;
+    public static final int MAX_INPUT_MONEY = 2_147_483_647;
 
     public static int insertMoney(){
         int insertedMoney = 0;
         try {
             insertedMoney = PrintScan.requesetInputMoney();
-            CheckException.checkInsertedMoneyIsValid(insertedMoney,1000,2147483647);
+            CheckException.checkInsertedMoneyIsValid(insertedMoney, MIN_INPUT_MONEY, MAX_INPUT_MONEY);
             return insertedMoney;
         } catch (InputMismatchException | IllegalArgumentException e) {
-            PrintScan.exeptionPrint();
+            PrintScan.printOutofRangeNotice();
             return insertMoney();
         }
     }
--- src/main/java/domain/util/PrintScan.java
+++ src/main/java/domain/util/PrintScan.java
@@ -3,17 +3,15 @@ package domain.util;
 import java.util.Scanner;
 
 public class PrintScan {
-    private static final String REQUEST_INPUT_MONEY_TEXT = "로또를 구매할 금액을 입력해 주세요. 로또는 장당 1000원 입니다";
-
     private static Scanner scan = new Scanner(System.in);
 
     public static int requesetInputMoney () {
-        System.out.println(REQUEST_INPUT_MONEY_TEXT);
+        System.out.format("로또를 구매할 금액을 입력해 주세요. 로또는 장당 %d원 입니다.%n",Input.MIN_INPUT_MONEY);
         return Integer.parseInt(scan.nextLine());
     }
 
-    public static void exeptionPrint(){
-        System.out.println("1,000이상 2,147,483,647미만의 숫자를 입력해 주세요");
+    public static void printOutofRangeNotice(){
+        System.out.format("%d이상 %d이하의 숫자를 입력해 주세요.%n",Input.MIN_INPUT_MONEY,Input.MAX_INPUT_MONEY);
     }
 
 
