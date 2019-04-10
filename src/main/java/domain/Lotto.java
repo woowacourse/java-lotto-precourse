9ed8ee46c2cb4d3351540295df426a9f4a7c9094
483759 <483759@naver.com>
2019-04-11 00:30:16 +0900

    사용자의 로또 목록을 출력하는 기능 구현

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -6,12 +6,20 @@ import java.util.List;
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
-    private final static int lottoCount = 25;
+    private final static int oneLottoAmount = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
     }
 
+    public void PrintLottoNum(){
+        System.out.print("[");
+        for (int i = 0; i < oneLottoAmount-1; i++) {
+            System.out.print(numbers.get(i)+",");
+        }
+        System.out.print(numbers.get(oneLottoAmount-1));
+        System.out.println("]");
+    }
     // 추가 기능 구현
 }
--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,19 +1,20 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class LottoGame {
-  public static int oneLottoPrice = 1000;
-  public static int oneLottoAmount = 6;
-  public static int maxLottoNum = 45;
+  private final static int oneLottoPrice = 1000;
+  private final static int oneLottoAmount = 6;
+  private final static int maxLottoNum = 45;
+  private final static int lottoCount = 25;
 
   public int purchasePrice;
   public int purchaseAmount;
-  public int lottoAmount;
 
   private boolean IsValidAmount(int purchasePrice){
     if(purchasePrice < 0){
@@ -37,35 +38,34 @@ public class LottoGame {
     return purchasePrice;
   }
 
-  private boolean IsAlreadyNumber(int oneNumber, boolean[] DuplicateCheck){
-    if(DuplicateCheck[oneNumber])
+  private boolean IsAlreadyNumber(int oneNumber, boolean[] duplicateCheck){
+    if(duplicateCheck[oneNumber])
       return false;
     return true;
   }
 
-  private int MakeRandomNum(boolean[] DuplicateCheck){
+  private int MakeRandomNum(boolean[] duplicateCheck){
     Random random = new Random();
     int randomNum;
     do{
-      randomNum = random.nextInt(45)+1;
-    }while(!IsAlreadyNumber(randomNum, DuplicateCheck));
-    DuplicateCheck[randomNum] = true;
+      randomNum = random.nextInt(maxLottoNum)+1;
+    }while(!IsAlreadyNumber(randomNum, duplicateCheck));
+    duplicateCheck[randomNum] = true;
     return randomNum;
   }
 
-  private List<Integer> MakeLottoCombination(){
-    List<Integer> oneLottoList = new ArrayList(8);
-    boolean[] DuplicateCheck = new boolean[46];
+  private List<Integer> MakeLottoCombination(boolean[] duplicateCheck){
+    List<Integer> oneLottoList = new ArrayList(oneLottoAmount);
     for(int i=1;i<=maxLottoNum;i++){
-      DuplicateCheck[i] = false;
+      duplicateCheck[i] = false;
     }
     for(int i=0;i<oneLottoAmount;i++){
-      oneLottoList.add(MakeRandomNum(DuplicateCheck));
+      oneLottoList.add(MakeRandomNum(duplicateCheck));
     }
     return oneLottoList;
   }
 
-  public void StartLotto(){
+  private void InputUserPurchase(){
     purchasePrice = InputPurchasePrice();
     if(!IsValidAmount(purchasePrice)) {
       System.out.println("잘못된 금액입니다.");
@@ -73,7 +73,30 @@ public class LottoGame {
     }
     purchaseAmount = purchasePrice/oneLottoPrice;
     System.out.println("\n"+purchaseAmount+"개를 구매했습니다.");
-    List<Integer> oneLottoCombination = MakeLottoCombination();
+  }
+
+  public Lotto MakeOneLotto(boolean[] duplicateCheck){
+    List<Integer> oneLottoCombination = MakeLottoCombination(duplicateCheck);
+    Lotto oneLotto = new Lotto(oneLottoCombination);
+    return oneLotto;
+  }
+
+  public List<Lotto> MakeUserLottoList(){
+     boolean[] duplicateCheck = new boolean[maxLottoNum+1];
+     List<Lotto> userLotto = new ArrayList();
+     for(int i=0;i<purchaseAmount;i++) {
+      userLotto.add(MakeOneLotto(duplicateCheck));
+      userLotto.get(i).PrintLottoNum();
+    }
+
+    return userLotto;
+  }
+
+  public void StartLotto(){
+    this.InputUserPurchase();
+    if(purchaseAmount==-1)
+      return;
+    List<Lotto> userLotto = MakeUserLottoList();
     /*
     for (int i = 0; i < oneLottoAmount; i++) {
       System.out.print(list.get(i) + " ");
