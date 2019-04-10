3d47dad27331522748c1e0f89d66a1833d93fee9
men7627 <men1219@hanmail.net>
2019-04-11 04:36:44 +0900

    Game클래스 start() 구현

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -3,14 +3,18 @@ package domain;
 import creator.WinningLottoCreator;
 import object.mainobject.PurchaseInfo;
 import object.WinningLotto;
+import object.mainobject.WinningStats;
 
 public class Game {
-        Game(){}
+        Game() {
+        }
 
-        public void start(){
+        public void start() {
                 PurchaseInfo purchaseInfo = new PurchaseInfo();
                 purchaseInfo.printPurchasedLottosNumber();
                 WinningLottoCreator creator = new WinningLottoCreator();
                 WinningLotto winningLotto = creator.create();
+                WinningStats winningStats = new WinningStats(purchaseInfo, winningLotto);
+                winningStats.printStats();
         }
 }
