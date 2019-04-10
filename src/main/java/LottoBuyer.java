84a5217c51b2d1a40fef0060624e75f8f3c6603a
LeeJaeilHan <wodlf1212@hanyang.ac.kr>
2019-04-10 19:22:38 +0900

    [feature : buy Lotto] change LottoBuyer class

--- src/main/java/LottoBuyer.java
+++ src/main/java/LottoBuyer.java
@@ -2,17 +2,35 @@ import domain.Lotto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class LottoBuyer {
-    private List<Lotto> list = new ArrayList<>();
-    private int money;
+    private List<Lotto> lottoList = new ArrayList<>();
+    private int initMoney;
+    private int curMoney;
 
     public void buyLottos(){
-        int cost = LottoSeller.giveLottos(money,list);
-        money -= cost;
+        int cost = LottoSeller.giveLottos(initMoney, lottoList);
+        curMoney -= cost;
     }
 
-    public LottoBuyer(int money) {
-        this.money = money;
+    public void setMoney() {
+        Scanner sc = new Scanner(System.in);
+        int money;
+        while(true){
+            System.out.println("구입금액을 입력해 주세요.");
+            money = Integer.valueOf(sc.nextLine());
+            if(money>=1000) break;
+            System.out.println("1000원 이상의 구입금액을 입력해주세요");
+        }
+        this.initMoney = money;
+    }
+
+    public int getInitMoney() {
+        return initMoney;
+    }
+
+    public List<Lotto> getLottoList() {
+        return lottoList;
     }
 }
