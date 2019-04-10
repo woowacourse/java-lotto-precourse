9d0290de20b8a6fbe9b5268504061b729df7dcdb
andole87 <andole87@gmail.com>
2019-04-10 10:56:20 +0900

    Create LottoMain.java
    게임의 기본 흐름을 작성. 추후 리팩토링 예정.

--- src/main/java/domain/UserLottos.java
+++ src/main/java/domain/UserLottos.java
@@ -15,18 +15,26 @@ public class UserLottos {
             for (int i = 1; i <= 45; i ++){
                 basicLottoNumbers.add(i);
             }
+            System.out.println(buyToLottoMoney + "개를 구매했습니다.");
             for (int i = 1; i <= buyToLottoMoney; i++){
                 Collections.shuffle(basicLottoNumbers);
-                List<Integer> selectedLottoNumbers = new ArrayList<>(basicLottoNumbers.subList(0,5));
+                List<Integer> selectedLottoNumbers = new ArrayList<>(basicLottoNumbers.subList(0,6));
+                System.out.println(selectedLottoNumbers);
                 userLottos.add(new Lotto(selectedLottoNumbers));
             }
         }catch (NumberFormatException ex){
             throw new NumberFormatException("구입금액은 숫자만 가능합니다.");
         }
     }
-
     public int getUserLottosCount(){
         return userLottos.size();
+    }
+
+    public void checkWinningLotto(WinningLotto winningLotto){
+        for (Lotto lotto : userLottos){
+            System.out.println(winningLotto.match(lotto).getCountOfMatch());
+
+        }
     }
 
 
--- src/test/java/domain/WinningLottoTest.java
+++ src/test/java/domain/WinningLottoTest.java
@@ -8,21 +8,44 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class WinningLottoTest {
+
+    public List<Integer> getNumberOfList (int[] inputnumbers){
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 6; i ++){
+            numbers.add(inputnumbers[i]);
+        }
+        return numbers;
+    }
+
     @Test
     public void matchTest() {
-        List<Integer> lottoNumbers = new ArrayList<>();
-
-        lottoNumbers.add(1);
-        lottoNumbers.add(2);
-        lottoNumbers.add(3);
-        lottoNumbers.add(4);
-        lottoNumbers.add(5);
-        lottoNumbers.add(6);
+        List<Integer> lottoNumbers;
+        lottoNumbers = getNumberOfList(new int[]{1,2,3,4,5,6});
 
         Lotto lotto = new Lotto(lottoNumbers);
-
         WinningLotto winningLotto = new WinningLotto(lotto,7);
-
         assertEquals(Rank.FIRST, winningLotto.match(lotto));
+
+        lottoNumbers = getNumberOfList(new int[]{1,2,3,4,5,7});
+        Lotto lotto1 = new Lotto(lottoNumbers);
+        assertEquals(Rank.SECOND, winningLotto.match(lotto1));
+
+        lottoNumbers = getNumberOfList(new int[]{3,4,5,6,2,8});
+        Lotto lotto2 = new Lotto(lottoNumbers);
+        assertEquals(Rank.THIRD, winningLotto.match(lotto2));
+
+        lottoNumbers = getNumberOfList(new int[]{4,5,6,1,7,8});
+        Lotto lotto3 = new Lotto(lottoNumbers);
+        assertEquals(Rank.FOURTH, winningLotto.match(lotto3));
+
+        lottoNumbers = getNumberOfList(new int[]{4,5,6,9,8,10});
+        Lotto lotto4 = new Lotto(lottoNumbers);
+        assertEquals(Rank.FIFTH, winningLotto.match(lotto4));
+
+        lottoNumbers = getNumberOfList(new int[]{1,2,8,9,10,11});
+        Lotto lotto5 = new Lotto(lottoNumbers);
+        assertEquals(Rank.MISS, winningLotto.match(lotto5));
+
     }
+
 }
