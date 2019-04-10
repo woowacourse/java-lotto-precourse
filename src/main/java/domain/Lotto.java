39c5aea00c500131e4023e737bf8d8cff7b70876
jasoncho1021 <jhcho1021@gmail.com>
2019-04-10 20:34:14 +0900

    기능 목록 작성 및 구입금액 입력 기능 구현
    1. 기능 목록 초안 작성
    2. 구입금액 예외 처리 구현

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 /**
  * 로또 한장을 의미하는 객체
+ * 
+ * @version 1.00 2019/04/10
+ * @author 조재훈
  */
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		this.numbers = numbers;
+	}
 
-    // 추가 기능 구현
+	// 추가 기능 구현
 }
---
+++ src/main/java/service/Dealer.java
@@ -0,0 +1,13 @@
+package service;
+
+/**
+ * @version 1.00 2019/04/10
+ * @author 조재훈
+ */
+public class Dealer {
+
+	public static void main(String[] args) {
+		InputManager inputManager = new InputManager();
+		int money = inputManager.getMoney();
+	}
+}
