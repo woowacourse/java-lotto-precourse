56d2c1e995ce65d9e796db61b02bc436faab80a5
Soojin Roh <sooreal91@gmail.com>
2019-04-11 01:12:23 +0900

    로또 리스트 생성
    게임을 진행하는 LottoGame 클래스에서 구입한 매수 만큼의 로또 객체를 생성하는 기능 구현

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,8 +1,20 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoGame {
+	
+	LottoGenerator lottoGenerator = new LottoGenerator();
 
-    public static void main(String[] args) {
-
-	}
+    private List<Lotto> makeLotto() {
+    	ArrayList<Lotto> lottos = lottoGenerator.makeLotto();
+    	
+    	for (int i = 0; i < lottos.size(); i++) {
+    		System.out.println(lottos.get(i).toString());
+    	}
+    	
+    	return lottos;
+    }
+	
 }
