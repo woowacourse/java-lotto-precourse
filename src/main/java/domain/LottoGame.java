4c7ce1c6f866637dbe2049f92a716aceffdb4202
U-Yeon <leanner@naver.com>
2019-04-10 21:29:08 +0900

    예외경우(숫자 입력시 공백이 들어갈때)의 처리

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -17,11 +17,12 @@ public class LottoGame {
 		System.out.println(lottoCount + "개를 구매했습니다.");
 		List<Lotto> lottoList = makeLottoList(lottoCount);
 		List<Rank> rankList = new ArrayList<Rank>();
-		Lotto winningNumber; 
+		Lotto winningNumber;
 		scan.nextLine();
-		int totalReward=0;
-		WinningLotto winLotto = new WinningLotto(winningNumber = new Lotto(scanWinningNumber()), scanBonusNumber(winningNumber));
-		for(int i=0; i<lottoCount; ++i) {
+		int totalReward = 0;
+		WinningLotto winLotto = new WinningLotto(winningNumber = new Lotto(scanWinningNumber()),
+				scanBonusNumber(winningNumber));
+		for (int i = 0; i < lottoCount; ++i) {
 			rankList.add(winLotto.match(lottoList.get(i)));
 		}
 		printResult(rankList, price);
@@ -30,18 +31,19 @@ public class LottoGame {
 
 	public static int getPrice() {
 		System.out.println("구입 금액을 입력해 주세요.");
-		int price = scan.nextInt();;
-		if(isValidPrice(price)) {
+		int price = scan.nextInt();
+		;
+		if (isValidPrice(price)) {
 			price = scan.nextInt();
 		}
-		if(price%LOTTO_PRICE!=0) {
-			System.out.println("거스름돈은 "+price%1000+"원 입니다.");
+		if (price % LOTTO_PRICE != 0) {
+			System.out.println("거스름돈은 " + price % 1000 + "원 입니다.");
 		}
 		return price;
 	}
-	
+
 	public static boolean isValidPrice(int price) {
-		if(price<LOTTO_PRICE) {
+		if (price < LOTTO_PRICE) {
 			System.out.println("최소 구입 금액은 1000원입니다.");
 			System.out.println("다시 입력해주십시오.");
 			return true;
@@ -83,113 +85,112 @@ public class LottoGame {
 		}
 		return lottoList;
 	}
-	
+
 	public static List<Integer> getWinningNumber() {
 		List<Integer> winningNumber = new ArrayList<Integer>();
 		String[] input = scan.nextLine().split(",");
-		if(input.length!=LOTTO_NUM_LENGTH) {
+		if (input.length != LOTTO_NUM_LENGTH) {
 			System.out.println("숫자의 갯수가 6개가 넘습니다!\n다시 입력해주세요.");
 			return getWinningNumber();
 		}
-		for(int i=0; i<LOTTO_NUM_LENGTH; ++i) {
-			winningNumber.add(Integer.parseInt(input[i]));
+		for (int i = 0; i < LOTTO_NUM_LENGTH; ++i) {
+			winningNumber.add(Integer.parseInt(input[i].trim()));
 		}
 		return winningNumber;
 	}
-	
+
 	public static boolean isNotWinningValidRange(List<Integer> winningNumber) {
 		boolean result = false;
-		for(int i=0; i<LOTTO_NUM_LENGTH; ++i) {
-			result |= (winningNumber.get(i)>45 || winningNumber.get(i)<1);
+		for (int i = 0; i < LOTTO_NUM_LENGTH; ++i) {
+			result |= (winningNumber.get(i) > 45 || winningNumber.get(i) < 1);
 		}
 		return result;
 	}
-	
+
 	public static void printErrorMessage(int tryNum) {
-		if(tryNum>0) {
+		if (tryNum > 0) {
 			System.out.println("중복 숫자와 범위 밖 숫자는 입력할 수 없습니다.");
 			System.out.println("다시 입력하십시오.");
 		}
 	}
-	
+
 	public static List<Integer> scanWinningNumber() {
 		List<Integer> winningNumber;
 		System.out.println("\n지난 추 당첨 번호를 입력해 주세요.");
-		int tryNum=0;
+		int tryNum = 0;
 		do {
 			printErrorMessage(tryNum);
 			winningNumber = getWinningNumber();
 			Collections.sort(winningNumber);
 			++tryNum;
-		}while(isNotValid(winningNumber)||isNotWinningValidRange(winningNumber));
+		} while (isNotValid(winningNumber) || isNotWinningValidRange(winningNumber));
 		return winningNumber;
 	}
-	
-	
+
 	public static boolean isAlready(Lotto winningNumber, int bonusNumber) {
 		boolean result = false;
-		for(int i=0; i<LOTTO_NUM_LENGTH; ++i) {
-			result |= (winningNumber.getNumbers().get(i)==bonusNumber);
+		for (int i = 0; i < LOTTO_NUM_LENGTH; ++i) {
+			result |= (winningNumber.getNumbers().get(i) == bonusNumber);
 		}
-		
+
 		return result;
 	}
-	
+
 	public static boolean isNotBonusValidRange(int bonusNumber) {
 		boolean result = false;
-		if(bonusNumber>45||bonusNumber<1) {
+		if (bonusNumber > 45 || bonusNumber < 1) {
 			return true;
 		}
 		return false;
-		
+
 	}
-	
+
 	public static int scanBonusNumber(Lotto winningNumber) {
 		System.out.println("보너스 볼을 입력해주세요.");
 		int bonusNumber = scan.nextInt();
-		while(isAlready(winningNumber, bonusNumber)||isNotBonusValidRange(bonusNumber)) {
+		while (isAlready(winningNumber, bonusNumber) || isNotBonusValidRange(bonusNumber)) {
 			System.out.println("당첨 숫자와 중복되는 수와 범위 밖 숫자는 입력할 수 없습니다.");
 			System.out.println("다시 입력하십시오.");
-			bonusNumber=scan.nextInt();
+			bonusNumber = scan.nextInt();
 		}
 		return bonusNumber;
 	}
-	
+
 	public static int matchCondition(Rank userNum, Rank lottoNum) {
 		if (userNum.equals(lottoNum)) {
 			return 1;
 		}
 		return 0;
 	}
-	
+
 	public static int matchCount(List<Rank> rankList, Rank rank) {
 		int count = 0;
-		for (int i=0; i<rankList.size(); ++i) {
+		for (int i = 0; i < rankList.size(); ++i) {
 			count += matchCondition(rankList.get(i), rank);
 		}
 		return count;
 	}
-	
+
 	public static void printBonus(Rank rank) {
-		if(rank == Rank.SECOND) {
+		if (rank == Rank.SECOND) {
 			System.out.print(", 보너스 볼 일치");
 			return;
 		}
 		System.out.print(" ");
 	}
-	
+
 	public static void printResult(List<Rank> rankList, int myPrice) {
 		int count = 0;
 		int totalReward = 0;
 		Rank[] rankTypes = Rank.values();
-		for (int i = LOTTO_WINNING_MIN_COUNT+1; i>=0; --i) {
+		for (int i = LOTTO_WINNING_MIN_COUNT + 1; i >= 0; --i) {
 			Rank rank = rankTypes[i];
 			count = matchCount(rankList, rank);
-			System.out.print(rank.getCountOfMatch() +"개 일치");
+			System.out.print(rank.getCountOfMatch() + "개 일치");
 			printBonus(rank);
-			System.out.println("("+rank.getWinningMoney() + "원) - " +count+"개");
-			totalReward += count *rank.getWinningMoney();
+			System.out.println("(" + rank.getWinningMoney() + "원) - " + count + "개");
+			totalReward += count * rank.getWinningMoney();
 		}
-		System.out.println("총 수익률은" +(double) totalReward/myPrice + "입니다.");
+		System.out.println("총 수익률은" + (double) totalReward / myPrice + "입니다.");
 	}
 }
--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -1,37 +1,38 @@
 package domain;
 
 import java.util.List;
+
 /**
  * 당첨 번호를 담당하는 객체
  */
 public class WinningLotto {
-    private final Lotto lotto;
-    private final int bonusNo;
+	private final Lotto lotto;
+	private final int bonusNo;
 
-    public WinningLotto(Lotto lotto, int bonusNo) {
-        this.lotto = lotto;
-        this.bonusNo = bonusNo;
-    }
+	public WinningLotto(Lotto lotto, int bonusNo) {
+		this.lotto = lotto;
+		this.bonusNo = bonusNo;
+	}
 
-    public Rank match(Lotto userLotto) {
-    	List<Integer> userLottoNum = userLotto.getNumbers();
-    	int countOfMatch = 0;
-    	boolean matchOfBonus = false;
-    	for(int i=0; i<LottoGame.LOTTO_NUM_LENGTH; ++i) {
-    		countOfMatch += getCountOfMatch(userLottoNum.get(i));
-    		matchOfBonus |= (bonusNo ==userLottoNum.get(i));
-    	}
-    	return Rank.valueOf(countOfMatch, matchOfBonus);
-    }
-    
-    public int getCountOfMatch(int userLottoNum) {
-    	boolean isMatched = false;
-    	for(int i=0; i<LottoGame.LOTTO_NUM_LENGTH; ++i) {
-    		isMatched |= (userLottoNum == this.lotto.getNumbers().get(i));
-    	}
-    	if(isMatched) {
-    		return 1;
-    	}
-    	return 0;
-    }
+	public Rank match(Lotto userLotto) {
+		List<Integer> userLottoNum = userLotto.getNumbers();
+		int countOfMatch = 0;
+		boolean matchOfBonus = false;
+		for (int i = 0; i < LottoGame.LOTTO_NUM_LENGTH; ++i) {
+			countOfMatch += getCountOfMatch(userLottoNum.get(i));
+			matchOfBonus |= (bonusNo == userLottoNum.get(i));
+		}
+		return Rank.valueOf(countOfMatch, matchOfBonus);
+	}
+
+	public int getCountOfMatch(int userLottoNum) {
+		boolean isMatched = false;
+		for (int i = 0; i < LottoGame.LOTTO_NUM_LENGTH; ++i) {
+			isMatched |= (userLottoNum == this.lotto.getNumbers().get(i));
+		}
+		if (isMatched) {
+			return 1;
+		}
+		return 0;
+	}
 }
