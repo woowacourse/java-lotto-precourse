54931212b7b66ef164ae43d53113fff6621f07bf
Unknown <9105lgm@naver.com>
2019-04-10 15:33:50 +0900

    로또 구입, 로또 숫자생성, 로또 숫자 출력 관련 기능 구현
    로또 구입액에 따른 산 로또 갯수 및 거스름 돈 반환
    각 구입한 로또마다 숫자 중복되지 않게 7개 숫자 생성
    구입한 모든 로또의 숫자 출력

--- src/main/java/domain/Controller.java
+++ src/main/java/domain/Controller.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -9,8 +11,11 @@ public class Controller {
     private static final int MINIMUM_MONEY = 1000;      //로또 구입할수 있는 최소 가격
     private static final int MAXIMUM_MONEY = 100000;    //로또 구입할수 있는 최대 가격
     Scanner scanner = new Scanner(System.in);
+    private static final int LOTTOS_NUMBER = 7;         //로또의 번호 갯수
+    private static final int LOTTOS_LIMIT_NUMBER = 45;  //로또번호 한도 숫자
 
     private int money;
+    private List<Lotto> lottos = new ArrayList<>();
 
     public void askHowMany() {
         String inputValue;
@@ -34,5 +39,54 @@ public class Controller {
 
     public boolean checkInputRight() {
         return ((money >= MINIMUM_MONEY) && (money <= MAXIMUM_MONEY));
+    }
+
+    public void buyLotto() {
+        int lottoAmount = calculateLottoAmount();
+
+        System.out.printf("%d개를 구매했습니다.\n", lottoAmount);
+
+        for (int i = 0; i < lottoAmount; i++) {
+            lottos.add(new Lotto(makeRandomNumber()));
+        }
+    }
+
+    /* 받은 돈으로 살수 있는 로또 갯수와 거스름돈 반환 */
+    public int calculateLottoAmount() {
+        if ((money % MINIMUM_MONEY) != 0) {
+            System.out.printf("%d 남았습니다.\n", money % MINIMUM_MONEY);
+        }
+
+        return money / MINIMUM_MONEY;
+    }
+
+    public List<Integer> makeRandomNumber() {
+        List<Integer> randomNumberList = new ArrayList<>();
+        int randomNum;
+
+        do {
+            randomNum = (int) (Math.random() * LOTTOS_LIMIT_NUMBER) + 1;
+        } while (checkNumberToList(randomNum, randomNumberList));
+
+        return randomNumberList;
+    }
+
+    /* 생성된 숫자가 중복되는지 확인 및 7개 만들어졌는지 확인 하는 메소드 */
+    public boolean checkNumberToList(int randomNum, List<Integer> randomNumberList) {
+        if (randomNumberList.size() == LOTTOS_NUMBER) {
+            return false;
+        }
+
+        if (!randomNumberList.contains(randomNum)) {
+            randomNumberList.add(randomNum);
+        }
+        return true;
+    }
+
+    /* 만들어진 로또들의 숫자 전부 보여주기 */
+    public void showLottosNumber() {
+        for (Lotto each : lottos) {
+            System.out.println(each.getNumbersList().toString());
+        }
     }
 }
