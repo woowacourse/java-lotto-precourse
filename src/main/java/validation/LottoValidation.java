35efc80538ab6aadfc887674e537a2a3f09c1c7b
hyperpace <cnk9109@gmail.com>
2019-04-11 16:09:02 +0900

    Add LottoValidation

---
+++ src/main/java/validation/LottoValidation.java
@@ -0,0 +1,7 @@
+package validation;
+
+public class LottoValidation {
+	public static final int MIN_NUMBER = 1;
+	public static final int MAX_NUMBER = 45;
+	public static final int NUMBER_CNT = 6;
+}
