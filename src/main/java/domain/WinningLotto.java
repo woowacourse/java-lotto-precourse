cb0e92ce7d170194a37ecc8642e10a8085b4b350
starhn87 <starhn87@gmail.com>
2019-04-11 17:38:44 +0900

    존재 여부를 판단하는 상수를 만든다

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -1,9 +1,14 @@
 package domain;
 
+import java.util.List;
+
+import static domain.Rank.valueOf;
+
 /**
  * 당첨 번호를 담당하는 객체
  */
 public class WinningLotto {
+    private static final int EXIST = 1;
     private final Lotto lotto;
     private final int bonusNo;
 
