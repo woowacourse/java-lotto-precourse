fc64ee4e01aab8aecd6b0c4b5a2c1d0520ae7188
kimss90 <kimss9990@gmail.com>
2019-04-11 21:02:10 +0900

    Add functionality to set winning numbers and bonus number

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -8,6 +8,11 @@ import java.util.ArrayList;
 public class Main {
 	static final String AMOUNT_QUESTION = "구입금액을 입력해 주세요.";
 	static final String BOUGHT_STATEMENT = "%d개를 구매했습니다.\n";
+	static final String LAST_WINNING_NUMBER_QUESTION = "지난 주 당첨 번호를 입력해 주세요.";
+	static final String BONUS_NUMBER_QUESTION = "보너스 볼을 입력해 주세요.";
+	
+	public static int[] winningNumbers = new int[46];
+	public static int bonusWinningNumber = 0;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -23,6 +28,22 @@ public class Main {
 		
 		initializeLottos(userLottos);
 		printLottos(userLottos);
+		
+		System.out.println(LAST_WINNING_NUMBER_QUESTION);
+		String winningNumbersInput = in.readLine();
+		setWinningNumber(winningNumbersInput);
+		
+		System.out.println(BONUS_NUMBER_QUESTION);
+		bonusWinningNumber = Integer.parseInt(in.readLine());
+		
+	}
+	
+	private static void setWinningNumber(String winningNumbersInput) {
+		String[] winningNumberArr = winningNumbersInput.split(",");
+		for(int i = 0; i < winningNumberArr.length; i++) {
+			int currWinningNumber = Integer.parseInt(winningNumberArr[i]);
+			++winningNumbers[currWinningNumber];
+		}
 	}
 	
 	private static void printLottos(Lotto[] userLottos) {
