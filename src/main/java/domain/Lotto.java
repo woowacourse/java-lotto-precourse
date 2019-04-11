e4cd22a14c948d4fadce81a039c512ec2da05ddb
kimss90 <kimss9990@gmail.com>
2019-04-11 20:40:24 +0900

    Add functionality for picking random numbers

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,16 +1,30 @@
 package domain;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
     private final List<Integer> numbers;
-
+    Random r = new Random();
+    
     public Lotto(List<Integer> numbers) {
         this.numbers = numbers;
     }
+    
+    
 
     // 추가 기능 구현
+    
+    public void setWinningNumbers() {
+    	int randInt;
+    	while(numbers.size() < 6) {
+    		randInt = (Math.abs(r.nextInt()) % 65) + 1;
+    		if(!numbers.contains(randInt)) {
+    			numbers.add(randInt);
+    		}
+    	}
+    }
 }
--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -6,14 +6,19 @@ import java.io.InputStreamReader;
 
 public class Main {
 	static final String AMOUNT_QUESTION = "구입금액을 입력해 주세요.";
+	static final String BOUGHT_STATEMENT = "%d개를 구매했습니다.\n";
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println(AMOUNT_QUESTION);
 		
-		int inputMoney = in.read();
+		int inputMoney = Integer.parseInt(in.readLine());
 		
 		int numberOfLottoToBuy = numberOfLotto(inputMoney);
+		
+		System.out.printf(BOUGHT_STATEMENT, numberOfLottoToBuy);
+		
+		Lotto[] lottoNumbers = new Lotto[numberOfLottoToBuy];
 	}
 	
 	private static int numberOfLotto(int inputMoney) {
