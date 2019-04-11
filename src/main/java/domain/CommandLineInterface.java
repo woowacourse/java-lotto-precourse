4c9ba98a4da9229807de4146e2555eeacdfd3cf9
Junmo Han <wnsah052@naver.com>
2019-04-11 20:18:19 +0900

    값의 개수가 한 개만 가능하도록 한다

--- src/main/java/domain/CommandLineInterface.java
+++ src/main/java/domain/CommandLineInterface.java
@@ -16,13 +16,21 @@ package domain;
 
 public class CommandLineInterface {
 		private static final String INPUT_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+		private static final String NUMBER_ERROR = "숫자로 이루어진 값을 하나만 입력해 주세요.(','를 제거해주세요.)";
+		private static final String PRICE_RANGE_ERROR = Validator.MIN_PURCHASE_AMOUNT
+													  + " 이상 "
+													  + Validator.MAX_PURCHASE_AMOUNT
+													  + " 이하의 값을 입력해 주세요.";
 		
 		public static void printInputPurchasePrice() {
 				System.out.println(INPUT_PURCHASE_PRICE);			
 		}
+		
+		public static void printPurchasePrcieNumberError() {
+				System.out.println(NUMBER_ERROR);
+		}
+		
+		public static void printPurchasePriceRangeError() {
+				System.out.println(PRICE_RANGE_ERROR);
+		}
 }
-
-
-
-
-
