b7fa6cf8c2f3d1d97f436ff619dce62a9fc19afe
racoco083 <uytr083@naver.com>
2019-04-11 16:16:44 +0900

    update makeLottoList function

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,20 +1,23 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class User {
-    private List<Lotto> lottolist;
+    private ArrayList<Lotto> lottoList;
     private Scanner scanner;
     private String pay;
+    private int lottoCnt;
 
     User(){
         scanner = new Scanner(System.in);
+        lottoList = new ArrayList<>();
     }
 
     public String inputMoney(){
-        String pay = scanner.nextLine();
+        pay = scanner.nextLine();
         return pay;
     }
 
@@ -23,5 +26,33 @@ public class User {
         return random.nextInt(45) + 1;
     }
 
+    public void makeLottoList(){
+        lottoCnt = Integer.parseInt(pay) / 1000;
 
+        for(int i=0; i<lottoCnt; i++){
+            lottoList.add(makeLotto());
+        }
+    }
+    
+
+    public Lotto makeLotto(){
+        // 초기화가 되나?
+        boolean[] checkNumber = new boolean[46];
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Lotto lotto;
+
+        // indent 1로 나중에 수정하자
+        for(int i=0; i<6; i++){
+            int number = getRandomNumber();
+            if(!checkNumber[number]) {
+                checkNumber[number] = true;
+                numbers.add(number);
+                continue;
+            }
+            i--;
+        }
+
+        lotto = new Lotto(numbers);
+        return lotto;
+    }
 }
