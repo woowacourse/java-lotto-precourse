553d5040430f3388aec8634ae1cef87ee1b7dc27
hyperpace <cnk9109@gmail.com>
2019-04-11 17:07:20 +0900

    Update match() to WinningLotto

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -4,16 +4,20 @@ package domain;
  * 당첨 번호를 담당하는 객체
  */
 public class WinningLotto {
-    private final Lotto lotto;
-    private final int bonusNo;
+	private final Lotto lotto;
+	private final int bonusNo;
 
-    public WinningLotto(Lotto lotto, int bonusNo) {
-        this.lotto = lotto;
-        this.bonusNo = bonusNo;
-    }
+	public WinningLotto(Lotto lotto, int bonusNo) {
+		this.lotto = lotto;
+		this.bonusNo = bonusNo;
+	}
 
-    public Rank match(Lotto userLotto) {
-        // TODO 로직 구현
-        return null;
-    }
+	public Rank match(Lotto userLotto) {
+		int countOfMatch = getCountOfMatch(userLotto);
+		boolean matchBonus = getMatchBonus(userLotto);
+
+		return Rank.valueOf(countOfMatch, matchBonus);
+	}
+
+	
 }
