74f0f5ee77a77b0da7bfe2f4ffc9d2dc0eb8e560
khx1712 <khx1712@gmail.com>
2019-04-11 19:37:45 +0900

    create class WinningLotto

---
+++ baeminHW3/src/baeminHW3/WinningLotto.java
@@ -0,0 +1,15 @@
+package baeminHW3;
+
+public class WinningLotto {
+    private final Lotto lotto;
+    private final int bonusNo;
+
+    public WinningLotto(Lotto lotto, int bonusNo) {
+        this.lotto = lotto;
+        this.bonusNo = bonusNo;
+    }
+
+    public Rank match(Lotto userLotto) {
+        return null;
+    }
+}
