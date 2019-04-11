1464ddfb2a4e9a92d230ab51ac8818df6eabdb7c
DiceMono <qrdfg@naver.com>
2019-04-11 19:11:24 +0900

    Lotto 클래스  getNumbers 게터 추가

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -12,6 +12,10 @@ public class Lotto {
         this.numbers = numbers;
     }
     
+    public List<Integer> getNumbers() {
+    	return this.numbers;
+    }
+    
 	public void printNumbers() {
 		String numbersString = numbers.toString();
 		System.out.println(numbersString);
