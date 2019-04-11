89fa1c19d63dc34338df8bd60cff759bee33ff96
hyperpace <cnk9109@gmail.com>
2019-04-11 15:46:05 +0900

    Add Filed, checkInteger() to MoneyValidation

--- src/main/java/validation/MoneyValidation.java
+++ src/main/java/validation/MoneyValidation.java
@@ -1,9 +1,24 @@
 package validation;
 
 public class MoneyValidation implements Validation<Integer> {
+	public static final int MONEY_UNIT = 1_000;
+	public static final int MIN_MONEY = 1_000;
+	public static final int MAX_MONEY = 100_000;
+	private int money;
+
 	@Override
 	public boolean check(String value) {
 		return false;
+	}
+
+	private boolean checkInteger(String value) {
+		try {
+			Integer.parseInt(value);
+			return true;
+		} catch (NumberFormatException e) {
+			System.out.println( "정수가 아닙니다.\n");
+			return false;
+		}
 	}
 
 	@Override
