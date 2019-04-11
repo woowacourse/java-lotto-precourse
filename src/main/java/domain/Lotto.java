e110bda9b27b53641172e651290e016016e97bbb
Junmo Han <wnsah052@naver.com>
2019-04-11 20:27:49 +0900

    1 이상 45 이하 임의의 값을 생성한다

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,3 +1,17 @@
+/*
+ * @(#)Lotto.java		2019/4/11
+ * 
+ * Copyright (c) 2019 Junmo Han.
+ * All rights reserved.
+ */
+
+/**
+ * Lotto 클래스는 로또 한 장의 숫자들을 반환하는 기능을 합니다.
+ * 
+ * @version			1.00 2019년 4월 11일
+ * @author 			한준모
+ */
+
 package domain;
 
 import java.util.List;
@@ -6,11 +20,14 @@ import java.util.List;
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
-    private final List<Integer> numbers;
+		private final List<Integer> numbers;
+		
+		public Lotto(List<Integer> numbers) {
+    			this.numbers = numbers;
+    	}
 
-    public Lotto(List<Integer> numbers) {
-        this.numbers = numbers;
-    }
-
-    // 추가 기능 구현
-}
+    	// 추가 기능 구현
+    	public List<Integer> getNumbers() {
+    			return numbers;
+    	}
+}	
---
+++ src/main/java/domain/LottoNumberGenerator.java
@@ -0,0 +1,39 @@
+/*
+ * @(#)LottoNumberGenerator.java		2019/4/11
+ * 
+ * Copyright (c) 2019 Junmo Han.
+ * All rights reserved.
+ */
+
+/**
+ * LottoNumberGenerator 클래스는 로또 번호는 생성해주는 기능을 합니다. 
+ * 
+ * @version			1.00 2019년 4월 11일
+ * @author 			한준모
+ */
+
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Collections;
+
+public class LottoNumberGenerator {
+		public static final int THE_NUM_OF_LOTTO_NUMS = 6;
+		public static final int MIN_RANDOM_NUMBER = 1;
+		public static final int MAX_RANDOM_NUMBER = 45;
+		
+		private static final Random RANDOM = new Random();
+		
+		public static List<Integer> makeLottoNumbers() {
+				ArrayList<Integer> lottoNumbers = new ArrayList<Integer>();
+				
+				while(lottoNumbers.size() < THE_NUM_OF_LOTTO_NUMS) {
+						int randomNumber = RANDOM.nextInt(MAX_RANDOM_NUMBER) + MIN_RANDOM_NUMBER;
+						isDuplication(lottoNumbers, randomNumber);
+				}
+				
+				return lottoNumbers;
+		}
+}
