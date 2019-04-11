504d38397afdb91387a1dad1f3a447df6a67466d
Unknown <9105lgm@naver.com>
2019-04-11 20:51:04 +0900

    당첨 통계 관련 기능 구현
    결과 보여주는 기능
    지난주 당첨 번호 관련 Lotto 객체만든 기능
    어떤 것이 당첨되었는지 알기 위해서 map 구현
    당첨 금액 기능 구현
    보너스 마다 다른 출력 방식 구현

--- src/main/java/domain/Controller.java
+++ src/main/java/domain/Controller.java
@@ -1,8 +1,6 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * 게임의 흐름을 컨트럴하는 마스터 객체
@@ -10,17 +8,19 @@ import java.util.Scanner;
 public class Controller {
     private static final int MINIMUM_MONEY = 1000;      //로또 구입할수 있는 최소 가격
     private static final int MAXIMUM_MONEY = 100000;    //로또 구입할수 있는 최대 가격
-    private static final int LOTTOS_NUMBER = 7;         //로또의 번호 갯수
+    public static final int LOTTOS_NUMBER = 7;         //로또의 번호 갯수
     private static final int LOTTOS_LIMIT_NUMBER = 45;  //로또번호 한도 숫자
+    private static final int START_RANKING = 4;         // 출력이 반대로이기 때문에 RANK FIFITH 부터
 
     private int money;
-    private Message message;
     private List<Lotto> lottos;
     private Scanner scanner;
+    private List<Rank> ranks;
 
     public Controller() {
         scanner = new Scanner(System.in);
         lottos = new ArrayList<>();
+        ranks = new ArrayList<>();
     }
 
     public void askHowMany() {
@@ -95,4 +95,62 @@ public class Controller {
             System.out.println(each.getNumbersList().toString());
         }
     }
+
+    public void showResult(List<Integer> lastLotto) {
+        HashMap<Rank, Integer> resultRank = new LinkedHashMap<>();
+        int prizeMoney;
+
+        makeWinningLotto(lastLotto);
+        makeHashRank(resultRank);
+        prizeMoney = calculateRankPrize(resultRank);
+        for (int i = START_RANKING; i >= 0; --i) {                  // FIFTH 부터 시작
+            System.out.println(printResult(Rank.values()[i], resultRank));
+        }
+        printEarningRate(prizeMoney);
+    }
+
+    public void makeWinningLotto(List<Integer> lastLotto) {
+        int bonusNumber = lastLotto.get(LOTTOS_NUMBER - 1);
+        lastLotto.remove(LOTTOS_NUMBER - 1);             //마지막 보너스번호만 제외하기 위해서
+
+        WinningLotto winningLotto = new WinningLotto(new Lotto(lastLotto), bonusNumber);
+
+        for (Lotto users : lottos) {
+            ranks.add(winningLotto.match(users));
+        }
+
+    }
+
+    /* User가 갖고 있는 당첨이 어떤 것인지 알기 위해서 우선 모든 Rank를 key로한 map을 생성하는 기능 */
+    public void makeHashRank(HashMap<Rank, Integer> resultRank) {
+        for (Rank rank : Rank.values()) {
+            resultRank.put(rank, 0);
+        }
+    }
+
+    public int calculateRankPrize(HashMap<Rank, Integer> resultRank) {
+        int sumPrize = 0;
+
+        for (Rank userRank : ranks) {
+            sumPrize += userRank.getWinningMoney();
+            resultRank.put(userRank, resultRank.get(userRank) + 1);
+        }
+        return sumPrize;
+    }
+
+    public String printResult(Rank rank, HashMap<Rank, Integer> resultRank) {
+        if (rank.getWinningMoney() == 30000000) {
+            return String.format(rank.getCountOfMatch() + "개 일치, 보너스 볼 일치 ("
+                    + rank.getWinningMoney() + "원)-" + resultRank.get(rank) + "개");
+        }
+
+        return String.format(rank.getCountOfMatch() + "개 일치 ("
+                + rank.getWinningMoney() + "원)-" + resultRank.get(rank) + "개");
+    }
+
+    public void printEarningRate(int prizeMoney) {
+        double rate = (double) prizeMoney / (money - (money % MINIMUM_MONEY));
+        System.out.printf("총 수익률은 %.3f입니다.", rate);
+    }
+
 }
