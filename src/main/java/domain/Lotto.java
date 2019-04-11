7abac9f77e51dc6432f8cb28fc9606c674a75b4b
Leehaewon <leehaewon68@gmail.com>
2019-04-11 12:22:44 +0900

    assert lotto number count

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -6,11 +6,19 @@ import java.util.List;
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
+    private static final int LOTTO_NUMBERS_COUNT= 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
+        assertLottoNumbersCount(numbers);
         this.numbers = numbers;
     }
 
-    // 추가 기능 구현
+    private void assertLottoNumbersCount(List<Integer> numbers) {
+        if(numbers.size() < LOTTO_NUMBERS_COUNT)
+            throw new IllegalArgumentException("번호가 부족합니다. 다시 입력해주세요");
+        else if(numbers.size() > LOTTO_NUMBERS_COUNT)
+            throw new IllegalArgumentException("번호를 많이 선택하셨습니다. 다시 입력해주세요");
+    }
 }
