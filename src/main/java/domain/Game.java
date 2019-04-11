75afeb61dbeb1785ec66b9679efa763369e0b0c0
hyperpace <cnk9109@gmail.com>
2019-04-11 16:15:29 +0900

    Add Field, setMoney() to Game

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,4 +1,20 @@
 package domain;
 
+import validation.MoneyValidation;
+
 public class Game {
+	GameUser gameUser;
+	WinningLotto winningLotto;
+
+	public void setMoney(int money) {
+		this.gameUser = new GameUser(money);
+		int countOfLotto = money / MoneyValidation.MONEY_UNIT;
+
+		for (int i = 0; i < countOfLotto; i++) {
+			LottoCombinator lottoCombinator = new LottoCombinator();
+			Lotto lotto = lottoCombinator.getLotto();
+
+			this.gameUser.addLotto(lotto);
+		}
+	}
 }
