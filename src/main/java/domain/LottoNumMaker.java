4900a2edab2a5dd35a5756168d7a677e47e5a8e6
seongchanchoi <chsch911028@gmail.com>
2019-04-11 21:50:31 +0900

    ADD LottoNumMaker: 로또 번호(1~45)를 생성해야 한다.

--- src/main/java/domain/LottoNumMaker.java
+++ src/main/java/domain/LottoNumMaker.java
@@ -1,4 +1,30 @@
 package domain;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class LottoNumMaker {
+
+    private List<Integer> lottoNums;
+
+    public LottoNumMaker() {
+        this.lottoNums = new ArrayList<Integer>();
+    }
+
+    public List<Integer> createLottoNums() {
+        while (this.lottoNums.size() < 6) {
+            this.setLottoNum();
+        }
+
+        return this.lottoNums;
+    }
+
+    private void setLottoNum() {
+        int num = (int) (Math.random() * 45) + 1;
+
+        if (!this.lottoNums.contains(num)) {
+            this.lottoNums.add(num);
+        }
+    }
+
 }
