b22db1d576ceefca25e835760a314aab0c3bd72f
hyperpace <cnk9109@gmail.com>
2019-04-11 16:37:34 +0900

    Add askNumbers() to GameUI

--- src/main/java/ui/GameUI.java
+++ src/main/java/ui/GameUI.java
@@ -1,9 +1,11 @@
 package ui;
 
 import domain.Game;
+import validation.LottoValidation;
 import validation.MoneyValidation;
 import validation.Validation;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class GameUI {
@@ -26,6 +28,15 @@ public class GameUI {
 		Validation<Integer> validation = new MoneyValidation();
 
 		Input<Integer> input = new Input<>(sc, message, validation);
+
+		return input.ask();
+	}
+
+	private List<Integer> askNumbers(Scanner sc) {
+		String message = "지난 주 당첨번호를 입력해 주세요.";
+		Validation<List<Integer>> validation = new LottoValidation();
+
+		Input<List<Integer>> input = new Input<>(sc, message, validation);
 
 		return input.ask();
 	}
