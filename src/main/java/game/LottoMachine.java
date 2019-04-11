28ce9fae7b31bb1a23ca096fd04cd75512fe8d7c
starhn87 <starhn87@gmail.com>
2019-04-11 18:33:20 +0900

    로또 숫자들의 최소치에 해당하는 숫자를 상수로 만든다

--- src/main/java/game/LottoMachine.java
+++ src/main/java/game/LottoMachine.java
@@ -1,4 +1,16 @@
 package game;
 
+import domain.Lotto;
+
+import java.util.*;
+
+import static game.Buyer.NUMBER_PER_LOTTO;
+import static game.Buyer.LOTTO_NUMBER_BOUNDARY;
+
+/**
+ * 당첨 번호와 보너스 번호를 만드는 객체
+ */
 public class LottoMachine {
+    private static final int MIN_BOUNDARY = 1;
+
 }
