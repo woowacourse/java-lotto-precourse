4bcd9c336f88ea4f4447798f02ca4cdc22e2ef5b
hyperpace <cnk9109@gmail.com>
2019-04-11 15:41:53 +0900

    Add Constructor, ask() to Input

--- src/main/java/ui/Input.java
+++ src/main/java/ui/Input.java
@@ -1,8 +1,31 @@
 package ui;
 
+import validation.Validation;
+
+import java.util.Scanner;
+
 /**
  * 게임 사용자 인터페이스의 입력을 담당하는 객체
  */
 public class Input<T> {
+	private final Scanner sc;
+	private final String message;
+	private final Validation<T> validation;
 
+	Input(Scanner sc, String message, Validation<T> validation) {
+		this.sc = sc;
+		this.message = message;
+		this.validation = validation;
+	}
+
+	public T ask() {
+		String value;
+
+		do {
+			System.out.println(message);
+			value = sc.nextLine();
+		} while (!validation.check(value));
+
+		return validation.convert(value);
+	}
 }
