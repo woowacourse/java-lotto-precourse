ca6550f6c4b12f06d48762369ebc0c9061f38568
racoco083 <uytr083@naver.com>
2019-04-11 18:22:09 +0900

    update checkWinningNumbers function

--- src/main/java/domain/Manager.java
+++ src/main/java/domain/Manager.java
@@ -1,18 +1,67 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Manager {
-    User user;
+    private User user;
+    private ArrayList<Integer> winningNumbesrs;
+    boolean[] checkNumbers;
 
     Manager(){
         user = new User();
+        winningNumbesrs = new ArrayList<>();
+        checkNumbers = new boolean[46];
     }
 
     public void startGame(){
         while(!firstQuery());
         user.makeLottoList();
         user.printLottoList();
+        while(!secondQuery());
+    }
+
+    public boolean secondQuery(){
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        List<String> ret = user.inputNumbers();
+
+        if(!checkWinningNumbers(ret)){
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean checkWinningNumbers(List<String> list){
+        for(int i=1; i<46; i++){
+            checkNumbers[i] = false;
+        }
+        int cnt = 0;
+
+        for(int i=0; i<list.size(); i++){
+            if(!CheckFormatError(list.get(i))) return false;
+
+            int number = Integer.parseInt(list.get(i));
+            if(checkNumbers[number]){
+                System.out.println(InputError.DUPLICATE_ERROR);
+                return false;
+            }
+
+            if(number<1 || number>45){
+                System.out.println(InputError.NUMBER_RANGE_ERROR);
+                return false;
+            }
+
+            checkNumbers[number] = true;
+            cnt++;
+        }
+
+        if(cnt != 6){
+            System.out.println(InputError.NUMBER_CNT_ERROR);
+            return false;
+        }
+        return true;
     }
 
     public boolean firstQuery(){
