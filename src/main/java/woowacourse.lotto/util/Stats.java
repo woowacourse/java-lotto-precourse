b7878f57a25a490f07025c3b66389605b8305b8d
hjbc0921 <hjbc0921@gmail.com>
2019-04-11 21:12:33 +0900

    update Stats.java : fix int division in calYield method

--- src/main/java/woowacourse.lotto/util/Stats.java
+++ src/main/java/woowacourse.lotto/util/Stats.java
@@ -14,12 +14,12 @@ import java.util.HashMap;
 public class Stats {
     private static final String RANK_FORMAT_STRING =
             "당첨 통계\n"
-            + "---------\n"
-            + "3개 일치 (5000원)- %d개\n"
-            + "4개 일치 (50000원)- %d개\n"
-            + "5개 일치 (1500000원)- %d개\n"
-            + "5개 일치, 보너스 볼 일치(30000000원) - %d개\n"
-            + "6개 일치 (2000000000원)- %d개\n";
+                    + "---------\n"
+                    + "3개 일치 (5000원)- %d개\n"
+                    + "4개 일치 (50000원)- %d개\n"
+                    + "5개 일치 (1500000원)- %d개\n"
+                    + "5개 일치, 보너스 볼 일치(30000000원) - %d개\n"
+                    + "6개 일치 (2000000000원)- %d개\n";
     private static final String YIELD_FORMAT_STRING =
             "총 수익률은 %.3f입니다.\n";
     private HashMap<Rank, Integer> countOfRank = new HashMap<>();
@@ -34,23 +34,23 @@ public class Stats {
                 countOfRank.get(Rank.FIRST));
     }
 
+    void calYield(int totalPrice) {
+        double yield = calTotalWinningMoney() / (double)totalPrice;
+        System.out.format(YIELD_FORMAT_STRING, yield);
+    }
+
     private void updateRankCount(ArrayList<Rank> matchedRanks) {
-        for(Rank type : Rank.values()) {
+        for (Rank type : Rank.values()) {
             countOfRank.put(type, 0);
         }
-        for(Rank r : matchedRanks) {
+        for (Rank r : matchedRanks) {
             countOfRank.put(r, countOfRank.get(r) + 1);
         }
     }
 
-    void calYield(int totalPrice) {
-        double yield = calTotalWinningMoney() / totalPrice;
-        System.out.format(YIELD_FORMAT_STRING, yield);
-    }
-
     private long calTotalWinningMoney() {
         long totalWinningMoney = 0;
-        for(Rank type : Rank.values()) {
+        for (Rank type : Rank.values()) {
             totalWinningMoney += countOfRank.get(type)
                     * type.getWinningMoney();
         }
