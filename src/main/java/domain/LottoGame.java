d18c410f033c7257ff2c70d7ef8523f087efa7c1
seongchanchoi <chsch911028@gmail.com>
2019-04-11 22:01:41 +0900

    ADD LottoGame: 사용자로부터 구입금액을 입력받아 저장한다.

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,4 +1,27 @@
 package domain;
 
+import java.util.*;
+
 public class LottoGame {
+
+    private int cashToBuyLotto;
+    private List<Lotto> userLottos;
+    private WinningLotto winningLotto;
+
+    private static final int LOTTO_PRICE = 1000;
+
+    public LottoGame() {
+        this.cashToBuyLotto = 0;
+        this.userLottos = new ArrayList<Lotto>();
+        this.winningLotto = null;
+    }
+
+    public void setCashToBuyLotto(String cash) {
+        this.cashToBuyLotto = Integer.parseInt(cash);
+    }
+
+    public void showMessage(String msg) {
+        System.out.println(msg);
+    }
+
 }
