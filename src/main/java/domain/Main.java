f7321c9e921572369a9c02e7d776948663a994be
zerobell-lee <bnb3456@gmail.com>
2019-04-11 19:44:14 +0900

    feat: added getTrials()

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,4 +1,31 @@
 package domain;
 
+import java.util.Scanner;
+
 public class Main {
+    final static int LOTTO_PRICE = 1000;
+    static private Scanner staticSc;
+
+    private Scanner getScanner() {
+        if (staticSc == null) {
+            staticSc = new Scanner(System.in);
+        }
+
+        return staticSc;
+    }
+
+    private int getTrials() {
+        int value, trials;
+        Scanner sc = getScanner();
+        System.out.println("구입금액을 입력해주세요.");
+
+        value = sc.nextInt();
+        trials = (int)Math.floor(value / LOTTO_PRICE);
+
+        return trials;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
