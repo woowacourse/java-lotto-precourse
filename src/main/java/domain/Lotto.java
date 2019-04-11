031062e50f5df51ab8846333fee7d8183e56765f
hyperpace <cnk9109@gmail.com>
2019-04-11 17:03:41 +0900

    Add contains() to Lotto

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -6,13 +6,17 @@ import java.util.List;
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		this.numbers = numbers;
+	}
 
-    public List<Integer> getNumbers() {
-        return this.numbers;
-    }
+	public List<Integer> getNumbers() {
+		return this.numbers;
+	}
+
+	public boolean contains(int number) {
+		return numbers.contains(number);
+	}
 }
