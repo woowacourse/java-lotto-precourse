afdd381162f0728e1d0ecc692a6b82153f69e32f
racoco083 <uytr083@naver.com>
2019-04-11 18:19:32 +0900

    update NUMBER_CNT_ERROR constant

--- src/main/java/domain/InputError.java
+++ src/main/java/domain/InputError.java
@@ -6,4 +6,5 @@ public class InputError {
     static final String REST_ERROR = "나머지가 생겼습니다. 다시 입력해주세요.";
     static final String NUMBER_RANGE_ERROR = "1~45의 번호가 아닙니다. 다시 입력해주세요.";
     static final String DUPLICATE_ERROR = "번호가 중복되었습니다. 다시 입력해주세요.";
+    static final String NUMBER_CNT_ERROR = "번호가 6개가 아닙니다. 다시 입력해주세요.";
 }
