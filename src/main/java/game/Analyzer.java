26039df03d068d2d771e4b106728ee616c11ad16
starhn87 <starhn87@gmail.com>
2019-04-11 18:14:32 +0900

    당첨 통계와 총 수익률을 나타낸다

--- src/main/java/game/Analyzer.java
+++ src/main/java/game/Analyzer.java
@@ -13,4 +13,15 @@ import static game.Game.LOTTO_PRICE;
  */
 public class Analyzer {
     private static HashMap<Rank, Integer> map;           // 매칭 결과를 담을 HashMap
+
+    public void showResult(List<Lotto> lottos, WinningLotto winningLotto) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        String[] result = putCorrectCount(lottos, winningLotto);
+        double benefitRate = putBenefitRate(lottos);
+        for (String value : result) {
+            System.out.println(value);
+        }
+        System.out.println("총 수익률은 " + benefitRate + "입니다.");
+    }
 }
