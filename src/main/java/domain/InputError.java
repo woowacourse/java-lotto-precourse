00c8ab211239f515557bf05e7bfc57b04b0b038d
racoco083 <uytr083@naver.com>
2019-04-11 13:24:11 +0900

    update Error Messages

--- src/main/java/domain/InputError.java
+++ src/main/java/domain/InputError.java
@@ -1,4 +1,9 @@
 package domain;
 
 public class InputError {
+    static final String NUMBER_FORMAT_ERROR = "숫자가 아닌 값이 입력되었습니다. 다시 입력해주세요.";
+    static final String MINUS_ERROR = "양이 아닌 정수가 입력되었습니다.  다시 입력해주세요.";
+    static final String REST_ERROR = "나머지가 생겼습니다. 다시 입력해주세요.";
+    static final String NUMBER_RANGE_ERROR = "1~45의 번호가 아닙니다. 다시 입력해주세요.";
+    static final String DUPLICATE_ERROR = "번호가 중복되었습니다. 다시 입력해주세요.";
 }
