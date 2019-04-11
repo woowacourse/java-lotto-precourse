f500d1016bbcd3074d2e4872daac9207191e6fed
DaejunBae <dpudpu11@gmail.com>
2019-04-11 13:12:08 +0900

    Add 당첨번호, 보너스 번호를 입력한다.
    - 당첨번호 쉼표로 분리 후 Int 변환
    - trim() 사용으로 공백 제거

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -3,11 +3,14 @@ package lotto;
 import lotto.domain.Lotto;
 import lotto.domain.LottoMachine;
 import lotto.domain.LottoMoney;
+import lotto.domain.WinningLotto;
 import lotto.utils.RandomNumberGenerate;
 import lotto.view.InputConsoleView;
 import lotto.view.OutputConsoleView;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoGame {
     private InputConsoleView inputConsoleView;
@@ -23,5 +26,22 @@ public class LottoGame {
         List<Lotto> purchasedLottos =  lottoMachine.buyLottos(purchaseAmount);
         OutputConsoleView.printLottos(purchasedLottos);
 
+        WinningLotto winningLotto = getWinningLotto();
+        
     }
+
+    private WinningLotto getWinningLotto() {
+        Lotto winningNumbers = new Lotto(toListOfInteger(split(inputConsoleView.inputWinningNumbers())));
+        int bonusNo = inputConsoleView.inputBonusNo();
+        return new WinningLotto(winningNumbers, bonusNo);
+    }
+
+    private List<Integer> toListOfInteger(String[] winningNumbers) {
+        return Arrays.stream(winningNumbers).map(String::trim).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private String[] split(String winningNumbers) {
+        return winningNumbers.split(",");
+    }
+
 }
--- src/main/java/lotto/view/InputConsoleView.java
+++ src/main/java/lotto/view/InputConsoleView.java
@@ -3,9 +3,20 @@ package lotto.view;
 import java.util.Scanner;
 
 public class InputConsoleView {
+
     public long inputPurchaseAmount() {
         System.out.println("구입 금액을 입력해주세요.");
 
         return new Scanner(System.in).nextLong();
+    }
+
+    public String inputWinningNumbers() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return new Scanner(System.in).nextLine();
+    }
+
+    public int inputBonusNo() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return new Scanner(System.in).nextInt();
     }
 }
