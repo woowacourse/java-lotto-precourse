e7e039f990fe5769a64818ce5f64cf8526e7b0e2
JOngNan <dhorlawhddbs@naver.com>
2019-04-11 11:52:13 +0900

    로또 게임을 실행하는 메소드 생성(Main)

--- src/main/java/domain/GameRunner.java
+++ src/main/java/domain/GameRunner.java
@@ -1,4 +1,14 @@
 package domain;
 
+/**
+ * 로또 게임을 실행하는 객체
+ *
+ * @version 1.0(2019.04.11)
+ * @author jongyoon Kim
+ */
 public class GameRunner {
+    public static void main(String[] args) {
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.run();
+    }
 }
