af15364f4f8cd98e8ccab09da8176096f88bd524
hsycamp <hsycamp@gmail.com>
2019-04-11 21:52:40 +0900

    로또 구매 메소드 추가

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,10 +1,17 @@
 package domain;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 
 
 public class User {
 	static private Scanner scanner = new Scanner(System.in);
+	private static ArrayList<Lotto> userLottoList = new ArrayList<Lotto>();
 	private static final int LOTTO_NUMBER_COUNT = 6;
 	private static final int MAX_LOTTO_NUMBER = 45;
 	private static final int PRICE_OF_ONE_LOTTO = 1000;
@@ -20,6 +27,21 @@ public class User {
 	}
 
 	public static void buyLotto(int inputMoney) {
+		for (int i = 0; i < inputMoney / PRICE_OF_ONE_LOTTO; i++) {
+			Lotto lotto = new Lotto(makeLottoNumber());
+			userLottoList.add(lotto);
+		}
+	}
+
+	public static List<Integer> makeLottoNumber() {
+		Set<Integer> set = new HashSet<Integer>();
+		while (set.size() < LOTTO_NUMBER_COUNT) {
+			int num = (int) (Math.random() * MAX_LOTTO_NUMBER) + 1;
+			set.add(new Integer(num));
+		}
+		List<Integer> list = new LinkedList<Integer>(set);
+		Collections.sort(list);
+		return list;
 	}
 
 	public static void printBoughtLottoList() {
