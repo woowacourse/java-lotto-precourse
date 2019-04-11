24edee67c508004da3fd174e3e76709efce53291
seongchanchoi <chsch911028@gmail.com>
2019-04-11 22:03:01 +0900

    UPDATE LottoGame: 입력받은 구입금액에 맞는 갯수의 로또를 만든다.

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -20,6 +20,16 @@ public class LottoGame {
         this.cashToBuyLotto = Integer.parseInt(cash);
     }
 
+    public void createLottos() {
+        int cash = this.cashToBuyLotto;
+
+        while (cash >= this.LOTTO_PRICE) {
+            List<Integer> lottoNums = new LottoNumMaker().createLottoNums();
+            this.userLottos.add(new Lotto(lottoNums));
+            cash -= this.LOTTO_PRICE;
+        }
+    }
+
     public void showMessage(String msg) {
         System.out.println(msg);
     }
