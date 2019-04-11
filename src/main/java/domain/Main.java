a914850eb98d0e54016870ff19f7713901d1bfd0
seongchanchoi <chsch911028@gmail.com>
2019-04-11 22:09:57 +0900

    ADD Main: 게임 구현

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,4 +1,31 @@
 package domain;
 
 public class Main {
+
+    private static final String REQUEST_CASH_TO_BUY_LOTTO = "\n구입금액을 입력해주세요.";
+    private static final String REQUEST_LAST_WINNING_LOTTO_NUMS = "\n지난주 당첨 번호를 입력해주세요.";
+    private static final String REQUEST_BONUS_BALL_NUM = "\n보너스 볼을 입력해주세요.";
+    private static final String STATISTIC_MESSAGE = "\n당첨 통계\n---------";
+
+    public static void main(String[] args) {
+
+        LottoGame game = new LottoGame();
+        UserInput userInput = new UserInput();
+
+        game.showMessage(REQUEST_CASH_TO_BUY_LOTTO);
+        game.setCashToBuyLotto(userInput.getCashToBuyLotto());
+        game.createLottos();
+        game.showLottos();
+
+        game.showMessage(REQUEST_LAST_WINNING_LOTTO_NUMS);
+        String winningLottoNumbers = userInput.getWinningLottoNumbers();
+        game.showMessage(REQUEST_BONUS_BALL_NUM);
+        String bonusNumber = userInput.getBonusNumber();
+        game.setWinningLotto(winningLottoNumbers, bonusNumber);
+
+        game.showMessage(STATISTIC_MESSAGE);
+        game.showResult();
+        game.showROI();
+
+    }
 }
