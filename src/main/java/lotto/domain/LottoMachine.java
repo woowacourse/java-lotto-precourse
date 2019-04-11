cc1af34d52c7a0dadbbff6b8a4bec31e2c39cf70
DaejunBae <dpudpu11@gmail.com>
2019-04-11 12:47:31 +0900

    Add 로또 번호 생성중에 발생 할 수도 있는 무한루프 에러 처리

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class LottoMachine {
+    public static final int LOOP_COUNT_LIMIT = 1000;
     private NumberGenerator numberGenerator;
 
     public LottoMachine(NumberGenerator numberGenerator) {
@@ -27,11 +28,19 @@ public class LottoMachine {
 
     private List<Integer> generateNonDuplicateNumbers() {
         Set<Integer> nonDuplicateNumbers = new TreeSet<>();
-        while (nonDuplicateNumbers.size() < Lotto.LOTTO_NUMBER_SIZE) {
+        int cnt = 0;
+        while (nonDuplicateNumbers.size() < Lotto.LOTTO_NUMBER_SIZE
+                && cnt++ < LOOP_COUNT_LIMIT) {
             nonDuplicateNumbers.add(numberGenerator.getNumber());
         }
+        validateInfiniteLoop(cnt);
 
         return toList(nonDuplicateNumbers);
+    }
+
+    private void validateInfiniteLoop(int cnt) {
+        if (cnt > LOOP_COUNT_LIMIT)
+            throw new RuntimeException("로또 번호 생성과정에서 무한루프 발생.");
     }
 
     private List<Integer> toList(Set<Integer> nonDuplicateNumbers) {
