436e6d342466b02585382a83f0ad47372248bd9a
JOngNan <dhorlawhddbs@naver.com>
2019-04-11 11:50:12 +0900

    로또 게임에 필요한 것들을 입력받고 게임 진행을 하는 메소드 생성

--- src/main/java/domain/LottoGame.java
+++ src/main/java/domain/LottoGame.java
@@ -1,4 +1,47 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 로또 게임에 하기 위해 필요한 것들(사용자 입력)을 생성하는 객체
+ *
+ * @version 1.0(2019.04.11)
+ * @author jongyoon Kim
+ */
 public class LottoGame {
+    private static Inputter inputter = new Inputter();
+    private static LottoCreator lottoCreator = new LottoCreator();
+    private static StatisticalAnalyzer statisticalAnalyzer = new StatisticalAnalyzer();
+
+    public void run(){
+        ArrayList<Lotto> userLottoList = createLottoList(inputLottoAmount());
+        List<Integer> winningNum = splitWinningNumAndCheckReInput(inputWinningLotto());
+        WinningLotto winningLotto = createWinningLotto(winningNum, inputBonusNum(winningNum));
+        statisticalAnalyzer.calcWinningStatistical(userLottoList, winningLotto);
+    }
+
+    private int inputLottoAmount(){
+        return inputter.inputPurchaseAmount();
+    }
+
+    private ArrayList<Lotto> createLottoList(int lottoAmount){
+        return lottoCreator.purchaseLottoForAmount(lottoAmount);
+    }
+
+    private String inputWinningLotto(){
+        return inputter.inputWinningNumber();
+    }
+
+    private List<Integer> splitWinningNumAndCheckReInput(String winningNum){
+        return lottoCreator.splitWinningNumAndCheckingReInput(winningNum);
+    }
+
+    private int inputBonusNum(List<Integer> winningNum){
+        return inputter.inputBonusNumber(winningNum);
+    }
+
+    private WinningLotto createWinningLotto(List<Integer> winningNum, int bonusNum){
+        return lottoCreator.createWinningLotto(winningNum, bonusNum);
+    }
 }
