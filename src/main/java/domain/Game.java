a85fc8df83c2a24cd926f67cb44af921862e1dd7
dasistHYOJIN <hgenie14@gmail.com>
2019-04-11 16:30:19 +0900

    오류 수정

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -3,23 +3,14 @@ package domain;
 import java.util.*;
 
 public class Game {
-    private final String[] SENTENCE = {
-            "구입금액을 입력해 주세요.",
-            "개를 구매했습니다.",
-            "지난 주 당첨 번호를 입력해 주세요.",
-            "보너스 볼을 입력해 주세요.",
-            "당첨 통계\n" + "---------",
-            "총 수익률은 ",
-            "입니다."
-    };
-
     private Scanner sc = new Scanner(System.in);
     private Random random = new Random();
 
     private List<Lotto> lottos = new ArrayList<>();
     private WinningLotto winningLotto;
-    private int gameNum, prize = 0;
-    private int[] result = new int[6];
+    private long total, gameNum;
+    private int[] result = new int[5];
+    private double prize = 0;
 
     protected void execute() {
         setGameNum();
@@ -39,15 +30,15 @@ public class Game {
     }
 
     private void count(int countOfMatch, int winningMoney) {
-        if (countOfMatch == 6) result[5]++;
-        if (countOfMatch == 5 && winningMoney == 30000000) result[4]++;
-        if (countOfMatch == 5 && winningMoney == 1500000) result[3]++;
-        if (countOfMatch == 4) result[2]++;
-        if (countOfMatch == 3) result[1]++;
+        if (countOfMatch == 6) result[0]++;
+        if (countOfMatch == 5 && winningMoney == 30000000) result[1]++;
+        if (countOfMatch == 5 && winningMoney == 1500000) result[2]++;
+        if (countOfMatch == 4) result[3]++;
+        if (countOfMatch == 3) result[4]++;
     }
 
     private void printResult() {
-        System.out.println(SENTENCE[4]);
+        System.out.println("당첨 통계\n" + "---------");
 
         StringBuilder result = new StringBuilder();
         Rank[] rank = Rank.values();
@@ -55,17 +46,18 @@ public class Game {
             result.append(rank[i].getCountOfMatch() + "개 일치 (" + rank[i].getWinningMoney() + "원)-");
             result.append(this.result[i] + "개\n");
         }
+        result.append("총 수익률은 " + (prize / total) + "입니다.");
+
         System.out.println(result.toString());
     }
+
     private void printLottos() {
-        for (int i = 0; i < gameNum; i++) {
-            lottos.get(i).print();
-        }
+        for (Lotto lotto : lottos) lotto.print();
         System.out.println();
     }
 
     private WinningLotto generateWinningLotto() {
-        System.out.println(SENTENCE[2]);
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
 
         // 임시로 당첨 번호 받기
         List<Integer> list = new LinkedList<>();
@@ -74,11 +66,12 @@ public class Game {
             list.add(Integer.parseInt(str));
         }
 
-        System.out.println(SENTENCE[3]);
+        System.out.println("보너스 볼을 입력해 주세요.");
         int bonus = sc.nextInt();
 
         Lotto lotto = new Lotto(list);
 
+        System.out.println();
         return new WinningLotto(lotto, bonus);
     }
 
@@ -114,16 +107,16 @@ public class Game {
     }
 
     private void buyLotto() {
-        System.out.println(gameNum + SENTENCE[1]);
+        System.out.println(gameNum + "개를 구매했습니다.");
 
         for (int i = 0; i < gameNum; i++)
             lottos.add(generateLotto());
     }
 
     private void setGameNum() {
-        System.out.println(SENTENCE[0]);
+        System.out.println("구입금액을 입력해 주세요.");
 
-        int total = sc.nextInt();
+        total = sc.nextLong();
         sc.nextLine();
         gameNum = total / 1000;
 
