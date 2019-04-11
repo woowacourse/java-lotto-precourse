a52a838f2e43decfa56e6cfcc2fc8d7505d346e6
hyperpace <cnk9109@gmail.com>
2019-04-11 16:09:54 +0900

    Add Field, Constructor to LottoCombinator

--- src/main/java/domain/LottoCombinator.java
+++ src/main/java/domain/LottoCombinator.java
@@ -1,4 +1,20 @@
 package domain;
 
+import validation.LottoValidation;
+
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class LottoCombinator {
+	private static final int MIN_NUMBER = LottoValidation.MIN_NUMBER;
+	private static final int MAX_NUMBER = LottoValidation.MAX_NUMBER;
+	private static final int NUMBER_CNT = LottoValidation.NUMBER_CNT;
+	private Stack<Integer> lottoNumbers;
+
+	public LottoCombinator() {
+		lottoNumbers = IntStream.rangeClosed(MIN_NUMBER, MAX_NUMBER)
+				.boxed()
+				.collect(Collectors.toCollection(Stack::new));
+	}
 }
