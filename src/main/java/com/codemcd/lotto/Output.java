5abf06b21802639402c4e9025efc8eb786bb3314
SUNGBUMPARK <psbum1609@gmail.com>
2019-04-11 11:36:47 +0900

    당첨 통계 출력 오타 수정
    * Output.java: 2등 출력 금액을 3,000,000원에서 30,000,000원으로 알맞게
    수정하였다. 그리고 그에 맞게 공백을 맞춰주었다.

--- src/main/java/com/codemcd/lotto/Output.java
+++ src/main/java/com/codemcd/lotto/Output.java
@@ -27,11 +27,11 @@ public class Output {
     public static void printLottoResult(List<Integer> countOfRank, double earningsRate) {
         System.out.println("당첨 통계");
         System.out.println("---------");
-        System.out.println(String.format("%-35s", "3개 일치 (5,000원)") + "- " + countOfRank.get(5) + "개");
-        System.out.println(String.format("%-35s", "4개 일치 (50,000원)") + "- " + countOfRank.get(4) + "개");
-        System.out.println(String.format("%-35s", "5개 일치 (1,500,000원)") + "- " + countOfRank.get(3) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (3,000,000원) - " + countOfRank.get(2) + "개");
-        System.out.println(String.format("%-35s", "6개 일치 (2,000,000,000원)") + "- " + countOfRank.get(1) + "개");
-        System.out.println("총 수익률은 " + String.format("%.3f", earningsRate) + "입니다.");
+        System.out.println(String.format("%-36s", "3개 일치 (5,000원)") + "- " + countOfRank.get(5) + "개");
+        System.out.println(String.format("%-36s", "4개 일치 (50,000원)") + "- " + countOfRank.get(4) + "개");
+        System.out.println(String.format("%-36s", "5개 일치 (1,500,000원)") + "- " + countOfRank.get(3) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countOfRank.get(2) + "개");
+        System.out.println(String.format("%-36s", "6개 일치 (2,000,000,000원)") + "- " + countOfRank.get(1) + "개");
+        System.out.println("총 수익률은 " + String.format("%.3f", earningsRate) + " 입니다.");
     }
 }
