f3188833b6335315b590715a313a60cab9050a5f
starhn87 <starhn87@gmail.com>
2019-04-11 18:34:39 +0900

    당첨 번호를 입력하고 조건에 맞으면 발급한다

--- src/main/java/game/LottoMachine.java
+++ src/main/java/game/LottoMachine.java
@@ -14,4 +14,19 @@ public class LottoMachine {
     private static final int MIN_BOUNDARY = 1;
     private static int bonusNumInput;            // 입력받은 보너스 숫자
 
+    /*
+     * 당첨 번호가 조건에 맞으면 발급
+     */
+    public Lotto makeWinningLotto() {
+        String input;
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> list;
+
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        do {
+            input = scanner.nextLine();
+            list = inspector(input);
+        } while (!inspectLottoCondition(list));
+        return new Lotto(list);
+    }
 }
