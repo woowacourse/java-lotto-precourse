edae3cd3f9e58f88464081eb10f264ecfc83594f
racoco083 <uytr083@naver.com>
2019-04-11 14:30:59 +0900

    update inputMoney function

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,4 +1,20 @@
 package domain;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class User {
+    private List<Lotto> lottolist;
+    private Scanner scanner;
+    private String pay;
+
+    User(){
+        scanner = new Scanner(System.in);
+    }
+
+    public String inputMoney(){
+        String pay = scanner.nextLine();
+        return pay;
+    }
+
 }
