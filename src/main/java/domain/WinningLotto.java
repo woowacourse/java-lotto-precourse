2b2a95023b44f8f9abb7ada41d46acb0acb1d102
DiceMono <qrdfg@naver.com>
2019-04-11 19:08:12 +0900

    기능 : 당첨 분류; WinningLotto와 userLotto를 비교하여 Rank.valueOf메서드를 이용해 분류한다.

--- src/main/java/domain/WinningLotto.java
+++ src/main/java/domain/WinningLotto.java
@@ -1,19 +1,43 @@
 package domain;
 
+import java.util.List;
+
 /**
  * 당첨 번호를 담당하는 객체
  */
 public class WinningLotto {
-    private final Lotto lotto;
-    private final int bonusNo;
+	private final Lotto lotto;
+	private final int bonusNo;
 
-    public WinningLotto(Lotto lotto, int bonusNo) {
-        this.lotto = lotto;
-        this.bonusNo = bonusNo;
+	public WinningLotto(Lotto lotto, int bonusNo) {
+		this.lotto = lotto;
+		this.bonusNo = bonusNo;
+	}
+
+	private boolean matchBonus(Lotto userLotto) {
+		List<Integer> userLottoNumbers = userLotto.getNumbers();
+		return userLottoNumbers.contains(this.bonusNo);
+	}
+	
+	private int countPlusOneIfListContainsNumber(int count, List<Integer> integerList, int number) {
+		if (integerList.contains(number)) {
+			count++;
+		}
+		return count;
+	}
+	
+	private int countMatch(Lotto userLotto) {
+    	List<Integer> userLottoNumbers = userLotto.getNumbers();
+    	List<Integer> winningLottoNumbers = this.lotto.getNumbers();
+    	int countOfMatch = 0;
+    	
+    	for (int userLottoNumber : userLottoNumbers) {
+    		countOfMatch = countPlusOneIfListContainsNumber(countOfMatch, winningLottoNumbers, userLottoNumber);
+    	}
+    	return countOfMatch;
     }
 
-    public Rank match(Lotto userLotto) {
-        // TODO 로직 구현
-        return null;
-    }
+	public Rank match(Lotto userLotto) {
+		return Rank.valueOf(countMatch(userLotto), matchBonus(userLotto));
+	}
 }
