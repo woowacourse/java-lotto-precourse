ef1281d0cffe0c4809bcde9288a2ea24a92818fe
hyperpace <cnk9109@gmail.com>
2019-04-11 16:03:43 +0900

    Add Constructor to GameUser

--- src/main/java/domain/GameUser.java
+++ src/main/java/domain/GameUser.java
@@ -1,4 +1,14 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameUser {
+	private final int money;
+	private List<Lotto> lottos;
+
+	public GameUser(int money) {
+		this.money = money;
+		lottos = new ArrayList<>();
+	}
 }
