7fbbd7f7049bf6bdcf4ac2812be0cda8f3046976
starhn87 <starhn87@gmail.com>
2019-04-11 18:22:44 +0900

    구입금액 입력이 조건에 맞으면 구매를 완료한다

--- src/main/java/game/Buyer.java
+++ src/main/java/game/Buyer.java
@@ -11,4 +11,16 @@ public class Buyer {
     static final int  NUMBER_PER_LOTTO = 6;
     static final int LOTTO_NUMBER_BOUNDARY = 45;
     private static final int MIN_PAY_MONEY = 0;
+
+    /*
+     * 구입금액 입력이 조건에 맞으면 구매를 완료한다
+     */
+    public int buyMoney() {
+        int money;
+        System.out.println("구입금액을 입력해 주세요.");
+        do {
+            money = inspectBuyMoneyCondition();
+        } while (money < MIN_PAY_MONEY);
+        return money;
+    }
 }
