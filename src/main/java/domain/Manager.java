d0c04a2ab2aa996a0400acbd1055a1737b119ed9
racoco083 <uytr083@naver.com>
2019-04-11 13:43:01 +0900

    update CheckFromatError function

--- src/main/java/domain/Manager.java
+++ src/main/java/domain/Manager.java
@@ -1,4 +1,37 @@
 package domain;
 
+import java.util.Scanner;
+
 public class Manager {
+    User user;
+
+    Manager(){
+        user = new User();
+    }
+
+    public void startGame(){
+        firstQuery();
+
+    }
+
+    public boolean firstQuery(){
+        System.out.println("구입금액을 입력해 주세요.");
+        String ret = user.inputMoney();
+
+        CheckFormatError(ret);
+
+
+        return true;
+    }
+
+    public boolean CheckFormatError(String input){
+        try{
+            Integer.parseInt(input);
+        }catch(NumberFormatException e){
+            System.out.println(InputError.NUMBER_FORMAT_ERROR);
+            return false;
+        }
+        return true;
+    }
+
 }
