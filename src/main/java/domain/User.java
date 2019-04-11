45f7188ff71a62e687adf2e64b4351fc226acc75
hongduk kim <2012103911@khu.ac.kr>
2019-04-11 20:29:02 +0900

    사용자로부터 당첨 로또 번호 6개 입력 받는 기능 구현

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -8,17 +8,25 @@ import java.util.Scanner;
 
 
 public class User {
+    private final int MinNumber = 1;
+    private final int MaxNumber = 45;
     private final int MinPrice = 1000;
+    private final int MaxNumberLength = 6;
+    private List<Lotto> createdLotto;
+    private List<Integer> checkedLastLotto;
+    private HashMap<Integer, Integer> overlap;
     private Scanner Input;
+
     public int getPrice() {
         Input = new Scanner(System.in);
         int price = 0;
         do {
             System.out.println("구입금액을 입력해 주세요.");
             price = Input.nextInt();
-        } while(checkPrice(price));
+        } while (checkPrice(price));
         return price;
     }
+
     private boolean checkPrice(int price) {
         if (price < MinPrice) {
             return true;
@@ -26,4 +34,37 @@ public class User {
         return false;
     }
 
+    private int randomNumber() {
+        int no = 0;
+        do {
+            no = (int) (Math.random() * MaxNumber) + MinNumber;
+        } while (checkLottoOverlap(no));
+        return no;
+    }
+
+    private boolean checkLottoOverlap(int no) {
+        if (overlap.containsKey(no)) {
+            return true;
+        }
+        overlap.put(no, 0);
+        return false;
+    }
+
+    public List<Lotto> getLotto(int price) {
+        BuyLotto(price);
+        System.out.println(createdLotto.size()+"개를 구매했습니다.");
+        return createdLotto;
+    }
+
+    public List<Integer> getWinningLotto() {
+        Input = new Scanner(System.in);
+        do {
+            overlap = new HashMap<Integer, Integer>();
+            System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+            String number = Input.nextLine();
+            checkedLastLotto = checkLastLotto(number);
+        } while (checkLastLottoSize(checkedLastLotto));
+
+        return checkedLastLotto;
+    }
 }
