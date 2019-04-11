7f3b91510e9c2e47e784eeac76a79159e76b5e10
hyperpace <cnk9109@gmail.com>
2019-04-11 16:26:22 +0900

    Add checkList() to LottoValidation

--- src/main/java/validation/LottoValidation.java
+++ src/main/java/validation/LottoValidation.java
@@ -1,7 +1,31 @@
 package validation;
 
-public class LottoValidation {
+import java.util.List;
+
+public class LottoValidation implements Validation<List<Integer>> {
 	public static final int MIN_NUMBER = 1;
 	public static final int MAX_NUMBER = 45;
 	public static final int NUMBER_CNT = 6;
+	private static final String REGULAR_EXPRESSION = "^([0-9]+)(,[0-9]+){5}$";
+	private static final String SEPARATOR = ",";
+	private List<Integer> numbers;
+
+	@Override
+	public boolean check(String value) {
+		return false;
+	}
+
+	private boolean checkList(String value) {
+		if (!value.matches(REGULAR_EXPRESSION)) {
+			System.out.println("입력 형식을 맞춰주세요.");
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public List<Integer> convert(String value) {
+		return null;
+	}
 }
