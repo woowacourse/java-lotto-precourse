70002a92de17993bf08622d2e56cb2b13c137629
hyun <jhs695@naver.com>
2019-04-11 13:34:16 +0900

    Fix State class
    statsLottoResult() 등수 카운터 집계 기능 추가

--- src/main/java/domain/State.java
+++ src/main/java/domain/State.java
@@ -1,4 +1,18 @@
 package domain;
 
+import lotto.Rank;
+
+import java.util.List;
+import java.util.TreeMap;
+
 public class State {
+    public TreeMap<Rank, Long> statsLottoResult(List<Rank> ranks) {
+        TreeMap<Rank, Long> stats = new TreeMap<>();
+
+        for (Rank value : Rank.values()) {
+            stats.put(value, ranks.stream().filter(rank -> rank == value).count());
+        }
+
+        return stats;
+    }
 }
