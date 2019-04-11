96688aa946308479049f15033c3f1c4f8b655ba6
Sunkwan Kim <skyvlu@gmail.com>
2019-04-11 23:32:20 +0900

    Main, Const 클래스

---
+++ src/main/java/domain/Const.java
@@ -0,0 +1,12 @@
+package domain;
+
+/**
+ * 상수 클래스
+ */
+public class Const {
+    public static final int TICKET_PRICE = 1000;
+    public static final int SELECTABLE_LOTTO_NUMBER_MIN = 1;
+    public static final int SELECTABLE_LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_NUMBER_SIZE = 6;
+    public static final int LOTTO_GRADE_NUMBER = 5;
+}
---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,11 @@
+package domain;
+
+/**
+ * 게임 시작
+ */
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.Start();
+    }
+}
