3b6806d42e94af41b41fe5d5f1865904c889ce38
starhn87 <starhn87@gmail.com>
2019-04-11 18:28:07 +0900

    로또 한 장의 가격을 의미하는 상수를 만든다

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,4 +1,13 @@
 package game;
 
+import domain.Lotto;
+import domain.WinningLotto;
+
+import java.util.*;
+
+/**
+ * 로또 게임을 진행하는 클래스
+ */
 public class Game {
+    static final int LOTTO_PRICE = 1000;
 }
