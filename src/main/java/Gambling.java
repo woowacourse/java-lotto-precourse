6fecdf1de587e5e89deb1cc3ced4d88863e49395
hyun <jhs695@naver.com>
2019-04-11 12:34:02 +0900

    Fix Gambling class
    add makeLottos(), makeWinningLotto()
    makeLotto() : 로또 구매
    makeWinningLotto() : 당첨번호, 보너스넘버로 WinningLotto객체 생성

--- src/main/java/Gambling.java
+++ src/main/java/Gambling.java
@@ -1,9 +1,42 @@
 import domain.Console;
+import lotto.BuyLotto;
+import lotto.Lotto;
+import lotto.WinningLotto;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Gambling {
     private Console console;
 
     Gambling(){
         console = new Console();
+    }
+
+    public void start() throws IOException {
+        List<Lotto> lottos = makeLottos();
+        WinningLotto winningLotto = makeWinningLotto();
+    }
+
+    private List<Lotto> makeLottos() throws IOException {
+        int money = console.readMoney();
+        List<Lotto> lottos = new BuyLotto().buying(money);
+        console.writeLottos(lottos);
+        return lottos;
+    }
+
+    private WinningLotto makeWinningLotto() throws IOException {
+        Lotto winningNumber = winningNumber();
+        int bonusNumber = console.readBonusNumber();
+
+        return new WinningLotto(winningNumber, bonusNumber);
+    }
+
+    private Lotto winningNumber() throws IOException {
+        return new Lotto(Arrays.stream(console.readWinningNumbers())
+                .boxed()
+                .collect(Collectors.toList()));
     }
 }
